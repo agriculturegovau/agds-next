051dc5d58ea8f4943fc0301f08a663b5cdcffa23
Mike Hazell <mikehazell@users.noreply.github.com>
2021-12-07 17:44:25 +1100

    example apply ag theme

--- example-site/pages/_app.tsx
+++ example-site/pages/_app.tsx
@@ -1,9 +1,10 @@
 import type { AppProps } from 'next/app';
 import { Theme } from '@ag.ds-next/core';
+import { theme } from '@ag.ds-next/theme-ag';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<Theme>
+		<Theme theme={theme}>
 			<Component {...pageProps} />
 		</Theme>
 	);
