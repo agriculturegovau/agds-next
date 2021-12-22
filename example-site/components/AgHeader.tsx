e4b4697275cf23fa20ee8a62b063c7b1f06d5a37
Mike Hazell <mikehazell@users.noreply.github.com>
2021-12-22 12:43:05 +1100

    update example

--- example-site/components/AgHeader.tsx
+++ example-site/components/AgHeader.tsx
@@ -3,13 +3,17 @@ import { Stack } from '@ag.ds-next/box';
 import { Header } from '@ag.ds-next/header';
 import { MainNav } from '@ag.ds-next/main-nav';
 
+import { useRouter } from 'next/router';
+
 const NAV_LINKS = [
 	{ label: 'Home', href: '/' },
 	{ label: 'Content', href: '/content' },
-	{ label: 'Form example', href: '/' },
+	{ label: 'Form example', href: '/form' },
 ];
 
 export const AgHeader = () => {
+	const router = useRouter();
+
 	return (
 		<Stack>
 			<Header
@@ -18,7 +22,7 @@ export const AgHeader = () => {
 				heading="AG Design-System"
 				subline="Welcome to the AG Design-System"
 			/>
-			<MainNav variant="darkAlt" links={NAV_LINKS} />
+			<MainNav variant="darkAlt" links={NAV_LINKS} activePath={router.asPath} />
 		</Stack>
 	);
 };
---
+++ example-site/pages/form.tsx
@@ -0,0 +1,37 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+import { Body } from '@ag.ds-next/body';
+import { Box } from '@ag.ds-next/box';
+import { Content } from '@ag.ds-next/content';
+
+import { AgHeader } from '../components/AgHeader';
+
+const FormPage: NextPage = () => {
+	return (
+		<Box fontFamily="body" theme="light">
+			<Head>
+				<title>AG Design System | Example site</title>
+				<meta
+					name="description"
+					content="An example of things that can be made with AG-DS"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<AgHeader />
+
+			<main>
+				<Content>
+					<Box>
+						<Body>
+							<h1>Form example</h1>
+						</Body>
+					</Box>
+				</Content>
+			</main>
+		</Box>
+	);
+};
+
+export default FormPage;
