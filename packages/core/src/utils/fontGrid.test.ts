a88a334a123b254f04662c76074a167e014c0116
Nathan Simpson <hello@nathansimpson.design>
2021-12-23 09:54:50 +1100

    Add tests

---
+++ packages/core/src/utils/fontGrid.test.ts
@@ -0,0 +1,22 @@
+import { fontGrid } from './fontGrid';
+
+test('works with sm font token and default lineHeight', () => {
+	expect(fontGrid('sm', 'default')).toStrictEqual({
+		fontSize: '1rem',
+		lineHeight: 1.5,
+	});
+});
+
+test('works with font size of 4.321rem and heading lineHeight', () => {
+	expect(fontGrid(4.321, 'heading')).toStrictEqual({
+		fontSize: '4.321rem',
+		lineHeight: 1.2728535061328397,
+	});
+});
+
+test('works with font size of 1.234567rem and default lineHeight', () => {
+	expect(fontGrid(2.6789, 'default')).toStrictEqual({
+		fontSize: '2.6789rem',
+		lineHeight: 1.4931501735787076,
+	});
+});
