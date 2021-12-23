c0147cb04d7781f5feeba593cef3435a6f5c2001
Mike Hazell <mikehazell@users.noreply.github.com>
2021-12-23 20:41:04 +1100

    Enable overriding link component in application

--- example-site/components/AgHeader.tsx
+++ example-site/components/AgHeader.tsx
@@ -1,9 +1,11 @@
+import type { ReactNode } from 'react';
 import { Logo } from '@ag.ds-next/ag-branding';
 import { Stack } from '@ag.ds-next/box';
 import { Header } from '@ag.ds-next/header';
 import { MainNav } from '@ag.ds-next/main-nav';
 
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 const NAV_LINKS = [
 	{ label: 'Home', href: '/' },
@@ -13,6 +15,12 @@ const NAV_LINKS = [
 
 // TODO: example links when authenticated / not authenticated
 const SECONDARY_LINKS = [{ label: 'Sign in', href: '/sign-in' }];
+
+const NavLink = ({ href, label }: { href: string; label: ReactNode }) => (
+	<Link href={href}>
+		<a>{label}</a>
+	</Link>
+);
 
 export const AgHeader = () => {
 	const router = useRouter();
@@ -30,6 +38,7 @@ export const AgHeader = () => {
 				links={NAV_LINKS}
 				secondaryLinks={SECONDARY_LINKS}
 				activePath={router.asPath}
+				linkComponent={NavLink}
 			/>
 		</Stack>
 	);
--- packages/main-nav/src/MainNav.tsx
+++ packages/main-nav/src/MainNav.tsx
@@ -5,16 +5,22 @@ export function MainNav({
 	variant,
 	links,
 	secondaryLinks,
+	linkComponent,
 	activePath,
 }: React.PropsWithChildren<{
 	variant: NavContainerProps['variant'];
 	links: NavListProps['links'];
 	secondaryLinks?: NavListProps['links'];
+	linkComponent?: NavListProps['linkComponent'];
 	activePath?: string;
 }>) {
 	return (
 		<NavContainer variant={variant}>
-			<NavList links={links} activePath={activePath} />
+			<NavList
+				links={links}
+				activePath={activePath}
+				linkComponent={linkComponent}
+			/>
 			{secondaryLinks?.length ? (
 				<NavList links={secondaryLinks} activePath={activePath} />
 			) : null}
