8813c323caf159efccb0d568855efda866b6c939
Mike Hazell <mikehazell@users.noreply.github.com>
2022-01-06 14:27:58 +1100

    Docs: remove sitemap from 404 page

--- docs/pages/404.tsx
+++ docs/pages/404.tsx
@@ -1,38 +1,18 @@
-import Link from 'next/link';
-import { H1, H2 } from '@ag.ds-next/heading';
+import { H1 } from '@ag.ds-next/heading';
 import { Flex } from '@ag.ds-next/box';
 
 import { Layout } from '../components/Layout';
 import { getAllNavItems } from '../lib/mdxUtils';
+import { Text } from '@ag.ds-next/text';
 
 type StaticProps = Awaited<ReturnType<typeof getStaticProps>>['props'];
 
 export default function NotFoundPage({ navItems }: StaticProps) {
 	return (
 		<Layout navItems={navItems}>
-			<Flex flexDirection="column" gap={2}>
+			<Flex flexDirection="column" gap={1}>
 				<H1>404</H1>
-				<H2>Sitemap</H2>
-				<ul
-					style={{
-						listStyle: 'none',
-						margin: 0,
-						padding: 0,
-					}}
-				>
-					<li style={{ margin: 0 }}>
-						<Link href="/">
-							<a>Home</a>
-						</Link>
-					</li>
-					{navItems.pkgList.map(({ slug, name }) => (
-						<li key={slug} style={{ margin: '1rem 0' }}>
-							<Link href={`/packages/${slug}`}>
-								<a>{name}</a>
-							</Link>
-						</li>
-					))}
-				</ul>
+				<Text as="p">We could not find this page.</Text>
 			</Flex>
 		</Layout>
 	);
