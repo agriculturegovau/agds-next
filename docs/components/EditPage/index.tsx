d969c87cdf4ec8f71cc8c14b64f5da7f1945c0b9
Mike Hazell <mikehazell@users.noreply.github.com>
2022-01-07 16:27:26 +1100

    tidy up

--- docs/components/EditPage/index.tsx
+++ docs/components/EditPage/index.tsx
@@ -1,11 +1,15 @@
 import { Box } from '@ag.ds-next/box';
 
+const ORG = 'steelthreads';
+const REPO = 'agds-next';
+const BRANCH = 'main';
+
 export function EditPage({ path = '' }) {
 	return (
 		<Box
 			as="a"
 			display="inline-block"
-			href={`https://github.com/steelthreads/agds-next/edit/main${path}`}
+			href={`https://github.com/${ORG}/${REPO}/edit/${BRANCH}${path}`}
 			target="_blank"
 			rel="noopener noreferrer"
 			color="action"
