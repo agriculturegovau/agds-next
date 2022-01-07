5d4d1aa2333aa6205eab0704cb634ba3415fc218
Mike Hazell <mikehazell@users.noreply.github.com>
2022-01-07 15:51:04 +1100

    Fix highlighting main nav on subpages

--- packages/main-nav/src/NavList.tsx
+++ packages/main-nav/src/NavList.tsx
@@ -3,6 +3,7 @@ import { Flex } from '@ag.ds-next/box';
 import { themeVars, tokens, useLinkComponent } from '@ag.ds-next/core';
 
 import { NavItem } from './NavItem';
+import { active } from '../../../docs/public/storybook/vendors~main.903e0c9f354136890982.manager.bundle';
 
 export type NavListProps = {
 	links: { href: string; label: ReactNode }[]; // FIXME: this is too restrictive
@@ -11,6 +12,7 @@ export type NavListProps = {
 
 export function NavList({ links, activePath }: NavListProps) {
 	const Link = useLinkComponent();
+	const bestMatch = findBestMatch(links, activePath);
 	return (
 		<Flex
 			as="ul"
@@ -26,7 +28,7 @@ export function NavList({ links, activePath }: NavListProps) {
 			}}
 		>
 			{links.map(({ href, label, ...props }, index) => (
-				<NavItem key={index} active={href === activePath}>
+				<NavItem key={index} active={href === bestMatch}>
 					<Link href={href} {...props}>
 						{label}
 					</Link>
@@ -34,4 +36,21 @@ export function NavList({ links, activePath }: NavListProps) {
 			))}
 		</Flex>
 	);
+}
+
+function findBestMatch(links: { href: string }[], activePath?: string) {
+	if (!activePath) return '';
+	let bestMatch = '';
+
+	for (const link of links) {
+		if (link.href === activePath) return link.href;
+		if (
+			activePath?.startsWith(link.href) &&
+			link.href.length > bestMatch.length
+		) {
+			bestMatch = link.href;
+		}
+	}
+
+	return bestMatch;
 }
