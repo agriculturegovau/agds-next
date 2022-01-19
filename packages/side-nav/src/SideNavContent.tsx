97eed205dbd834cd03fd3f5d79eb3b277c17c0ec
Nathan Simpson <hello@nathansimpson.design>
2022-01-20 10:24:16 +1100

    Remove styles leftover from Accordion

--- packages/side-nav/src/SideNavContent.tsx
+++ packages/side-nav/src/SideNavContent.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from 'react';
 import { Box, BoxProps } from '@ag.ds-next/box';
-import { tokens, themeVars } from '@ag.ds-next/core';
+import { themeVars } from '@ag.ds-next/core';
 
 export type SideNavVariantType = keyof typeof variantMap;
 
@@ -22,27 +22,12 @@ export const SideNavContent = ({
 			theme={variantMap[variant].theme}
 			fontSize="sm"
 			lineHeight="default"
-			paddingLeft={{ xs: 1, md: 0 }}
-			paddingRight={{ xs: 1, md: 0 }}
 			css={{
-				overflow: 'hidden',
-
 				'& > ul > li': {
 					borderBottom: `1px solid ${themeVars.border}`,
 
 					'&:last-of-type': {
 						border: 'none',
-					},
-				},
-
-				// Desktop styles
-				[tokens.mediaQuery.min.md]: {
-					overflow: 'visible',
-
-					// Open the menu on desktop
-					'.au-accordion__body.au-accordion--closed': {
-						display: 'block',
-						height: 'auto',
 					},
 				},
 			}}
