580d9f9b884f838e5e31fc78789d64be8bb0f4c0
Nathan Simpson <hello@nathansimpson.design>
2022-01-19 12:09:23 +1100

    Apply suggestions from code review
    Co-authored-by: Mike <814227+mikehazell@users.noreply.github.com>

--- packages/body/src/Body.stories.tsx
+++ packages/body/src/Body.stories.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Body } from './index';
-import { H1 } from '@ag.ds-next/heading';
 
 export default {
-	title: 'layout/Body',
+	title: 'content/Body',
 	component: Body,
 } as ComponentMeta<typeof Body>;
 
 export const Basic: ComponentStory<typeof Body> = (args) => {
 	return (
 		<Body {...args}>
-			<H1>Hello</H1>
+			<h1>Hello</h1>
 			<p>This is the first paragraph</p>
 
 			<h2>H2 Heading</h2>
