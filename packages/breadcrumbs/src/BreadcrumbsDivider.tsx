b3ec56d3342416c4da60fe75a243555b251b4529
Jordan Overbye <jordanoverbye@gmail.com>
2022-01-20 16:07:31 +1000

    Update BreadcrumbsDivider

--- packages/breadcrumbs/src/BreadcrumbsDivider.tsx
+++ packages/breadcrumbs/src/BreadcrumbsDivider.tsx
@@ -1,14 +1,5 @@
-import { Text } from '@ag.ds-next/text';
+import { Icon } from '@ag.ds-next/icon';
 
-// FIXME This needs to be replaced with an Icon
 export const BreadcrumbsDivider = () => (
-	<Text
-		as="span"
-		aria-hidden="true"
-		fontSize="xs"
-		lineHeight="nospace"
-		color="muted"
-	>
-		&#x3e;
-	</Text>
+	<Icon icon="chevronRight" size={0.75} color="muted" />
 );
