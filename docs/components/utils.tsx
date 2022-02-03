080dea5632e7e4d29a91ea15dc7d191bcbcbd8a7
Mike <814227+mikehazell@users.noreply.github.com>
2022-02-03 11:57:54 +0100

    Update docs/components/utils.tsx

--- docs/components/utils.tsx
+++ docs/components/utils.tsx
@@ -1,10 +1,7 @@
 import { Fragment } from 'react';
 import { Code } from './Code';
 
-import { designSystemComponents } from './design-system-components';
-
 export const mdxComponents = {
-	...designSystemComponents,
 	code: Code,
 	Fragment,
 };
