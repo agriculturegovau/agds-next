7bdf0abf84af979f1695457557b4176a06e07d45
Nathan Simpson <hello@nathansimpson.design>
2022-02-03 15:44:06 +1100

    Column: Add columnStart, columnEnd props

--- packages/columns/src/Column.tsx
+++ packages/columns/src/Column.tsx
@@ -8,18 +8,38 @@ import { Box, BoxProps } from '@ag.ds-next/box';
 
 export type ColumnProps = BoxProps & {
 	columnSpan: ResponsiveProp<1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12>;
+	columnStart?: ResponsiveProp<
+		1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
+	>;
+	columnEnd?: ResponsiveProp<1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12>;
 };
 
 export const Column = forwardRefWithAs<'div', ColumnProps>(function Column(
-	{ columnSpan, ...props },
+	{ columnSpan, columnStart, columnEnd, ...props },
 	ref
 ) {
-	const styles = columnStyles({ columnSpan });
+	const styles = columnStyles({ columnSpan, columnStart, columnEnd });
 	return <Box ref={ref} css={styles} {...props} />;
 });
 
-const columnStyles = ({ columnSpan }: Pick<ColumnProps, 'columnSpan'>) => {
+const columnStyles = ({
+	columnSpan,
+	columnStart,
+	columnEnd,
+}: Pick<ColumnProps, 'columnSpan' | 'columnStart' | 'columnEnd'>) => {
 	return mq({
 		gridColumn: mapResponsiveProp(columnSpan, (v) => `span ${v}/span ${v}`),
+		...(columnStart && {
+			gridColumnStart: mapResponsiveProp(
+				columnStart,
+				(v) => `span ${v}/span ${v}`
+			),
+		}),
+		...(columnEnd && {
+			gridColumnStart: mapResponsiveProp(
+				columnEnd,
+				(v) => `span ${v}/span ${v}`
+			),
+		}),
 	});
 };
