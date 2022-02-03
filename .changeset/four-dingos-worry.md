f6e76c0ae422122ab1fcf1e4af7cc2115010be4e
Mike <814227+mikehazell@users.noreply.github.com>
2022-02-03 23:13:36 +0100

    feature: Card component (#73)
    * docs: a simple placeholder component
    
    * card: Initial commit - add the package and docs
    
    * fixup! docs: a simple placeholder component
    
    * fixup! card: Initial commit - add the package and docs
    
    * card: WIP — basic card
    
    * docs: use the card component in list pages
    
    * Box, Body, TextLink, Breadcrumbs - consolidate link styles
    
    * Body: don’t override styled headings
    
    * Card: WIP - clickable card
    
    * Docs: add package pictograms
    
    * Card: fix focus on CardLink
    
    * Core: add font family to reset
    
    * Card: more examples
    
    * Update docs/components/utils.tsx
    
    * Update packages/card/src/index.tsx
    
    * changesets
    
    Co-authored-by: Mike Hazell <mikehazell@users.noreply.github.com>

---
+++ .changeset/four-dingos-worry.md
@@ -0,0 +1,5 @@
+---
+'@ag.ds-next/card': major
+---
+
+Created the Card component
---
+++ packages/card/src/Card.tsx
@@ -0,0 +1,47 @@
+import { PropsWithChildren, ElementType } from 'react';
+import { Box } from '@ag.ds-next/box';
+import { outline } from '@ag.ds-next/core';
+
+export type CardProps = PropsWithChildren<{
+	as?: ElementType;
+	shadow?: boolean;
+	clickable?: boolean;
+}>;
+
+const componentTokens = {
+	borderColor: 'lightgray',
+	shadow: '0 2px 4px rgba(0, 0, 0, 0.3)',
+	hoverShadow: '0 1px 1px rgba(0, 0, 0, 0.3)',
+};
+
+export const Card = ({ as, children, shadow, clickable }: CardProps) => {
+	return (
+		<Box
+			as={as} // Note: this should be an li when used in a card list
+			display="block"
+			border={!shadow}
+			background="page"
+			rounded
+			css={{
+				position: 'relative',
+				overflow: 'hidden',
+				borderColor: componentTokens.borderColor,
+
+				...(clickable && {
+					cursor: 'pointer',
+					// If any element inside the card receives focus, add a focus ring around the wrapper card div
+					'&:focus-within': outline,
+				}),
+
+				...(shadow && {
+					boxShadow: componentTokens.shadow,
+					'&:hover': clickable
+						? { boxShadow: componentTokens.hoverShadow }
+						: undefined,
+				}),
+			}}
+		>
+			{children}
+		</Box>
+	);
+};
