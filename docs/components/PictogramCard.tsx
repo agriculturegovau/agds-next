09d13d43a3f4ca99a68f18fe42ca4462c748e96f
Jordan Overbye <jordanoverbye@gmail.com>
2022-02-07 11:36:10 +1000

    Create PictogramCard component

---
+++ docs/components/PictogramCard.tsx
@@ -0,0 +1,32 @@
+import { Card, CardInner, CardLink } from '@ag.ds-next/card';
+import { Flex } from '@ag.ds-next/box';
+import { Icon } from '@ag.ds-next/icon';
+
+import { getPictogram } from './pictograms';
+
+export const PictogramCard = ({
+	title,
+	slug,
+	pictogram,
+}: {
+	title: string;
+	slug: string;
+	pictogram: string;
+}) => (
+	<Card as="li" clickable shadow>
+		<Flex
+			alignItems="center"
+			justifyContent="center"
+			padding={1}
+			background="shade"
+		>
+			<img src={getPictogram(pictogram).src} alt={`Pictogram of ${title}`} />
+		</Flex>
+		<CardInner>
+			<CardLink href={slug}>
+				{title}
+				<Icon icon="chevronRight" size={1} />
+			</CardLink>
+		</CardInner>
+	</Card>
+);
