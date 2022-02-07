7ee633faeb3cf75b018e9416962f840c3bff84a4
jordanoverbye <6265154+jordanoverbye@users.noreply.github.com>
2022-02-07 04:14:01 +0000

    Deploying to gh-pages from @ steelthreads/agds-next@f0af8d9f9d408a9a06a0c7777cc85de2b820f71b 🚀

---
+++ _next/static/8iSlfbN9XlvK7vsB5Cfjm/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fguides","\u002Fpackages","\u002Freleases","\u002Fpackages\u002F[group]","\u002Fguides\u002F[slug]","\u002Fpackages\u002F[group]\u002F[slug]","\u002Freleases\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- _next/static/YQiJ37KbQ7YzKCV0ubSgO/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fguides","\u002F","\u002Fpackages","\u002Freleases","\u002Fpackages\u002F[group]","\u002Fguides\u002F[slug]","\u002Fpackages\u002F[group]\u002F[slug]","\u002Freleases\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
