f0af8d9f9d408a9a06a0c7777cc85de2b820f71b
Jordan Overbye <6265154+jordanoverbye@users.noreply.github.com>
2022-02-07 14:10:14 +1000

    docs: Update home page (#83)

--- docs/components/PkgCardList.tsx
+++ docs/components/PkgCardList.tsx
@@ -1,9 +1,5 @@
-import { Flex } from '@ag.ds-next/box';
-import { Icon } from '@ag.ds-next/icon';
-import { Card, CardInner, CardLink } from '@ag.ds-next/card';
 import { Columns, Column } from '@ag.ds-next/columns';
-
-import { getPictogram } from './pictograms';
+import { PictogramCard } from './PictogramCard';
 
 type Pkg = {
 	slug: string;
@@ -11,31 +7,19 @@ type Pkg = {
 	title: string;
 };
 
-const PkgCard = ({ pkg }: { pkg: Pkg }) => (
-	<Card as="li" clickable shadow>
-		{/* Place holder - this needs to be an image */}
-		<Flex
-			alignItems="center"
-			justifyContent="center"
-			padding={1}
-			background="shade"
-		>
-			<img src={getPictogram(pkg.slug).src} alt={`Pictogram of ${pkg.title}`} />
-		</Flex>
-		<CardInner>
-			<CardLink href={`/packages/${pkg.group}/${pkg.slug}`}>
-				{pkg.title}
-				<Icon icon="chevronRight" size={1} />
-			</CardLink>
-		</CardInner>
-	</Card>
-);
-
 export const PkgCardList = ({ items }: { items: Pkg[] }) => (
 	<Columns gap={1} flexWrap="wrap" as="ul">
-		{items.map((p) => (
-			<Column columnSpan={{ xs: 12, sm: 6, md: 6, lg: 4, xl: 3 }} key={p.slug}>
-				<PkgCard pkg={p} />
+		{items.map(({ title, slug, group }) => (
+			<Column
+				key={slug}
+				as="li"
+				columnSpan={{ xs: 12, sm: 6, md: 6, lg: 4, xl: 3 }}
+			>
+				<PictogramCard
+					title={title}
+					pictogram={slug}
+					slug={`/packages/${group}/${slug}`}
+				/>
 			</Column>
 		))}
 	</Columns>
