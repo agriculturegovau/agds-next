07298e0e8cc9846aa07f68a78340df00de821907
Jordan Overbye <jordanoverbye@gmail.com>
2022-02-10 12:05:45 +1000

    Use `TextLink` in `LinkListItem`

--- packages/link-list/src/LinkListItem.tsx
+++ packages/link-list/src/LinkListItem.tsx
@@ -1,34 +1,13 @@
 import { PropsWithChildren } from 'react';
 import { Box } from '@ag.ds-next/box';
-import { useLinkComponent, boxPalette, outline } from '@ag.ds-next/core';
+import { TextLink } from '@ag.ds-next/text';
 
 export const LinkListItem = (
 	props: PropsWithChildren<{
 		href: string;
 	}>
-) => {
-	const Link = useLinkComponent();
-	return (
-		<Box
-			as="li"
-			fontSize="sm"
-			fontFamily="body"
-			lineHeight="default"
-			css={{
-				a: {
-					color: boxPalette.foregroundAction,
-					textDecoration: 'underline',
-
-					'&:hover': {
-						color: boxPalette.foregroundAction,
-						textDecoration: 'none',
-					},
-
-					'&:focus': outline,
-				},
-			}}
-		>
-			<Link {...props} />
-		</Box>
-	);
-};
+) => (
+	<Box as="li">
+		<TextLink {...props} />
+	</Box>
+);
