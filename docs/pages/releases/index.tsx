202b494af9cf8e1971f1b3e4f43be78d870aed93
Jordan Overbye <6265154+jordanoverbye@users.noreply.github.com>
2022-02-11 10:41:44 +1000

    docs: Automatically generate release notes page content (#107)

--- docs/pages/releases/index.tsx
+++ docs/pages/releases/index.tsx
@@ -1,7 +1,8 @@
 import { MDXRemote } from 'next-mdx-remote';
 import { normalize } from 'path';
 import { Body } from '@ag.ds-next/body';
-
+import { Card, CardInner, CardLink } from '@ag.ds-next/card';
+import { Stack } from '@ag.ds-next/box';
 import {
 	getMarkdownData,
 	getReleaseList,
@@ -30,6 +31,20 @@ export default function ReleasesHome({ source, releaseLinks }: StaticProps) {
 					<Body>
 						<MDXRemote {...source} components={mdxComponents} />
 					</Body>
+					<Stack as="ul" gap={1}>
+						{releaseLinks.map(({ href, label, description }) => (
+							<Card key={label} as="li" clickable shadow>
+								<CardInner>
+									<Body>
+										<h3>
+											<CardLink href={href}>{label}</CardLink>
+										</h3>
+										{description ? <p>{description}</p> : null}
+									</Body>
+								</CardInner>
+							</Card>
+						))}
+					</Stack>
 				</PageLayout>
 			</AppLayout>
 		</>
@@ -42,9 +57,11 @@ export async function getStaticProps() {
 	);
 	const source = await serializeMarkdown(content);
 	const releaseList = await getReleaseList();
-	const releaseLinks = releaseList.map(({ slug, title }) => ({
+
+	const releaseLinks = releaseList.map(({ slug, title, description }) => ({
 		href: `/releases/${slug}`,
 		label: title,
+		description,
 	}));
 
 	return {
