c0e8dcc67cdd0cf01999a28d3014d06f3911fbd6
Jordan Overbye <jordanoverbye@gmail.com>
2022-02-23 15:29:31 +1000

    Fix SearchBox

--- packages/search-box/src/SearchBoxButton.tsx
+++ packages/search-box/src/SearchBoxButton.tsx
@@ -22,7 +22,7 @@ export const SearchBoxButton = forwardRef<
 			flexShrink={0}
 			css={{ position: 'relative' }}
 			borderLeft
-			borderWidth="default"
+			borderWidth="bold"
 		>
 			<Button ref={ref} type="submit" aria-label={children} css={buttonStyles}>
 				<span>{children}</span>
