3028ea26b92ff44a996cd61c560244fc3e4289cd
Jordan Overbye <jordanoverbye@gmail.com>
2022-03-03 16:50:40 +1000

    Fixed type name

--- packages/fieldset/src/FieldsetContainer.tsx
+++ packages/fieldset/src/FieldsetContainer.tsx
@@ -1,11 +1,11 @@
 import { ReactNode } from 'react';
 import { Stack } from '@ag.ds-next/box';
 
-export type FieldsetProps = {
+export type FieldsetContainerProps = {
 	children: ReactNode;
 };
 
-export const FieldsetContainer = ({ children }: FieldsetProps) => (
+export const FieldsetContainer = ({ children }: FieldsetContainerProps) => (
 	<fieldset css={{ padding: 0, margin: 0, border: 'none' }}>
 		<Stack gap={1.5}>{children}</Stack>
 	</fieldset>
