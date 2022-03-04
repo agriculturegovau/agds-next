cfea9d7e3c6a5d08e0f731c25f878959efd1e31c
Nathan Simpson <hello@nathansimpson.design>
2022-03-04 13:29:50 +1100

    Icon: Add classname support (#194)
    * Icon: Add support for className
    
    * Apply non-scaling-stroke
    
    * Changeset
    
    * Rollback wrong change

--- packages/icon/src/icons/MenuIcon.tsx
+++ packages/icon/src/icons/MenuIcon.tsx
@@ -3,9 +3,9 @@ import { createIcon } from '../Icon';
 
 export const MenuIcon = createIcon(
 	<Fragment>
-		<path d="M3 6H21" />
-		<path d="M3 12H21" />
-		<path d="M3 18H21" />
+		<path d="M3 6H21" vectorEffect="non-scaling-stroke" />
+		<path d="M3 12H21" vectorEffect="non-scaling-stroke" />
+		<path d="M3 18H21" vectorEffect="non-scaling-stroke" />
 	</Fragment>,
 	'MenuIcon'
 );
--- packages/icon/src/icons/SuccessIcon.tsx
+++ packages/icon/src/icons/SuccessIcon.tsx
@@ -3,8 +3,8 @@ import { createIcon } from '../Icon';
 
 export const SuccessIcon = createIcon(
 	<Fragment>
-		<circle cx="12" cy="12" r="10.5" />
-		<path d="M7 13L10 16L17 9" />
+		<circle cx="12" cy="12" r="10.5" vectorEffect="non-scaling-stroke" />
+		<path d="M7 13L10 16L17 9" vectorEffect="non-scaling-stroke" />
 	</Fragment>,
 	'SuccessIcon'
 );
