6e62afe1802771817b793abfdedfab488cc752e7
Jordan Overbye <6265154+jordanoverbye@users.noreply.github.com>
2022-03-11 10:32:07 +1000

    Tags: Apply design feedback (#214)

--- packages/tags/src/Tag.tsx
+++ packages/tags/src/Tag.tsx
@@ -1,6 +1,6 @@
 import { Box } from '@ag.ds-next/box';
 import { TextLink } from '@ag.ds-next/text';
-import { LinkProps } from '@ag.ds-next/core';
+import { boxPalette, LinkProps } from '@ag.ds-next/core';
 
 export type TagProps = Omit<LinkProps, 'color'>;
 
@@ -15,6 +15,11 @@ export const Tag = (props: TagProps) => {
 			paddingX={0.5}
 			fontSize="sm"
 			color={href ? 'action' : 'text'}
+			css={{
+				'&:hover': {
+					backgroundColor: href ? boxPalette.backgroundShade : undefined,
+				},
+			}}
 			{...props}
 		>
 			{children}
