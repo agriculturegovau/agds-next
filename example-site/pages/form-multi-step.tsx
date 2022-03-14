c4f7304e595ac36693d5ce94fc4b917d303491a6
Jordan Overbye <jordanoverbye@gmail.com>
2022-03-14 12:10:16 +1000

    Update multi step example

--- example-site/pages/form-multi-step.tsx
+++ example-site/pages/form-multi-step.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import { Stack } from '@ag.ds-next/box';
-import { Content } from '@ag.ds-next/content';
+import { Content, ContentBleed } from '@ag.ds-next/content';
 import { Columns, Column } from '@ag.ds-next/columns';
 import { ProgressIndicator } from '@ag.ds-next/progress-indicator';
 
@@ -23,22 +23,24 @@ const FormMultiStepPage: NextPage = () => {
 				<Stack gap={3}>
 					<Columns>
 						<Column columnSpan={{ xs: 12, md: 3 }}>
-							<ProgressIndicator
-								items={[
-									{
-										label: 'Personal details',
-										status: 'doing',
-									},
-									{
-										label: 'Contact details',
-										status: 'todo',
-									},
-									{
-										label: 'Notification preferences',
-										status: 'todo',
-									},
-								]}
-							/>
+							<ContentBleed visible={{ md: false }}>
+								<ProgressIndicator
+									items={[
+										{
+											label: 'Personal details',
+											status: 'doing',
+										},
+										{
+											label: 'Contact details',
+											status: 'todo',
+										},
+										{
+											label: 'Notification preferences',
+											status: 'todo',
+										},
+									]}
+								/>
+							</ContentBleed>
 						</Column>
 						<Column columnSpan={{ xs: 12, md: 8 }} columnStart={{ md: 5 }}>
 							<FormExampleMultiStep />
