edcff2a13a046a5256f64cce1a04d25cd64bf122
Nathan Simpson <hello@nathansimpson.design>
2022-03-22 14:04:26 +1100

    AgBranding: New tones

---
+++ .changeset/proud-nails-crash.md
@@ -0,0 +1,5 @@
+---
+'@ag.ds-next/ag-branding': minor
+---
+
+New tone colours
--- packages/ag-branding/src/theme.ts
+++ packages/ag-branding/src/theme.ts
@@ -8,4 +8,10 @@ export const theme: Theme = {
 	darkBackgroundShadeAlt: '#1B3154',
 	darkForegroundAction: '#9EE8FF',
 	accent: '#F36C52',
+	success: '#098666',
+	successMuted: '#E6F3F0',
+	warning: '#E67300',
+	warningMuted: '#FDF1E6',
+	info: '#009CEB',
+	infoMuted: '#E5F5FD',
 };
