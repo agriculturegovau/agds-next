12333bf24c9da956ac811585910229cf04add990
Nathan Simpson <hello@nathansimpson.design>
2022-03-22 17:13:10 +1100

    Example-site: Add PageAlert to form example (#244)

---
+++ .changeset/early-snakes-drop.md
@@ -0,0 +1,5 @@
+---
+'@ag.ds-next/example-site': minor
+---
+
+Add PageAlert to form example
--- example-site/components/FormExampleSinglePage.tsx
+++ example-site/components/FormExampleSinglePage.tsx
@@ -3,6 +3,7 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import { Button } from '@ag.ds-next/button';
 import { Checkbox } from '@ag.ds-next/control-input';
+import { Body } from '@ag.ds-next/body';
 import { Flex, Stack } from '@ag.ds-next/box';
 import { Fieldset } from '@ag.ds-next/fieldset';
 import { FormStack } from '@ag.ds-next/form-stack';
@@ -10,6 +11,7 @@ import { Select } from '@ag.ds-next/select';
 import { Textarea } from '@ag.ds-next/textarea';
 import { TextInput } from '@ag.ds-next/text-input';
 import { TextLink } from '@ag.ds-next/text';
+import { PageAlert } from '@ag.ds-next/page-alert';
 import { Divider } from './Divider';
 
 const formSchema = yup
@@ -45,14 +47,31 @@ export const FormExampleSinglePage = () => {
 		console.log(data);
 	};
 
+	const hasErrors = Boolean(Object.keys(errors).length);
+
 	return (
 		<form onSubmit={handleSubmit(onSubmit)}>
 			<Stack gap={3}>
+				{hasErrors && (
+					<PageAlert tone="error" title="There is a problem">
+						<Body>
+							<p>Please correct the following fields and try again</p>
+							<ul>
+								{Object.entries(errors).map(([key, value]) => (
+									<li key={key}>
+										<a href={`#${key}`}>{value.message}</a>
+									</li>
+								))}
+							</ul>
+						</Body>
+					</PageAlert>
+				)}
 				<Fieldset legend="Personal details">
 					<FormStack>
 						<TextInput
 							label="Full name"
 							{...register('fullName')}
+							id="fullName"
 							invalid={Boolean(errors.fullName?.message)}
 							message={errors.fullName?.message}
 							maxWidth="xl"
@@ -61,6 +80,7 @@ export const FormExampleSinglePage = () => {
 						<TextInput
 							label="Street address"
 							{...register('streetAddress')}
+							id="streetAddress"
 							invalid={Boolean(errors.streetAddress?.message)}
 							message={errors.streetAddress?.message}
 							maxWidth="xl"
@@ -69,6 +89,7 @@ export const FormExampleSinglePage = () => {
 						<Select
 							label="State"
 							{...register('state')}
+							id="state"
 							placeholder="Please select"
 							options={[
 								{ label: 'QLD', value: 'qld' },
@@ -83,6 +104,7 @@ export const FormExampleSinglePage = () => {
 						<TextInput
 							label="Postcode"
 							{...register('postcode')}
+							id="postcode"
 							invalid={Boolean(errors.postcode?.message)}
 							message={errors.postcode?.message}
 							maxWidth="sm"
@@ -97,6 +119,7 @@ export const FormExampleSinglePage = () => {
 							label="Email"
 							type="email"
 							{...register('email')}
+							id="email"
 							invalid={Boolean(errors.email?.message)}
 							message={errors.email?.message}
 							maxWidth="xl"
@@ -107,6 +130,7 @@ export const FormExampleSinglePage = () => {
 							type="tel"
 							hint="We use this to send you SMS notifications"
 							{...register('mobile')}
+							id="mobile"
 							invalid={Boolean(errors.mobile?.message)}
 							message={errors.mobile?.message}
 							maxWidth="md"
@@ -118,12 +142,14 @@ export const FormExampleSinglePage = () => {
 					<Textarea
 						label="Message"
 						{...register('message')}
+						id="message"
 						invalid={Boolean(errors.message?.message)}
 						message={errors.message?.message}
 						maxWidth="xl"
 					/>
 					<Checkbox
 						{...register('termsAndConditions')}
+						id="termsAndConditions"
 						invalid={Boolean(errors.termsAndConditions?.message)}
 					>
 						By checking this box you agree to our{' '}
