7d52e4b87d5ed6f74205afc5f01514f9ee3dad59
Jordan Overbye <jordanoverbye@gmail.com>
2022-03-24 11:59:19 +1000

    example

---
+++ example-site/pages/form-book.tsx
@@ -0,0 +1,36 @@
+import type { NextPage } from 'next';
+import { Body } from '@ag.ds-next/body';
+import { Stack } from '@ag.ds-next/box';
+import { Content } from '@ag.ds-next/content';
+import { Columns, Column } from '@ag.ds-next/columns';
+import { AppLayout } from '../components/AppLayout';
+import { DocumentTitle } from '../components/DocumentTitle';
+import { FormExampleBook } from '../components/FormExampleBook';
+
+const FormSignInPage: NextPage = () => {
+	return (
+		<>
+			<DocumentTitle title="Booking example" />
+			<AppLayout>
+				<Content>
+					<Columns>
+						<Column columnSpan={{ xs: 12, md: 7 }}>
+							<Stack gap={2}>
+								<Body>
+									<h1>Book a visit</h1>
+									<p>
+										The page heading communicates the main focus of the page.
+										Make your page heading descriptive and keep it succinct.
+									</p>
+								</Body>
+								<FormExampleBook />
+							</Stack>
+						</Column>
+					</Columns>
+				</Content>
+			</AppLayout>
+		</>
+	);
+};
+
+export default FormSignInPage;
