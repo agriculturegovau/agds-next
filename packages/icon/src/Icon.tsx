b944a5345660a231682eee3592ca2d097d772508
Jordan Overbye <jordanoverbye@gmail.com>
2022-03-31 11:54:54 +1000

    Add default width/height

--- packages/icon/src/Icon.tsx
+++ packages/icon/src/Icon.tsx
@@ -51,6 +51,8 @@ export const createIcon = (children: ReactNode, name: string) => {
 		return (
 			<svg
 				aria-hidden="true"
+				width="24"
+				height="24"
 				viewBox="0 0 24 24"
 				clipRule="evenodd"
 				xmlns="http://www.w3.org/2000/svg"
