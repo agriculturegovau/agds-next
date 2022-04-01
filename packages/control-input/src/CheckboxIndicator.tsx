b3dd0600978f966977f7e096c6d55c1d59e86405
Jordan Overbye <jordanoverbye@gmail.com>
2022-04-01 10:06:25 +1000

    Update icon size

--- packages/control-input/src/CheckboxIndicator.tsx
+++ packages/control-input/src/CheckboxIndicator.tsx
@@ -50,32 +50,33 @@ export const CheckboxIndicator = ({
 			color="text"
 			rounded
 		>
-			<CheckboxIcon
-				size={iconSize[size]}
-				css={{ display: 'none', color: globalPalette.lightForegroundText }}
-			/>
+			<CheckboxIcon size={iconSize[size]} />
 		</Flex>
 	);
 };
 
-const CheckboxIcon = ({
-	className,
-	size,
-}: {
-	className?: string;
-	size: Spacing;
-}) => (
+const CheckboxIcon = ({ size }: { size: Spacing }) => (
 	<svg
 		aria-hidden="true"
-		width={mapSpacing(size)}
-		height={mapSpacing(size)}
 		viewBox="0 0 24 24"
-		fill="currentColor"
-		fillRule="evenodd"
 		clipRule="evenodd"
 		xmlns="http://www.w3.org/2000/svg"
-		className={className}
+		focusable="false"
+		css={{
+			width: mapSpacing(size),
+			height: mapSpacing(size),
+			display: 'none',
+			color: globalPalette.lightForegroundText,
+			fill: 'none',
+			stroke: 'currentColor',
+		}}
+		role="img"
 	>
-		<path d="M21.35 8.15C21.55 7.95 21.55 7.65 21.35 7.45L19.05 5.15C18.85 4.95 18.55 4.95 18.35 5.15L9.75 13.75L6.15 10.15C5.95 9.95 5.65 9.95 5.45 10.15L3.15 12.45C2.95 12.65 2.95 12.95 3.15 13.15L9.45 19.45C9.65 19.65 9.95 19.65 10.15 19.45L21.35 8.15V8.15Z" />
+		<path
+			d="M3 13.4286L8.4 19L21 6"
+			strokeWidth={3}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		/>
 	</svg>
 );
