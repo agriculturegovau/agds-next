cd511ca34416f945ac8475cf894e77d6ad6aa51d
Nathan Simpson <hello@nathansimpson.design>
2022-04-04 15:24:47 +1000

    ModalTitle: Title is H2

--- packages/modal/src/ModalTitle.tsx
+++ packages/modal/src/ModalTitle.tsx
@@ -5,7 +5,7 @@ export type ModalTitleProps = { children: ReactNode; id?: string };
 
 export const ModalTitle = ({ children, id }: ModalTitleProps) => {
 	return (
-		<Text fontSize="lg" fontWeight="bold" lineHeight="heading" id={id}>
+		<Text as="h2" fontSize="lg" fontWeight="bold" lineHeight="heading" id={id}>
 			{children}
 		</Text>
 	);
