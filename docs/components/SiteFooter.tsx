0b25487b283f573444d4b54332811ca4c75f5d99
Jordan Overbye <jordanoverbye@gmail.com>
2022-04-14 11:01:27 +1000

    Add welcome to country

--- docs/components/SiteFooter.tsx
+++ docs/components/SiteFooter.tsx
@@ -1,6 +1,7 @@
 import { Footer, FooterDivider } from '@ag.ds-next/footer';
 import { Text } from '@ag.ds-next/text';
 import { LinkList } from '@ag.ds-next/link-list';
+import { tokens } from '@ag.ds-next/core';
 
 const footerLinks = [
 	{ label: 'Home', href: '/' },
@@ -34,10 +35,15 @@ export const SiteFooter = () => {
 	return (
 		<Footer variant="agriculture">
 			<LinkList links={footerLinks} horizontal />
-
 			<FooterDivider />
-
-			<Text>&copy; Commonwealth of Australia.</Text>
+			<Text fontSize="xs" maxWidth={tokens.maxWidth.bodyText}>
+				We acknowledge the traditional owners of country throughout Australia
+				and recognise their continuing connection to land, waters and culture.
+				We pay our respects to their Elders past, present and emerging.
+			</Text>
+			<Text fontSize="xs" maxWidth={tokens.maxWidth.bodyText}>
+				&copy; Commonwealth of Australia.
+			</Text>
 		</Footer>
 	);
 };
