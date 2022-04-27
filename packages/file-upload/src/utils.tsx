fa799ce6a03b908d40c38d5a6fa3b4b7d66731d6
Nathan Simpson <hello@nathansimpson.design>
2022-04-27 16:45:03 +1000

    Update packages/file-upload/src/utils.tsx
    Co-authored-by: Jordan Overbye <6265154+jordanoverbye@users.noreply.github.com>

--- packages/file-upload/src/utils.tsx
+++ packages/file-upload/src/utils.tsx
@@ -2,6 +2,6 @@ import formatFileSize from 'filesize';
 
 export const getFilesTotal = (files: { size: number }[]) => {
 	const label = files.length > 1 ? 'files' : 'file';
-	const size = files.reduce((memo, { size }) => memo + size, 0);
+	const size = files.reduce((a, { size }) => a + size, 0);
 	return `${files.length} ${label} selected (${formatFileSize(size)})`;
 };
