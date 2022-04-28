885a539d30e9db1f38adee167e036b55b22849f5
Jordan Overbye <jordanoverbye@gmail.com>
2022-04-28 12:05:19 +1000

    Improve modularity of HeroBanner

--- packages/hero-banner/src/HeroBanner.tsx
+++ packages/hero-banner/src/HeroBanner.tsx
@@ -1,9 +1,14 @@
 import { PropsWithChildren, ReactNode } from 'react';
-import { Box, Stack } from '@ag.ds-next/box';
-import { Content } from '@ag.ds-next/content';
-import { boxPalette, mq, tokens } from '@ag.ds-next/core';
-import { Heading } from '@ag.ds-next/heading';
-import { Text } from '@ag.ds-next/text';
+import { HeroBannerBackground } from './HeroBannerBackground';
+import {
+	HeroBannerSubTitle,
+	HeroBannerTitle,
+	HeroBannerTitleContainer,
+} from './HeroBannerTitle';
+import { HeroBannerContent } from './HeroBannerContent';
+import { HeroBannerMobileImage } from './HeroBannerMobileImage';
+import { HeroBannerContainer } from './HeroBannerContainer';
+import { HeroBannerVariant } from './utils';
 
 export type HeroBannerProps = PropsWithChildren<{
 	backgroundImageSrc?: string;
@@ -20,75 +25,24 @@ export const HeroBanner = ({
 	children,
 	title,
 	subTitle,
-}: HeroBannerProps) => {
-	const { palette, background, backgroundVar } = variantMap[variant];
-	return (
-		<Box as="section" palette={palette} background={background}>
-			{mobileImage ? (
-				<Box display={['block', 'none']}>{mobileImage}</Box>
-			) : null}
-			<Box
-				width="100%"
-				height="100%"
-				css={
-					backgroundImageSrc
-						? mq({
-								background: [
-									null,
-									`linear-gradient(90deg, ${boxPalette[backgroundVar]} 0%, ${boxPalette[backgroundVar]} 60%, transparent 100%), no-repeat center right url(${backgroundImageSrc});`,
-								],
-								backgroundSize: [null, 'auto 100%'],
-						  })
-						: undefined
-				}
-			>
-				<Content as="div" spacing="sm">
-					<Box
-						width="100%"
-						maxWidth={[null, '66%', '66%', tokens.maxWidth.bodyText]}
-						padding={{ xs: 1, lg: 2 }}
-					>
-						<Stack gap={[1.5, 2]}>
-							{title || subTitle ? (
-								<Stack gap={[0.5, 1]}>
-									<Heading type="h1" fontSize="xxxl">
-										{title}
-									</Heading>
-									<Text as="p" fontSize="md">
-										{subTitle}
-									</Text>
-								</Stack>
-							) : null}
-							{children}
-						</Stack>
-					</Box>
-				</Content>
-			</Box>
-		</Box>
-	);
-};
-
-export const variantMap = {
-	light: {
-		palette: 'light',
-		background: 'body',
-		backgroundVar: 'backgroundBody',
-	},
-	lightAlt: {
-		palette: 'light',
-		background: 'bodyAlt',
-		backgroundVar: 'backgroundBodyAlt',
-	},
-	dark: {
-		palette: 'dark',
-		background: 'body',
-		backgroundVar: 'backgroundBody',
-	},
-	darkAlt: {
-		palette: 'dark',
-		background: 'bodyAlt',
-		backgroundVar: 'backgroundBodyAlt',
-	},
-} as const;
-
-type HeroBannerVariant = keyof typeof variantMap;
+}: HeroBannerProps) => (
+	<HeroBannerContainer variant={variant}>
+		{mobileImage ? (
+			<HeroBannerMobileImage>{mobileImage}</HeroBannerMobileImage>
+		) : null}
+		<HeroBannerBackground
+			backgroundImageSrc={backgroundImageSrc}
+			variant={variant}
+		>
+			<HeroBannerContent>
+				{title || subTitle ? (
+					<HeroBannerTitleContainer>
+						<HeroBannerTitle>{title}</HeroBannerTitle>
+						<HeroBannerSubTitle>{subTitle}</HeroBannerSubTitle>
+					</HeroBannerTitleContainer>
+				) : null}
+				{children}
+			</HeroBannerContent>
+		</HeroBannerBackground>
+	</HeroBannerContainer>
+);
--- packages/hero-banner/src/index.tsx
+++ packages/hero-banner/src/index.tsx
@@ -1,1 +1,6 @@
 export * from './HeroBanner';
+export * from './HeroBannerBackground';
+export * from './HeroBannerContainer';
+export * from './HeroBannerContent';
+export * from './HeroBannerMobileImage';
+export * from './HeroBannerTitle';
