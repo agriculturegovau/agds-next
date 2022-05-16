cb1b24d5f951cefae26fa73e95c2cb27e081e007
Jordan Overbye <jordanoverbye@gmail.com>
2022-05-17 09:43:44 +1000

    cancel modal

--- example-site/components/FormExampleMultiStep/FormExampleMultiStepActions.tsx
+++ example-site/components/FormExampleMultiStep/FormExampleMultiStepActions.tsx
@@ -1,10 +1,14 @@
-import { Button } from '@ag.ds-next/button';
-import { Flex, Stack } from '@ag.ds-next/box';
-import { useFormExampleMultiStepProdiver } from './FormExampleMultiStep';
 import { Fragment } from 'react';
-import { boxPalette } from '@ag.ds-next/core';
+import { Button } from '@ag.ds-next/button';
+import { Flex } from '@ag.ds-next/box';
+import { boxPalette, useTernaryState } from '@ag.ds-next/core';
+import { Modal, ModalButtonGroup } from '@ag.ds-next/modal';
+import { useFormExampleMultiStepProdiver } from './FormExampleMultiStep';
+import { Text } from '@ag.ds-next/text';
 
 export const FormExampleMultiStepActions = () => {
+	const [isModalOpen, openModal, closeModal] = useTernaryState(false);
+
 	const { submittingStep, saveAndExit, saving, cancel } =
 		useFormExampleMultiStepProdiver();
 
@@ -36,10 +40,29 @@ export const FormExampleMultiStepActions = () => {
 				>
 					Save and exit
 				</Button>
-				<Button type="button" variant="tertiary" onClick={cancel}>
+				<Button type="button" variant="tertiary" onClick={openModal}>
 					Cancel
 				</Button>
 			</Flex>
+			<Modal
+				isOpen={isModalOpen}
+				onDismiss={closeModal}
+				title="Are you sure you want to cancel?"
+				actions={
+					<ModalButtonGroup>
+						<Button onClick={closeModal}>Yes, cancel</Button>
+						<Button variant="secondary" onClick={closeModal}>
+							No, take me back
+						</Button>
+					</ModalButtonGroup>
+				}
+			>
+				<Text as="p">
+					This is the Modal Body paragraph, it provides detailed instruction and
+					context for the the modal action. It can also span lines but long form
+					content should be avoided.
+				</Text>
+			</Modal>
 		</Fragment>
 	);
 };
