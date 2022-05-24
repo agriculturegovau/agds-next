e5c81e8224e20225a74ac77a3642e54110e12fc3
Nathan Simpson <hello@nathansimpson.design>
2022-05-24 13:42:35 +1000

    example-site: New single-page form

--- example-site/pages/form-single-page.tsx
+++ example-site/pages/form-single-page.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next';
 import { Stack } from '@ag.ds-next/box';
+import { Breadcrumbs } from '@ag.ds-next/breadcrumbs';
 import { Content } from '@ag.ds-next/content';
 import { Columns, Column } from '@ag.ds-next/columns';
 import { AppLayout } from '../components/AppLayout';
 import { DocumentTitle } from '../components/DocumentTitle';
 import { FormExampleSinglePage } from '../components/FormExampleSinglePage';
-import { Divider } from '../components/Divider';
 import { PageTitle } from '../components/PageTitle';
 
 const FormSinglePage: NextPage = () => {
@@ -17,13 +17,22 @@ const FormSinglePage: NextPage = () => {
 			>
 				<Content>
 					<Columns>
-						<Column columnSpan={{ xs: 12, md: 7 }}>
+						<Column columnSpan={{ xs: 12, md: 8 }}>
 							<Stack gap={3}>
-								<PageTitle
-									title="Single page form example"
-									introduction="Lorem ipsum dolor sit amet, laoreet necessitatibus sed in, ut quem latine eligendi vim, noster utamur sit an."
+								<Breadcrumbs
+									links={[
+										{ href: '#', label: 'Home' },
+										{ href: '#', label: 'Establishments' },
+										{ label: 'Applications' },
+									]}
 								/>
-								<Divider />
+								<PageTitle
+									title="Provide details form title - multi question - xxl/display
+                  (H1)"
+									introduction="Introductory paragraph providing context for this single
+                  page of the multipage form. All questions on page must be
+                  related - md/default (P)"
+								/>
 								<FormExampleSinglePage />
 							</Stack>
 						</Column>
