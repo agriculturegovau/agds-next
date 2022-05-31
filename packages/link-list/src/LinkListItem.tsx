989b45b49d3a0bab37004df76733a7d0c8a500a1
Jordan Overbye <6265154+jordanoverbye@users.noreply.github.com>
2022-06-01 09:09:39 +1000

    LinkList: Use external link indicator (#398)

--- packages/link-list/src/LinkListItem.tsx
+++ packages/link-list/src/LinkListItem.tsx
@@ -1,10 +1,14 @@
 import { Box } from '@ag.ds-next/box';
-import { TextLink, TextLinkProps } from '@ag.ds-next/text';
+import { LinkProps } from '@ag.ds-next/core';
+import { TextLink, TextLinkExternal } from '@ag.ds-next/text';
 
-export type LinkListItemProps = TextLinkProps;
+export type LinkListItemProps = LinkProps;
 
-export const LinkListItem = (props: LinkListItemProps) => (
-	<Box as="li">
-		<TextLink {...props} />
-	</Box>
-);
+export const LinkListItem = (props: LinkListItemProps) => {
+	const TextComponent = props.target == '_blank' ? TextLinkExternal : TextLink;
+	return (
+		<Box as="li">
+			<TextComponent {...props} />
+		</Box>
+	);
+};
