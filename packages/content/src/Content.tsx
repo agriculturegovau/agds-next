b37f2d06b40de22eb6bd98cea9e725a70757e25e
Jordan Overbye <jordanoverbye@gmail.com>
2022-05-31 16:04:09 +1000

    Remove duplication

--- packages/content/src/Content.tsx
+++ packages/content/src/Content.tsx
@@ -2,7 +2,7 @@ import { BaseContent, BaseContentProps } from './BaseContent';
 
 export type ContentProps = BaseContentProps;
 
-export function Content({ children, as = 'section', ...props }: ContentProps) {
+export function Content({ children, as = 'div', ...props }: ContentProps) {
 	return (
 		<BaseContent as={as} {...props} paddingY="none">
 			{children}
