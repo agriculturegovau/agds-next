b7fc8c3132772d046118b765fdc711c0e58c57c4
Jordan Overbye <jordanoverbye@gmail.com>
2022-06-01 12:38:46 +1000

    Use TextLink

--- packages/text/src/TextLinkExternal.tsx
+++ packages/text/src/TextLinkExternal.tsx
@@ -1,7 +1,7 @@
 import { ExternalLinkCallout } from '@ag.ds-next/a11y';
-import { focusStyles, linkStyles } from '@ag.ds-next/box';
 import { ExternalLinkIcon } from '@ag.ds-next/icon';
-import { LinkProps, mapSpacing, useLinkComponent } from '@ag.ds-next/core';
+import { LinkProps, mapSpacing } from '@ag.ds-next/core';
+import { TextLink } from './TextLink';
 
 export type TextLinkExternalProps = LinkProps;
 
@@ -9,14 +9,8 @@ export const TextLinkExternal = ({
 	children,
 	...props
 }: TextLinkExternalProps) => {
-	const Link = useLinkComponent();
 	return (
-		<Link
-			css={[linkStyles, focusStyles]}
-			target="_blank"
-			rel="noopener noreferrer"
-			{...props}
-		>
+		<TextLink target="_blank" rel="noopener noreferrer" {...props}>
 			{children}
 			<ExternalLinkCallout />
 			<ExternalLinkIcon
@@ -28,6 +22,6 @@ export const TextLinkExternal = ({
 					marginLeft: mapSpacing(0.25),
 				}}
 			/>
-		</Link>
+		</TextLink>
 	);
 };
