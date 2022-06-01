470d5d53f03ea9706354ef0b108c8c22933836cb
Jordan Overbye <jordanoverbye@gmail.com>
2022-06-02 09:07:48 +1000

    Move

---
+++ packages/hero-banner/src/HeroBanner/index.tsx
@@ -0,0 +1,6 @@
+export * from './HeroBanner';
+export * from './HeroBannerContainer';
+export * from './HeroBannerContent';
+export * from './HeroBannerImage';
+export * from './HeroBannerMobileImage';
+export * from './HeroBannerTitle';
--- packages/hero-banner/src/index.tsx
+++ packages/hero-banner/src/index.tsx
@@ -1,6 +1,1 @@
 export * from './HeroBanner';
-export * from './HeroBannerContainer';
-export * from './HeroBannerContent';
-export * from './HeroBannerImage';
-export * from './HeroBannerMobileImage';
-export * from './HeroBannerTitle';
