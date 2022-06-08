72948154588e55ca0ad72c4c7af798bf2b8b1902
Jordan Overbye <jordanoverbye@gmail.com>
2022-06-08 11:08:49 +1000

    Update footer

--- example-site/components/SiteFooter.tsx
+++ example-site/components/SiteFooter.tsx
@@ -1,27 +1,48 @@
-import { tokens } from '@ag.ds-next/core';
 import { Footer, FooterDivider } from '@ag.ds-next/footer';
-import { LinkList } from '@ag.ds-next/link-list';
 import { Text } from '@ag.ds-next/text';
+import { LinkList } from '@ag.ds-next/link-list';
+import { tokens } from '@ag.ds-next/core';
 
 const footerLinks = [
-	{ href: '#', label: 'Accessibility' },
-	{ href: '#', label: 'Disclaimer' },
-	{ href: '#', label: 'Privacy' },
+	{
+		label: 'Home',
+		href: '/',
+	},
+	{
+		label: 'Storybook',
+		href: 'https://steelthreads.github.io/agds-next/storybook/index.html',
+		target: '_blank',
+		rel: 'noopener noreferrer',
+	},
+	{
+		label: 'Playroom',
+		href: 'https://steelthreads.github.io/agds-next/playroom/index.html',
+		target: '_blank',
+		rel: 'noopener noreferrer',
+	},
+	{
+		label: 'Starter kit',
+		href: 'https://github.com/steelthreads/agds-next-starter-kit',
+		target: '_blank',
+		rel: 'noopener noreferrer',
+	},
 ];
 
-export const SiteFooter = () => (
-	<Footer variant="agriculture">
-		<nav aria-label="footer">
-			<LinkList links={footerLinks} horizontal />
-		</nav>
-		<FooterDivider />
-		<Text fontSize="xs" maxWidth={tokens.maxWidth.bodyText}>
-			We acknowledge the traditional owners of country throughout Australia and
-			recognise their continuing connection to land, waters and culture. We pay
-			our respects to their Elders past, present and emerging.
-		</Text>
-		<Text fontSize="xs" maxWidth={tokens.maxWidth.bodyText}>
-			&copy; Commonwealth of Australia.
-		</Text>
-	</Footer>
-);
+export const SiteFooter = () => {
+	return (
+		<Footer variant="agriculture">
+			<nav aria-label="footer">
+				<LinkList links={footerLinks} horizontal />
+			</nav>
+			<FooterDivider />
+			<Text fontSize="xs" maxWidth={tokens.maxWidth.bodyText}>
+				We acknowledge the traditional owners of country throughout Australia
+				and recognise their continuing connection to land, waters and culture.
+				We pay our respects to their Elders past, present and emerging.
+			</Text>
+			<Text fontSize="xs" maxWidth={tokens.maxWidth.bodyText}>
+				&copy; Commonwealth of Australia.
+			</Text>
+		</Footer>
+	);
+};
