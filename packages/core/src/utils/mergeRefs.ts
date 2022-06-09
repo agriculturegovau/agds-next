bc8aab118dde1a3e7f4321f9d93909d30a00a7e7
Jordan Overbye <jordanoverbye@gmail.com>
2022-06-09 12:06:26 +1000

    Add mergeRefs

---
+++ packages/core/src/utils/mergeRefs.ts
@@ -0,0 +1,21 @@
+import type { LegacyRef, MutableRefObject, RefCallback } from 'react';
+
+/**
+ * Sometimes we need both a local ref _and_ a forwarded ref for the same element.
+ * This utility merges them for us (as React doesn't offer this natively).
+ *
+ * @see https://github.com/gregberge/react-merge-refs/blob/main/src/index.tsx
+ */
+export function mergeRefs<T = any>(
+	refs: Array<MutableRefObject<T> | LegacyRef<T>>
+): RefCallback<T> {
+	return (value) => {
+		refs.forEach((ref) => {
+			if (typeof ref === 'function') {
+				ref(value);
+			} else if (ref != null) {
+				(ref as MutableRefObject<T | null>).current = value;
+			}
+		});
+	};
+}
