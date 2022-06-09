f9e89917f6f51ee3c4062f509a900f25caadac70
Nathan Simpson <hello@nathansimpson.design>
2022-06-09 14:35:15 +1000

    New stories

--- packages/file-upload/src/FileUpload.stories.tsx
+++ packages/file-upload/src/FileUpload.stories.tsx
@@ -1,17 +1,117 @@
 import React, { useState } from 'react';
+import {
+	useForm,
+	SubmitHandler,
+	SubmitErrorHandler,
+	Controller,
+} from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import { Box } from '@ag.ds-next/box';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { FileUpload, FileUploadValue } from './FileUpload';
+import { FileUpload } from './FileUpload';
+import { FileWithStatus } from './utils';
+import { FormStack } from '@ag.ds-next/form-stack';
+import { Button } from '@ag.ds-next/button';
+import { LoadingBlanket } from '@ag.ds-next/loading';
 
 export default {
 	title: 'forms/FileUpload',
 	component: FileUpload,
 } as ComponentMeta<typeof FileUpload>;
 
-export const OnLight: ComponentStory<typeof FileUpload> = (args) => {
-	const [value, setValue] = useState<FileUploadValue[]>([]);
+const Template: ComponentStory<typeof FileUpload> = (args) => {
+	const [value, setValue] = useState<FileWithStatus[]>([]);
+	return <FileUpload {...args} value={value} onChange={setValue} />;
+};
 
-	const onChange = (acceptedFiles: FileUploadValue[]) => {
+const TemplateWithValue: ComponentStory<typeof FileUpload> = (args) => {
+	const [value, setValue] = useState<FileWithStatus[]>([
+		new File(['this is an example file'], 'example.jpg', {
+			type: 'image/jpg',
+		}),
+	]);
+	return <FileUpload {...args} value={value} onChange={setValue} />;
+};
+
+export const OnLight = TemplateWithValue.bind({});
+OnLight.args = {
+	label: 'Drivers licence',
+};
+
+export const OnDark: ComponentStory<typeof FileUpload> = (args) => (
+	<Box background="body" palette="dark" padding={1.5}>
+		<TemplateWithValue {...args} />
+	</Box>
+);
+OnDark.args = {
+	label: 'Drivers licence',
+};
+
+export const Required = Template.bind({});
+Required.args = {
+	label: 'Drivers licence',
+	required: true,
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+	label: 'Drivers licence',
+	disabled: true,
+};
+
+export const Invalid = Template.bind({});
+Invalid.args = {
+	label: 'Drivers licence',
+	message: 'Please choose a valid file',
+	invalid: true,
+};
+
+export const Valid = TemplateWithValue.bind({});
+Valid.args = {
+	label: 'Drivers licence',
+	message: 'The file you have submitted is valid',
+	valid: true,
+};
+
+export const Multiple = Template.bind({});
+Multiple.args = {
+	label: 'Identity documents',
+	multiple: true,
+};
+
+export const AcceptedFormats = Template.bind({});
+AcceptedFormats.args = {
+	label: 'Identity documents',
+	required: true,
+	hint: 'May include images of your passport, drivers licence etc.',
+	maxSize: 200,
+	accept: [
+		'image/jpeg',
+		'image/jpg',
+		'image/png',
+		'application/pdf',
+		'application/msword',
+		'application/vnd.ms-excel',
+		'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+		'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+	],
+	multiple: true,
+};
+
+export const MultipleImages = Template.bind({});
+MultipleImages.args = {
+	label: 'Photos from your holiday',
+	maxSize: 2000,
+	maxFiles: 3,
+	accept: ['image/jpeg', 'image/jpg', 'image/png', 'image/heic'],
+	multiple: true,
+};
+
+export const InstantUpload: ComponentStory<typeof FileUpload> = (args) => {
+	const [value, setValue] = useState<FileWithStatus[]>([]);
+
+	const onChange = (acceptedFiles: FileWithStatus[]) => {
 		// Update the UI to show loading state straight away
 		setValue(
 			acceptedFiles.map((file) => {
@@ -32,90 +132,88 @@ export const OnLight: ComponentStory<typeof FileUpload> = (args) => {
 
 	return <FileUpload {...args} value={value} onChange={onChange} />;
 };
-
-OnLight.args = {
-	label: 'Drivers licence',
-};
-
-export const OnDark: ComponentStory<typeof FileUpload> = (args) => (
-	<Box background="body" palette="dark" padding={1.5}>
-		<FileUpload {...args} onChange={console.log} />
-	</Box>
-);
-OnDark.args = {
-	label: 'Drivers licence',
-};
-
-export const Required: ComponentStory<typeof FileUpload> = (args) => {
-	return <FileUpload {...args} onChange={console.log} />;
-};
-Required.args = {
-	label: 'Drivers licence',
-	required: true,
-};
-
-export const Disabled: ComponentStory<typeof FileUpload> = (args) => {
-	return <FileUpload {...args} onChange={console.log} />;
-};
-Disabled.args = {
-	label: 'Drivers licence',
-	disabled: true,
-};
-
-export const Invalid: ComponentStory<typeof FileUpload> = (args) => (
-	<FileUpload {...args} onChange={console.log} />
-);
-Invalid.args = {
-	label: 'Drivers licence',
-	message: 'Please choose a valid file',
-	invalid: true,
-};
-
-export const Valid: ComponentStory<typeof FileUpload> = (args) => (
-	<FileUpload {...args} onChange={console.log} />
-);
-Valid.args = {
-	label: 'Drivers licence',
-	message: 'The file you have sublitted is valid',
-	valid: true,
-};
-
-export const Multiple: ComponentStory<typeof FileUpload> = (args) => {
-	return <FileUpload {...args} onChange={console.log} />;
-};
-Multiple.args = {
-	label: 'Identity documents',
-	multiple: true,
-};
-
-export const AcceptedFormats: ComponentStory<typeof FileUpload> = (args) => {
-	return <FileUpload {...args} onChange={console.log} />;
-};
-AcceptedFormats.args = {
-	label: 'Identity documents',
-	required: true,
-	hint: 'May include images of your passport, drivers licence etc.',
-	maxSize: 200,
-	accept: [
-		'image/jpeg',
-		'image/jpg',
-		'image/png',
-		'application/pdf',
-		'application/msword',
-		'application/vnd.ms-excel',
-		'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-		'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-	],
-	multiple: true,
-};
-
-export const MultipleImages: ComponentStory<typeof FileUpload> = (args) => {
-	return <FileUpload {...args} onChange={console.log} />;
-};
-MultipleImages.args = {
+InstantUpload.args = {
 	label: 'Photos from your holiday',
-	maxSize: 2000,
-	maxFiles: 3,
-	accept: ['image/jpeg', 'image/jpg', 'image/png', 'image/heic'],
 	multiple: true,
+	maxFiles: 3,
+};
+
+export const UploadSingleFileOnSubmit: ComponentStory<typeof FileUpload> = (
+	args
+) => {
+	const [isSubmitting, setSubmitting] = useState(false);
+
+	const formSchema = yup
+		.object({
+			file: yup.mixed().required('Select a file to upload'),
+		})
+		.required();
+
+	type FormSchema = yup.InferType<typeof formSchema>;
+
+	const { control, handleSubmit } = useForm<FormSchema>({
+		defaultValues: {
+			file: [],
+		},
+		resolver: yupResolver(formSchema),
+	});
+
+	const onSubmit: SubmitHandler<FormSchema> = async (data) => {
+		setSubmitting(true);
+		setTimeout(() => {
+			setSubmitting(false);
+		}, 2000);
+	};
+
+	const onError: SubmitErrorHandler<FormSchema> = () => {};
+
+	return (
+		<form onSubmit={handleSubmit(onSubmit, onError)}>
+			<FormStack>
+				<Controller
+					control={control}
+					name="file"
+					render={({
+						field: { value, onChange, onBlur, name },
+						fieldState: { invalid, error },
+					}) => (
+						<div css={{ position: 'relative' }}>
+							<FileUpload
+								{...args}
+								id="file"
+								accept={['image/jpeg', 'image/jpg', 'image/png']}
+								maxSize={500} // 500kb
+								multiple={false}
+								value={value}
+								onChange={onChange}
+								onBlur={onBlur}
+								name={name}
+								invalid={invalid}
+								message={error?.message}
+								required
+							/>
+
+							{
+								// We use a LoadingBlanket to communucate that this field is causing the form
+								// to take longer than usual to submit
+								isSubmitting && <LoadingBlanket label="Uploading file" />
+							}
+						</div>
+					)}
+				/>
+				<div>
+					<Button type="submit" loading={isSubmitting}>
+						Submit evidence
+					</Button>
+				</div>
+			</FormStack>
+		</form>
+	);
+};
+UploadSingleFileOnSubmit.args = {
+	label: 'Upload evidence',
+	hint: 'General hint information',
+	maxSize: 100000,
+	multiple: false,
+	required: true,
 };
