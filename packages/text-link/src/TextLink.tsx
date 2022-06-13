b19daf108b7bf1ab60783292d6f4b79c62f2e013
Jordan Overbye <jordanoverbye@gmail.com>
2022-06-13 11:12:20 +1000

    Use box

--- packages/text-link/src/TextLink.tsx
+++ packages/text-link/src/TextLink.tsx
@@ -1,12 +1,11 @@
+import { Box } from '@ag.ds-next/box';
 import { useLinkComponent, forwardRefWithAs } from '@ag.ds-next/core';
-import { Text } from '@ag.ds-next/text';
+import { TextProps } from '@ag.ds-next/text';
 
-export const TextLink = forwardRefWithAs<'a', {}>(function TextLink(
-	{ as, ...props },
+export const TextLink = forwardRefWithAs<'a', TextProps>(function TextLink(
+	{ as, color = 'action', ...props },
 	ref
 ) {
 	const Link = useLinkComponent();
-	return (
-		<Text as={as ?? Link} ref={ref} color="action" link focus {...props} />
-	);
+	return <Box as={as ?? Link} ref={ref} color={color} link focus {...props} />;
 });
