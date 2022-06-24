bb77f99ddbaa32a06a4f8212b2c8200bd9006077
Jordan Overbye <6265154+jordanoverbye@users.noreply.github.com>
2022-06-24 14:41:34 +1000

    templates: Added mock network request to sign-in example (#482)

--- example-site/pages/sign-in-form.tsx
+++ example-site/pages/sign-in-form.tsx
@@ -1,25 +1,36 @@
 import { Stack } from '@ag.ds-next/box';
 import { PageContent } from '@ag.ds-next/content';
+import { Body } from '@ag.ds-next/body';
 import { Columns, Column } from '@ag.ds-next/columns';
 import { AppLayout } from '../components/AppLayout';
 import { DocumentTitle } from '../components/DocumentTitle';
 import { FormExampleSignIn } from '../components/FormExampleSignIn';
 import { PageTitle } from '../components/PageTitle';
+import { FormDivider } from '../components/FormDivider';
 
 export default function SignInFormPage() {
 	return (
 		<>
-			<DocumentTitle title="Sign in example" />
+			<DocumentTitle title="Sign in" />
 			<AppLayout template={{ name: 'Sign-in form', slug: 'sign-in' }}>
 				<PageContent>
 					<Columns>
 						<Column columnSpan={{ xs: 12, md: 7 }}>
 							<Stack gap={3}>
 								<PageTitle
-									title="Sign in example"
-									introduction="The page heading communicates the main focus of the page. Make your page heading descriptive and keep it succinct."
+									title="Sign in form - xxl/display (H1)"
+									introduction="Introductory paragraph providing context for this sign in form. All questions on page must be related - md/default (P)."
 								/>
 								<FormExampleSignIn />
+								<FormDivider />
+								<Body>
+									<p>
+										Don&apos;t have an account? <a href="#">Create account</a>
+									</p>
+									<p>
+										Read our <a href="#">privacy policy</a>
+									</p>
+								</Body>
 							</Stack>
 						</Column>
 					</Columns>
