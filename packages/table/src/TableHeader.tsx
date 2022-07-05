02ebe08134950964078879480d0b07ad40da7154
Jordan Overbye <jordanoverbye@gmail.com>
2022-07-06 09:36:03 +1000

    prop comments

--- packages/table/src/TableHeader.tsx
+++ packages/table/src/TableHeader.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from 'react';
-import { Box, BoxProps } from '@ag.ds-next/box';
+import { Box } from '@ag.ds-next/box';
 import { ResponsiveProp } from '@ag.ds-next/core';
 
 export type TableHeaderWidthType =
@@ -24,11 +24,15 @@ export type TableHeaderWidthType =
 	| '90%';
 
 export type TableHeaderProps = PropsWithChildren<{
+	/** Can be used to conditionally hide or show table cells at different breakpoints. */
+	display?: ResponsiveProp<'none' | 'table-cell'>;
+	/** Defines the cells that the header (defined in the <th>) element relates to. */
 	scope?: 'col' | 'row' | 'colgroup' | 'rowgroup';
+	/** Sets the horizontal alignment of the content. */
 	textAlign?: 'left' | 'center' | 'right';
+	/** Sets the width of the column. */
 	width?: ResponsiveProp<TableHeaderWidthType>;
-}> &
-	Pick<BoxProps, 'display'>;
+}>;
 
 export const TableHeader = ({
 	children,
