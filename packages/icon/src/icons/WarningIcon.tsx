8969f876ae584a648be12d8c17d42964540bb752
Nathan Simpson <hello@nathansimpson.design>
2022-07-05 17:33:06 +1000

    Icons: Add InfoIcon and WarningIcon (#514)

---
+++ packages/icon/src/icons/WarningIcon.tsx
@@ -0,0 +1,11 @@
+import { Fragment } from 'react';
+import { createIcon } from '../Icon';
+
+export const WarningIcon = createIcon(
+	<Fragment>
+		<path d="M12.8858 2.69113C12.5115 1.97647 11.4876 1.97824 11.1132 2.6929C7.60041 9.39921 5.30783 13.776 1.76707 20.5356C1.4183 21.2014 1.90105 22 2.65269 22H21.3473C22.099 22 22.5819 21.2018 22.2331 20.536L12.8858 2.69113Z" />
+		<line x1="12" y1="14" x2="12" y2="9" />
+		<path d="M12 18H12.01" />
+	</Fragment>,
+	'WarningIcon'
+);
