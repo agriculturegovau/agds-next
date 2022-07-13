fe102ffa49b97fbccc5081ea509ff529c22a353c
Jordan Overbye <jordanoverbye@gmail.com>
2022-07-13 15:15:12 +1000

    styles

--- packages/date-picker/src/reactDayPickerStyles.ts
+++ packages/date-picker/src/reactDayPickerStyles.ts
@@ -12,7 +12,6 @@ const cellSize = '2.875rem';
 export const reactDayPickerStyles = {
 	// Visually hidden
 	'.rdp-vhidden': visuallyHiddenStyles,
-
 	// Base button
 	'.rdp-button_reset': {
 		appearance: 'none',
@@ -24,7 +23,6 @@ export const reactDayPickerStyles = {
 		color: 'inherit',
 		font: 'inherit',
 	},
-
 	// Header
 	'.rdp-caption': {
 		display: 'flex',
@@ -32,7 +30,6 @@ export const reactDayPickerStyles = {
 		justifyContent: 'center',
 		marginBottom: mapSpacing(1),
 	},
-
 	'.rdp-caption_label': {
 		zIndex: 1,
 		whiteSpace: 'nowrap',
@@ -41,7 +38,6 @@ export const reactDayPickerStyles = {
 		fontWeight: tokens.fontWeight.bold,
 		...fontGrid('lg', 'nospace'),
 	},
-
 	// Left / right arrows
 	'.rdp-nav_button': {
 		display: 'inline-flex',
@@ -49,19 +45,16 @@ export const reactDayPickerStyles = {
 		justifyContent: 'center',
 		borderRadius: tokens.borderRadius,
 	},
-
 	'.rdp-nav_button_previous': {
 		position: 'absolute',
 		top: mapSpacing(1.5),
 		left: mapSpacing(1.5),
 	},
-
 	'.rdp-nav_button_next': {
 		position: 'absolute',
 		top: mapSpacing(1.5),
 		right: mapSpacing(1.5),
 	},
-
 	// Days of week
 	'.rdp-head_cell': {
 		verticalAlign: 'middle',
@@ -74,24 +67,19 @@ export const reactDayPickerStyles = {
 		color: boxPalette.foregroundMuted,
 		...fontGrid('sm', 'default'),
 	},
-
 	// Day button
 	'.rdp-day': {
 		color: boxPalette.foregroundAction,
 	},
-
 	".rdp-button[aria-disabled='true']": {
 		color: boxPalette.foregroundText,
 		opacity: 0.3,
 		pointerEvents: 'none',
 	},
-
 	".rdp-button:hover:not([aria-disabled='true'])": {
 		backgroundColor: boxPalette.backgroundShade,
 	},
-
 	'.rdp-button:focus, .rdp-button:active': packs.outline,
-
 	'.rdp-weeknumber, .rdp-day': {
 		display: 'flex',
 		justifyContent: 'center',
@@ -100,62 +88,47 @@ export const reactDayPickerStyles = {
 		height: cellSize,
 		borderRadius: tokens.borderRadius,
 	},
-
+	// Table
 	'.rdp-months': {
 		display: 'flex',
 	},
-
 	'.rdp-month': {
-		margin: '0 1em',
+		margin: `0 ${mapSpacing(1)}`,
 	},
-
 	'.rdp-month:first-of-type': {
 		marginLeft: 0,
 	},
-
 	'.rdp-month:last-of-type': {
 		marginRight: 0,
 	},
-
 	'.rdp-table': {
 		margin: 0,
-		maxWidth: 'calc(${cellSize} * 7)',
+		maxWidth: `calc(${cellSize} * 7)`,
 		borderCollapse: 'collapse',
 	},
-
-	'.rdp-with_weeknumber .rdp-table': {
-		maxWidth: 'calc(${cellSize} * 8)',
-		borderCollapse: 'collapse',
-	},
-
 	'.rdp-tbody': {
 		border: 0,
 	},
-
 	'.rdp-cell': {
 		width: cellSize,
 		height: cellSize,
 		padding: 0,
 		textAlign: 'center',
 	},
-
 	".rdp-day_selected:not([aria-disabled='true']), .rdp-day_selected:focus:not([aria-disabled='true']), .rdp-day_selected:active:not([aria-disabled='true']), .rdp-day_selected:hover:not([aria-disabled='true']), .rdp-day_selected:hover:not([aria-disabled='true'])":
 		{
 			backgroundColor: boxPalette.foregroundAction,
 			color: boxPalette.backgroundBody,
 		},
-
 	".rdp-day_selected:not([aria-disabled='true']).rdp-day_range_middle": {
 		backgroundColor: boxPalette.backgroundShade,
 		color: boxPalette.foregroundText,
 		borderRadius: 0,
 	},
-
 	'.rdp-day_range_start:not(.rdp-day_range_end)': {
 		borderTopRightRadius: 0,
 		borderBottomRightRadius: 0,
 	},
-
 	'.rdp-day_range_end:not(.rdp-day_range_start)': {
 		borderTopLeftRadius: 0,
 		borderBottomLeftRadius: 0,
