2a3961ed1a69b9d71ca8a5d4e8d1c0761d76f35d
Nathan Simpson <hello@nathansimpson.design>
2022-07-13 15:11:04 +1000

    Update layout for KitchenSink (#570)

--- .storybook/stories/kitchenSink.tsx
+++ .storybook/stories/kitchenSink.tsx
@@ -26,6 +26,7 @@ import { FileUpload } from '@ag.ds-next/file-upload';
 import { ProgressIndicator } from '@ag.ds-next/progress-indicator';
 import { PageAlert } from '@ag.ds-next/page-alert';
 import { Select } from '@ag.ds-next/select';
+import { SubNav } from '@ag.ds-next/sub-nav';
 import {
 	StatusBadge,
 	NotificationBadge,
@@ -36,6 +37,18 @@ import { Text } from '@ag.ds-next/text';
 import { Textarea } from '@ag.ds-next/textarea';
 import { TextInput } from '@ag.ds-next/text-input';
 import { DatePicker } from '@ag.ds-next/date-picker';
+import { Header } from '@ag.ds-next/header';
+import { Logo } from '@ag.ds-next/ag-branding';
+import { MainNav, MainNavLink } from '@ag.ds-next/main-nav';
+import { Footer, FooterDivider } from '@ag.ds-next/footer';
+import { LinkList } from '@ag.ds-next/link-list';
+import { tokens } from '@ag.ds-next/core';
+import {
+	HeroBanner,
+	HeroBannerSubtitle,
+	HeroBannerTitle,
+	HeroBannerTitleContainer,
+} from '@ag.ds-next/hero-banner';
 
 export default {
 	title: 'Examples/Kitchen Sink',
@@ -62,238 +75,325 @@ const sideNavItems = [
 
 const KitchenSink = () => {
 	return (
-		<PageContent>
-			<Columns>
-				<Column columnSpan={{ xs: 12, md: 4 }}>
-					<Stack gap={1}>
-						<DirectionLink
-							children="Direction link"
-							direction="left"
-							href="#"
+		<Box>
+			<Stack>
+				<Header
+					variant="dark"
+					logo={<Logo />}
+					heading="Export Service"
+					subline="Supporting Australian agricultural exports"
+				/>
+				<MainNav
+					id="main-nav"
+					variant="agriculture"
+					links={[
+						{ label: 'Home', href: '/' },
+						{ label: 'Category 1', href: '/category' },
+					]}
+					activePath={'/'}
+					rightContent={
+						<MainNavLink
+							label="Sign in"
+							href="/sign-in-form"
+							// icon={AvatarIcon}
 						/>
+					}
+				/>
+			</Stack>
+			<HeroBanner>
+				<HeroBannerTitleContainer>
+					<HeroBannerTitle>
+						Website hero banner title - xxxl/display (H1)
+					</HeroBannerTitle>
+					<HeroBannerSubtitle>
+						Short hero banner sentence - md/default (P)
+					</HeroBannerSubtitle>
+				</HeroBannerTitleContainer>
+				<SearchBox onSubmit={() => {}}>
+					<SearchBoxInput label="Search this website" />
+					<SearchBoxButton iconOnly={{ xs: true, md: false }}>
+						Search
+					</SearchBoxButton>
+				</SearchBox>
+			</HeroBanner>
 
-						<SideNav
-							title="Lodging your tax return"
-							titleLink="#"
-							collapseTitle="In this section"
-							items={sideNavItems}
-							activePath="#in-detail/record-keeping/incorrect-amounts"
-						/>
-						<ProgressIndicator
-							items={[
-								{ href: '#', label: 'Introduction', status: 'doing' },
-								{ href: '#', label: 'Business Contacts', status: 'todo' },
-								{ href: '#', label: 'Case Studies', status: 'done' },
-							]}
-						/>
-
-						<CallToActionLink href="#">Call to action</CallToActionLink>
-
-						<Flex gap={0.5} flexWrap="wrap">
-							<StatusBadge tone="info" label="In progress" />
-							<StatusBadge tone="success" label="Resolved" />
-							<StatusBadge tone="error" label="Rejected" />
-							<StatusBadge tone="warning" label="Attention" />
-							<StatusBadge tone="neutral" label="Draft" />
-						</Flex>
-
-						<Flex gap={0.5}>
-							<NotificationBadge tone="neutral" value={16} />
-							<NotificationBadge tone="action" value={8} />
-						</Flex>
-
-						<Flex gap={0.5}>
-							<IndicatorDot tone="neutral" />
-							<IndicatorDot tone="action" />
-						</Flex>
-					</Stack>
-				</Column>
-
-				<Column columnSpan={{ xs: 12, md: 8 }}>
-					<Stack gap={1}>
-						<SearchBox>
-							<SearchBoxInput />
-							<SearchBoxButton>Search</SearchBoxButton>
-						</SearchBox>
-
-						<PageAlert tone="info" title="Notice">
-							<Text as="p">
-								All vacancies close on the advertised closing date unless
-								otherwise specified.
-							</Text>
-						</PageAlert>
-
-						<PageAlert tone="success" title="Submission successful">
-							<Text as="p">
-								Your application has been successfully submitted.
-							</Text>
-						</PageAlert>
-
-						<PageAlert tone="error" title="There is a problem">
-							<Text as="p">
-								<a href="#email">Full name must not be empty</a>
-							</Text>
-						</PageAlert>
-
-						<PageAlert tone="warning" title="Browser out of date">
-							<Text as="p">Your web browser is out of date.</Text>
-						</PageAlert>
-
-						<Breadcrumbs
-							links={[
-								{ href: '#', label: 'Home' },
-								{ href: '#', label: 'Establishments' },
-								{ label: 'Applications' },
-							]}
-						/>
-
-						<InpageNav
-							title="Content"
-							links={[
-								{ href: '#section-1', label: 'Section 1' },
-								{ href: '#section-2', label: 'Section 2' },
-								{ href: '#section-3', label: 'Section 3' },
-								{ href: '#section-4', label: 'Section 4' },
-								{ href: '#section-5', label: 'Section 5' },
-							]}
-						/>
-						<H1>Kitchen Sink</H1>
-						<Text>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-							Pellentesque at arcu eleifend, varius enim non, eleifend nibh.
-							Quisque ac lacinia elit. Orci varius natoque penatibus et magnis
-							dis parturient montes, nascetur ridiculus mus. Integer scelerisque
-							at ligula tempor eleifend. Vestibulum volutpat, dolor eu rutrum
-							consequat, libero justo lacinia tortor, id varius tortor ante sit
-							amet nisl. Aenean at dui diam. Cras a ligula a ante aliquam
-							lacinia. Ut dolor quam, gravida eu dui quis, molestie lacinia
-							dolor. Fusce lacus mi, pharetra molestie tortor eu, finibus
-							lacinia libero.
-						</Text>
-
-						<Callout title="Callout heading">
-							<Text as="p">Description of the callout.</Text>
-						</Callout>
-
-						<Accordion>
-							<AccordionItem title="Accordion One">
-								<AccordionItemContent>
-									<Text as="p">
-										Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-										Pellentesque at arcu eleifend, varius enim non, eleifend
-										nibh. Quisque ac lacinia elit. Orci varius natoque penatibus
-										et magnis dis parturient montes, nascetur ridiculus mus.
-										Integer scelerisque at ligula tempor eleifend. Vestibulum
-										volutpat, dolor eu rutrum consequat, libero justo lacinia
-										tortor, id varius tortor ante sit amet nisl. Aenean at dui
-										diam. Cras a ligula a ante aliquam lacinia. Ut dolor quam,
-										gravida eu dui quis, molestie lacinia dolor. Fusce lacus mi,
-										pharetra molestie tortor eu, finibus lacinia libero.
-									</Text>
-								</AccordionItemContent>
-							</AccordionItem>
-							<AccordionItem title="Accordion Two">
-								<AccordionItemContent>
-									<Text as="p">
-										Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-										Pellentesque at arcu eleifend, varius enim non, eleifend
-										nibh. Quisque ac lacinia elit. Orci varius natoque penatibus
-										et magnis dis parturient montes, nascetur ridiculus mus.
-										Integer scelerisque at ligula tempor eleifend. Vestibulum
-										volutpat, dolor eu rutrum consequat, libero justo lacinia
-										tortor, id varius tortor ante sit amet nisl. Aenean at dui
-										diam. Cras a ligula a ante aliquam lacinia. Ut dolor quam,
-										gravida eu dui quis, molestie lacinia dolor. Fusce lacus mi,
-										pharetra molestie tortor eu, finibus lacinia libero.
-									</Text>
-								</AccordionItemContent>
-							</AccordionItem>
-						</Accordion>
-
-						<CardList templateColumns={2}>
-							<Card background="body">
-								<CardInner>
-									<Stack gap={1}>
-										<Heading type="h2">Card with body background</Heading>
-										<Text as="p">
-											Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-											Cras non finibus leo, non rhoncus neque. Nunc et dui
-											cursus, euismod felis eget, tristique augue. Praesent
-											consequat mollis lorem id efficitur.
-										</Text>
-									</Stack>
-								</CardInner>
-							</Card>
-							<Card background="bodyAlt">
-								<CardInner>
-									<Stack gap={1}>
-										<Heading type="h2">Card with bodyAlt background</Heading>
-										<Text as="p">
-											Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-											Cras non finibus leo, non rhoncus neque. Nunc et dui
-											cursus, euismod felis eget, tristique augue. Praesent
-											consequat mollis lorem id efficitur.
-										</Text>
-									</Stack>
-								</CardInner>
-							</Card>
-						</CardList>
-
-						<H2>Forms</H2>
-						<FormStack>
-							<TextInput label="Name" />
-							<TextInput
-								label="Email"
-								invalid
-								message="An email address is required"
-								id="email"
+			<PageContent>
+				<Columns>
+					<Column columnSpan={{ xs: 12, md: 4 }}>
+						<Stack gap={2}>
+							<SideNav
+								title="Lodging your tax return"
+								titleLink="#"
+								collapseTitle="In this section"
+								items={sideNavItems}
+								activePath="#in-detail/record-keeping/incorrect-amounts"
 							/>
-							<DatePicker
-								label="Birth date"
-								value={new Date()}
-								onChange={() => {}}
-							/>
-							<TextInput
-								label="Username"
-								value="example"
-								valid
-								message="This username is available"
-							/>
-							<Select
-								label="Example"
-								placeholder="Please select"
-								options={[
-									{ value: 'a', label: 'Option A' },
-									{ value: 'b', label: 'Option B' },
-									{ value: 'c', label: 'Option C' },
+							<ProgressIndicator
+								items={[
+									{ href: '#', label: 'Introduction', status: 'doing' },
+									{ href: '#', label: 'Business Contacts', status: 'todo' },
+									{ href: '#', label: 'Case Studies', status: 'done' },
 								]}
 							/>
-							<FileUpload
-								label="Drivers licence"
-								onChange={() => {}}
-								value={[]}
+						</Stack>
+					</Column>
+
+					<Column columnSpan={{ xs: 12, md: 8 }}>
+						<Stack gap={2}>
+							<SubNav
+								links={[
+									{ href: '#usage', label: 'Usage' },
+									{ href: '#code', label: 'Code' },
+									{ href: '#content', label: 'Content' },
+									{ href: '#accessibility', label: 'Accessibility' },
+								]}
 							/>
-							<Textarea label="Message" />
-							<ControlGroup>
-								<Radio checked={false}>Phone</Radio>
-								<Radio checked={false}>Tablet</Radio>
-								<Radio checked={true}>Laptop</Radio>
-							</ControlGroup>
-							<Checkbox checked={false}>Label</Checkbox>
-							<ButtonGroup>
-								<Button size="md">Primary</Button>
-								<Button size="md" variant="secondary">
-									Secondary
-								</Button>
-								<Button size="md" variant="tertiary">
-									Tertiary
-								</Button>
-							</ButtonGroup>
-						</FormStack>
-					</Stack>
-				</Column>
-			</Columns>
-		</PageContent>
+
+							<SearchBox>
+								<SearchBoxInput />
+								<SearchBoxButton>Search</SearchBoxButton>
+							</SearchBox>
+
+							<Stack gap={1}>
+								<PageAlert tone="info" title="Notice">
+									<Text as="p">
+										All vacancies close on the advertised closing date unless
+										otherwise specified.
+									</Text>
+								</PageAlert>
+
+								<PageAlert tone="success" title="Submission successful">
+									<Text as="p">
+										Your application has been successfully submitted.
+									</Text>
+								</PageAlert>
+
+								<PageAlert tone="error" title="There is a problem">
+									<Text as="p">
+										<a href="#email">Full name must not be empty</a>
+									</Text>
+								</PageAlert>
+
+								<PageAlert tone="warning" title="Browser out of date">
+									<Text as="p">Your web browser is out of date.</Text>
+								</PageAlert>
+							</Stack>
+
+							<Breadcrumbs
+								links={[
+									{ href: '#', label: 'Home' },
+									{ href: '#', label: 'Establishments' },
+									{ label: 'Applications' },
+								]}
+							/>
+
+							<InpageNav
+								title="Content"
+								links={[
+									{ href: '#section-1', label: 'Section 1' },
+									{ href: '#section-2', label: 'Section 2' },
+									{ href: '#section-3', label: 'Section 3' },
+									{ href: '#section-4', label: 'Section 4' },
+									{ href: '#section-5', label: 'Section 5' },
+								]}
+							/>
+							<H1>Kitchen Sink</H1>
+							<Text>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+								Pellentesque at arcu eleifend, varius enim non, eleifend nibh.
+								Quisque ac lacinia elit. Orci varius natoque penatibus et magnis
+								dis parturient montes, nascetur ridiculus mus. Integer
+								scelerisque at ligula tempor eleifend. Vestibulum volutpat,
+								dolor eu rutrum consequat, libero justo lacinia tortor, id
+								varius tortor ante sit amet nisl. Aenean at dui diam. Cras a
+								ligula a ante aliquam lacinia. Ut dolor quam, gravida eu dui
+								quis, molestie lacinia dolor. Fusce lacus mi, pharetra molestie
+								tortor eu, finibus lacinia libero.
+							</Text>
+
+							<Callout title="Callout heading">
+								<Text as="p">Description of the callout.</Text>
+							</Callout>
+
+							<Accordion>
+								<AccordionItem title="Accordion One">
+									<AccordionItemContent>
+										<Text as="p">
+											Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+											Pellentesque at arcu eleifend, varius enim non, eleifend
+											nibh. Quisque ac lacinia elit. Orci varius natoque
+											penatibus et magnis dis parturient montes, nascetur
+											ridiculus mus. Integer scelerisque at ligula tempor
+											eleifend. Vestibulum volutpat, dolor eu rutrum consequat,
+											libero justo lacinia tortor, id varius tortor ante sit
+											amet nisl. Aenean at dui diam. Cras a ligula a ante
+											aliquam lacinia. Ut dolor quam, gravida eu dui quis,
+											molestie lacinia dolor. Fusce lacus mi, pharetra molestie
+											tortor eu, finibus lacinia libero.
+										</Text>
+									</AccordionItemContent>
+								</AccordionItem>
+								<AccordionItem title="Accordion Two">
+									<AccordionItemContent>
+										<Text as="p">
+											Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+											Pellentesque at arcu eleifend, varius enim non, eleifend
+											nibh. Quisque ac lacinia elit. Orci varius natoque
+											penatibus et magnis dis parturient montes, nascetur
+											ridiculus mus. Integer scelerisque at ligula tempor
+											eleifend. Vestibulum volutpat, dolor eu rutrum consequat,
+											libero justo lacinia tortor, id varius tortor ante sit
+											amet nisl. Aenean at dui diam. Cras a ligula a ante
+											aliquam lacinia. Ut dolor quam, gravida eu dui quis,
+											molestie lacinia dolor. Fusce lacus mi, pharetra molestie
+											tortor eu, finibus lacinia libero.
+										</Text>
+									</AccordionItemContent>
+								</AccordionItem>
+							</Accordion>
+
+							<CardList templateColumns={2}>
+								<Card background="body">
+									<CardInner>
+										<Stack gap={1}>
+											<Heading type="h2">Card with body background</Heading>
+											<Text as="p">
+												Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+												Cras non finibus leo, non rhoncus neque. Nunc et dui
+												cursus, euismod felis eget, tristique augue. Praesent
+												consequat mollis lorem id efficitur.
+											</Text>
+										</Stack>
+									</CardInner>
+								</Card>
+								<Card background="bodyAlt">
+									<CardInner>
+										<Stack gap={1}>
+											<Heading type="h2">Card with bodyAlt background</Heading>
+											<Text as="p">
+												Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+												Cras non finibus leo, non rhoncus neque. Nunc et dui
+												cursus, euismod felis eget, tristique augue. Praesent
+												consequat mollis lorem id efficitur.
+											</Text>
+										</Stack>
+									</CardInner>
+								</Card>
+							</CardList>
+
+							<H2>Forms</H2>
+							<FormStack>
+								<TextInput label="Name" />
+								<TextInput
+									label="Email"
+									invalid
+									message="An email address is required"
+									id="email"
+								/>
+								<DatePicker
+									label="Birth date"
+									value={new Date()}
+									onChange={() => {}}
+								/>
+								<TextInput
+									label="Username"
+									value="example"
+									valid
+									message="This username is available"
+								/>
+								<Select
+									label="Example"
+									placeholder="Please select"
+									options={[
+										{ value: 'a', label: 'Option A' },
+										{ value: 'b', label: 'Option B' },
+										{ value: 'c', label: 'Option C' },
+									]}
+								/>
+								<FileUpload
+									label="Drivers licence"
+									onChange={() => {}}
+									value={[]}
+								/>
+								<Textarea label="Message" />
+								<ControlGroup>
+									<Radio checked={false}>Phone</Radio>
+									<Radio checked={false}>Tablet</Radio>
+									<Radio checked={true}>Laptop</Radio>
+								</ControlGroup>
+								<Checkbox checked={false}>Label</Checkbox>
+								<ButtonGroup>
+									<Button size="md">Primary</Button>
+									<Button size="md" variant="secondary">
+										Secondary
+									</Button>
+									<Button size="md" variant="tertiary">
+										Tertiary
+									</Button>
+								</ButtonGroup>
+							</FormStack>
+
+							<DirectionLink
+								children="Direction link"
+								direction="left"
+								href="#"
+							/>
+
+							<CallToActionLink href="#">Call to action</CallToActionLink>
+
+							<Flex gap={0.5} flexWrap="wrap">
+								<StatusBadge tone="info" label="In progress" />
+								<StatusBadge tone="success" label="Resolved" />
+								<StatusBadge tone="error" label="Rejected" />
+								<StatusBadge tone="warning" label="Attention" />
+								<StatusBadge tone="neutral" label="Draft" />
+							</Flex>
+
+							<Flex gap={0.5}>
+								<NotificationBadge tone="neutral" value={16} />
+								<NotificationBadge tone="action" value={8} />
+							</Flex>
+
+							<Flex gap={0.5}>
+								<IndicatorDot tone="neutral" />
+								<IndicatorDot tone="action" />
+							</Flex>
+						</Stack>
+					</Column>
+				</Columns>
+			</PageContent>
+			<Footer variant="agriculture">
+				<nav aria-label="footer">
+					<LinkList
+						links={[
+							{ label: 'Home', href: '/' },
+							{
+								label: 'Storybook',
+								href: 'https://steelthreads.github.io/agds-next/storybook/index.html',
+							},
+							{
+								label: 'Playroom',
+								href: 'https://steelthreads.github.io/agds-next/playroom/index.html',
+							},
+							{
+								label: 'Starter kit',
+								href: 'https://github.com/steelthreads/agds-next-starter-kit',
+							},
+						]}
+						horizontal
+					/>
+				</nav>
+				<FooterDivider />
+				<Text fontSize="xs" maxWidth={tokens.maxWidth.bodyText}>
+					We acknowledge the traditional owners of country throughout Australia
+					and recognise their continuing connection to land, waters and culture.
+					We pay our respects to their Elders past, present and emerging.
+				</Text>
+				<Text fontSize="xs" maxWidth={tokens.maxWidth.bodyText}>
+					&copy; 2022 Department of Agriculture, Fisheries and Forestry
+				</Text>
+			</Footer>
+		</Box>
 	);
 };
 
