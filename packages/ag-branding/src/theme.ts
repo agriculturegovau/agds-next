94a2530f2d0f8c0e145d50746295a2ac7a1e562d
Nathan Simpson <hello@nathansimpson.design>
2022-07-15 17:22:43 +1000

    New dark palette

--- packages/ag-branding/src/theme.ts
+++ packages/ag-branding/src/theme.ts
@@ -2,10 +2,10 @@ import { Theme } from '@ag.ds-next/core';
 
 export const theme: Theme = {
 	lightForegroundAction: '#00558b',
-	darkBackgroundBody: '#162845',
-	darkBackgroundShade: '#0A1931',
-	darkBackgroundBodyAlt: '#1F3A64',
-	darkBackgroundShadeAlt: '#1B3154',
+	darkBackgroundBody: '#192D4D',
+	darkBackgroundShade: '#122440',
+	darkBackgroundBodyAlt: '#0C1A31',
+	darkBackgroundShadeAlt: '#071121',
 	darkForegroundAction: '#9EE8FF',
 	accent: '#F36C52',
 	error: '#E60000',
