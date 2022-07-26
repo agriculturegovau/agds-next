857b3edd26b94c72295905e0e8c190f72113bbb9
Nathan Simpson <hello@nathansimpson.design>
2022-07-26 15:51:58 +1000

    Storybook: Add examples of Cards, and Table with StatusBadge (#606)

--- packages/card/src/Card.stories.tsx
+++ packages/card/src/Card.stories.tsx
@@ -1,6 +1,8 @@
+import { Fragment } from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { VisuallyHidden } from '@ag.ds-next/a11y';
 import { Box, Flex, Stack } from '@ag.ds-next/box';
-import { Heading } from '@ag.ds-next/heading';
+import { H2, H3, Heading } from '@ag.ds-next/heading';
 import { ChevronRightIcon } from '@ag.ds-next/icon';
 import { Columns, Column } from '@ag.ds-next/columns';
 import { DirectionLink } from '@ag.ds-next/direction-link';
@@ -8,6 +10,8 @@ import { Tags } from '@ag.ds-next/tags';
 import { Text } from '@ag.ds-next/text';
 import { TextLink } from '@ag.ds-next/text-link';
 import { ExternalLinkIcon } from '@ag.ds-next/icon';
+import { StatusBadge } from '@ag.ds-next/badge';
+import { PageContent } from '@ag.ds-next/content';
 import { Card } from './Card';
 import { CardInner } from './CardInner';
 import { CardLink } from './CardLink';
@@ -126,31 +130,84 @@ FeatureFooter.args = {
 	background: 'body',
 };
 
-const ExampleCard = () => (
-	<Card as="li">
-		<CardInner>
-			<Stack gap={1}>
-				<Heading as="h2" type="h3">
-					Card as `li`
-				</Heading>
-				<Text as="p">
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. In, voluptat
-				</Text>
-				<CardLink href="#">Learn more</CardLink>
-			</Stack>
-		</CardInner>
-	</Card>
-);
+export const CardListStory = () => {
+	const toneMapper = {
+		Closed: 'success',
+		Open: 'warning',
+		Pending: 'info',
+	} as const;
 
-export const CardListStory = () => (
-	<Columns cols={{ xs: 1, sm: 2, md: 3 }}>
-		<ExampleCard />
-		<ExampleCard />
-		<ExampleCard />
-		<ExampleCard />
-		<ExampleCard />
-	</Columns>
-);
+	const listData = [
+		{
+			id: 'RE4321–2201–03',
+			businessName: 'Orange Meat Works',
+			type: 'Record keeping (Minor)',
+			status: 'Pending',
+		},
+		{
+			id: 'RE4321–2201–02',
+			businessName: 'Orange Meat Works',
+			type: 'Hygiene (Major)',
+			status: 'Open',
+		},
+		{
+			id: 'RE4321–2201–01',
+			businessName: 'Orange Meat Works',
+			type: 'Record keeping (Minor)',
+			status: 'Closed',
+		},
+	] as const;
+
+	return (
+		<PageContent>
+			<Stack gap={1.5}>
+				<H2>Active CARs</H2>
+				<Text as="p">You may now manage your CARs online.</Text>
+				<Columns as="ul" gap={1} cols={{ xs: 1, sm: 2, lg: 3 }}>
+					{listData.map((item) => (
+						<Card as="li" shadow clickable key={item.id}>
+							<CardInner>
+								<Stack gap={0.5} width="100%" flexWrap="wrap">
+									<H3>
+										<CardLink href={`#${item.id}`}>
+											{item.businessName}
+										</CardLink>
+									</H3>
+
+									<Text as="p">
+										<VisuallyHidden>Type: </VisuallyHidden>
+										{item.type}
+									</Text>
+
+									<Flex
+										gap={0.5}
+										flexWrap="wrap"
+										justifyContent="space-between"
+										alignItems="center"
+									>
+										<Text color="muted" fontSize="xs">
+											<VisuallyHidden>{'CAR ID: '}</VisuallyHidden>
+											{item.id}
+										</Text>
+										<StatusBadge
+											tone={toneMapper[item.status]}
+											label={
+												<Fragment>
+													<VisuallyHidden>{'Status: '}</VisuallyHidden>
+													{item.status}
+												</Fragment>
+											}
+										/>
+									</Flex>
+								</Stack>
+							</CardInner>
+						</Card>
+					))}
+				</Columns>
+			</Stack>
+		</PageContent>
+	);
+};
 CardListStory.storyName = 'List of Cards';
 
 export const Compositions = () => {
@@ -240,24 +297,6 @@ export const Compositions = () => {
 								<Box fontWeight="bold">
 									<CardLink>Add a commodity, product or operation</CardLink>
 								</Box>
-							</Stack>
-						</CardInner>
-					</Card>
-
-					<Card>
-						<CardInner>
-							<Stack gap={0.5}>
-								<Flex justifyContent="space-between">
-									<Tags items={[{ label: '1034' }]} />
-									<Text color="muted">MEAT</Text>
-								</Flex>
-
-								<Text fontSize="lg" fontWeight="bold">
-									Orange Farmers Market
-								</Text>
-
-								<TextLink>Registrations</TextLink>
-								<TextLink>Insights</TextLink>
 							</Stack>
 						</CardInner>
 					</Card>
