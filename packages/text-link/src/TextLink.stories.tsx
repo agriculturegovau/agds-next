931a7e7a289e57dca316d979f3ba4994b57ea57a
Jordan Overbye <6265154+jordanoverbye@users.noreply.github.com>
2022-07-29 13:18:17 +1000

    button: Add new `text` variant (#622)

--- packages/text-link/src/TextLink.stories.tsx
+++ packages/text-link/src/TextLink.stories.tsx
@@ -1,9 +1,8 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { TextLink, TextButton } from './TextLink';
+import { TextLink } from './TextLink';
 
 export default {
 	title: 'navigation/TextLink',
-	subcomponents: { TextButton },
 	component: TextLink,
 } as ComponentMeta<typeof TextLink>;
 
@@ -13,11 +12,3 @@ const Template: ComponentStory<typeof TextLink> = (args) => {
 
 export const Basic = Template.bind({});
 Basic.args = { children: 'Internal link', href: '#' };
-
-export const Button: ComponentStory<typeof TextButton> = (args) => (
-	<TextButton {...args} />
-);
-Button.args = {
-	children: 'Button element',
-	onClick: console.log,
-};
--- packages/text-link/src/TextLink.tsx
+++ packages/text-link/src/TextLink.tsx
@@ -1,7 +1,6 @@
 import { forwardRef } from 'react';
 import { focusStyles, linkStyles } from '@ag.ds-next/box';
 import { LinkProps, useLinkComponent } from '@ag.ds-next/core';
-import { BaseButton, BaseButtonProps } from '@ag.ds-next/button';
 
 export type TextLinkProps = LinkProps;
 
@@ -9,13 +8,5 @@ export const TextLink = forwardRef<HTMLAnchorElement, TextLinkProps>(
 	function TextLink(props, ref) {
 		const Link = useLinkComponent();
 		return <Link ref={ref} css={[linkStyles, focusStyles]} {...props} />;
-	}
-);
-
-export type TextButtonProps = BaseButtonProps;
-
-export const TextButton = forwardRef<HTMLButtonElement, TextButtonProps>(
-	function TextButton(props, ref) {
-		return <BaseButton ref={ref} css={[linkStyles, focusStyles]} {...props} />;
 	}
 );
