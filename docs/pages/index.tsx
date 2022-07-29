2c34df5f5e5c0a815c66de5bbb0d9f574a44b14c
Jordan Overbye <6265154+jordanoverbye@users.noreply.github.com>
2022-07-29 11:44:06 +1000

    docs: update gap in package card list (#623)
    * PkgCardList gap
    
    * update home page

--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -57,37 +57,28 @@ export default function Homepage() {
 								hard to get a stable release out as soon as we can.
 							</p>
 						</Prose>
-						<Columns gap={1} as="ul">
-							<Column
-								as="li"
-								columnSpan={{ xs: 12, sm: 6, md: 6, lg: 4, xl: 3 }}
-							>
-								<PictogramCard
-									title="Packages"
-									pictogram="packages"
-									href="/packages"
-								/>
-							</Column>
-							<Column
-								as="li"
-								columnSpan={{ xs: 12, sm: 6, md: 6, lg: 4, xl: 3 }}
-							>
-								<PictogramCard
-									title="Guides"
-									pictogram="guides"
-									href="/guides"
-								/>
-							</Column>
-							<Column
-								as="li"
-								columnSpan={{ xs: 12, sm: 6, md: 6, lg: 4, xl: 3 }}
-							>
-								<PictogramCard
-									title="Starter kit"
-									pictogram="starter"
-									href="https://github.com/steelthreads/agds-next-starter-kit"
-								/>
-							</Column>
+						<Columns
+							as="ul"
+							cols={{
+								xs: 1,
+								sm: 2,
+								lg: 3,
+								xl: 4,
+							}}
+						>
+							<PictogramCard
+								title="Packages"
+								pictogram="packages"
+								href="/packages"
+							/>
+
+							<PictogramCard title="Guides" pictogram="guides" href="/guides" />
+
+							<PictogramCard
+								title="Starter kit"
+								pictogram="starter"
+								href="https://github.com/steelthreads/agds-next-starter-kit"
+							/>
 						</Columns>
 					</Stack>
 				</SectionContent>
