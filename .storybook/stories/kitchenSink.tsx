34037338d36d3e80a18e8290ae79e869350dba25
Jordan Overbye <6265154+jordanoverbye@users.noreply.github.com>
2022-08-05 11:46:07 +1000

    kitchen-sink: Update sub nav and checkbox (#627)
    Update sub nav and checkbox

--- .storybook/stories/kitchenSink.tsx
+++ .storybook/stories/kitchenSink.tsx
@@ -166,6 +166,7 @@ const KitchenSink = ({ background }: { background: 'body' | 'bodyAlt' }) => {
 						<Stack gap={2}>
 							<SubNav
 								background={background}
+								activePath="#usage"
 								links={[
 									{ href: '#usage', label: 'Usage' },
 									{ href: '#code', label: 'Code' },
@@ -364,7 +365,7 @@ const KitchenSink = ({ background }: { background: 'body' | 'bodyAlt' }) => {
 									<Radio checked={false}>Tablet</Radio>
 									<Radio checked={true}>Laptop</Radio>
 								</ControlGroup>
-								<Checkbox checked={false}>Label</Checkbox>
+								<Checkbox checked={true}>Label</Checkbox>
 								<ButtonGroup>
 									<Button size="md">Primary</Button>
 									<Button size="md" variant="secondary">
