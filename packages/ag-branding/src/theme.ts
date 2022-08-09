d2005cb253a650a257b026bf902fe116900d152c
Nathan Simpson <hello@nathansimpson.design>
2022-08-09 17:04:39 +1000

    Update values

--- packages/ag-branding/src/theme.ts
+++ packages/ag-branding/src/theme.ts
@@ -1,15 +1,19 @@
 import { Theme } from '@ag.ds-next/core';
 
 export const theme: Theme = {
+	lightForegroundAction: '#00558b',
 	lightBackgroundSuccess: '#E8FFF4',
 	lightBackgroundWarning: '#FDF1E6',
 	lightBackgroundInfo: '#E5F5FD',
-	lightForegroundAction: '#00558b',
 	darkBackgroundBody: '#0A1629',
 	darkBackgroundShade: '#122440',
 	darkBackgroundBodyAlt: '#162846',
 	darkBackgroundShadeAlt: '#20365B',
 	darkForegroundAction: '#9EE8FF',
+	darkBackgroundSuccess: '#133A4B',
+	darkBackgroundInfo: '#193A5B',
+	darkBackgroundWarning: '#393942',
+	darkBackgroundError: '#422F47',
 	accent: '#F36C52',
 	error: '#E60000',
 	success: '#0B996C',
