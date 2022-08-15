bf106655e125d80057d700b5b0e8ffd4f6603e34
Nathan Simpson <hello@nathansimpson.design>
2022-08-15 15:01:26 +1000

    Use new Headers in Kitchensink

--- .storybook/stories/kitchenSink.tsx
+++ .storybook/stories/kitchenSink.tsx
@@ -99,9 +99,9 @@ const KitchenSink = ({ background }: { background: 'body' | 'bodyAlt' }) => {
 	const backgroundSet = backgroundMapper[background];
 	return (
 		<Box background={background}>
-			<Stack>
+			<Stack palette="dark">
 				<Header
-					variant="darkAlt"
+					background="bodyAlt"
 					logo={<Logo />}
 					heading="Export Service"
 					subline="Supporting Australian agricultural exports"
@@ -455,37 +455,40 @@ const KitchenSink = ({ background }: { background: 'body' | 'bodyAlt' }) => {
 					</Column>
 				</Columns>
 			</PageContent>
-			<Footer variant="agriculture">
-				<nav aria-label="footer">
-					<LinkList
-						links={[
-							{ label: 'Home', href: '/' },
-							{
-								label: 'Storybook',
-								href: 'https://steelthreads.github.io/agds-next/storybook/index.html',
-							},
-							{
-								label: 'Playroom',
-								href: 'https://steelthreads.github.io/agds-next/playroom/index.html',
-							},
-							{
-								label: 'Starter kit',
-								href: 'https://github.com/steelthreads/agds-next-starter-kit',
-							},
-						]}
-						horizontal
-					/>
-				</nav>
-				<FooterDivider />
-				<Text fontSize="xs" maxWidth={tokens.maxWidth.bodyText}>
-					We acknowledge the traditional owners of country throughout Australia
-					and recognise their continuing connection to land, waters and culture.
-					We pay our respects to their Elders past, present and emerging.
-				</Text>
-				<Text fontSize="xs" maxWidth={tokens.maxWidth.bodyText}>
-					&copy; 2022 Department of Agriculture, Fisheries and Forestry
-				</Text>
-			</Footer>
+			<Box palette="dark">
+				<Footer background="bodyAlt">
+					<nav aria-label="footer">
+						<LinkList
+							links={[
+								{ label: 'Home', href: '/' },
+								{
+									label: 'Storybook',
+									href: 'https://steelthreads.github.io/agds-next/storybook/index.html',
+								},
+								{
+									label: 'Playroom',
+									href: 'https://steelthreads.github.io/agds-next/playroom/index.html',
+								},
+								{
+									label: 'Starter kit',
+									href: 'https://github.com/steelthreads/agds-next-starter-kit',
+								},
+							]}
+							horizontal
+						/>
+					</nav>
+					<FooterDivider />
+					<Text fontSize="xs" maxWidth={tokens.maxWidth.bodyText}>
+						We acknowledge the traditional owners of country throughout
+						Australia and recognise their continuing connection to land, waters
+						and culture. We pay our respects to their Elders past, present and
+						emerging.
+					</Text>
+					<Text fontSize="xs" maxWidth={tokens.maxWidth.bodyText}>
+						&copy; 2022 Department of Agriculture, Fisheries and Forestry
+					</Text>
+				</Footer>
+			</Box>
 		</Box>
 	);
 };
