8ea125287a776df0b4298bc86131eebd4552ad49
Jordan Overbye <jordanoverbye@gmail.com>
2022-09-01 10:48:34 +1000

    update open and close button aria label

--- packages/main-nav/src/MenuButtons.tsx
+++ packages/main-nav/src/MenuButtons.tsx
@@ -44,7 +44,7 @@ export function OpenButton({ onClick }: MainNavButtonProps) {
 			onClick={onClick}
 			aria-controls="main-nav-dialog"
 			aria-expanded="false"
-			aria-label="Open main navigation"
+			aria-label="Menu"
 		>
 			<svg
 				width="24"
@@ -70,7 +70,7 @@ export function CloseButton({ onClick }: MainNavButtonProps) {
 			onClick={onClick}
 			aria-controls="main-nav-dialog"
 			aria-expanded="true"
-			aria-label="Close main navigation"
+			aria-label="Close"
 		>
 			<svg
 				width="24"
