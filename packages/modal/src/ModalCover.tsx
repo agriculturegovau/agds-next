f485c5c4277cc5ad97bf7e2adb6ddc0cb66a030d
Jordan Overbye <jordanoverbye@gmail.com>
2022-09-06 15:47:59 +1000

    forward ref

--- packages/modal/src/ModalCover.tsx
+++ packages/modal/src/ModalCover.tsx
@@ -1,23 +1,28 @@
-import { KeyboardEventHandler, PropsWithChildren } from 'react';
+import { forwardRef, KeyboardEventHandler, PropsWithChildren } from 'react';
 
 export type ModalCoverProps = PropsWithChildren<{
 	onKeyDown: KeyboardEventHandler<HTMLDivElement>;
 }>;
 
-export const ModalCover = ({ children, onKeyDown }: ModalCoverProps) => (
-	<div
-		css={{
-			position: 'fixed',
-			top: 0,
-			left: 0,
-			bottom: 0,
-			right: 0,
-			backgroundColor: `rgba(0, 0, 0, 0.8)`,
-			zIndex: 100,
-			overflowY: 'auto',
-		}}
-		onKeyDown={onKeyDown}
-	>
-		{children}
-	</div>
+export const ModalCover = forwardRef<HTMLDivElement, ModalCoverProps>(
+	function ModalCover({ children, onKeyDown }, ref) {
+		return (
+			<div
+				ref={ref}
+				css={{
+					position: 'fixed',
+					top: 0,
+					left: 0,
+					bottom: 0,
+					right: 0,
+					backgroundColor: `rgba(0, 0, 0, 0.8)`,
+					zIndex: 100,
+					overflowY: 'auto',
+				}}
+				onKeyDown={onKeyDown}
+			>
+				{children}
+			</div>
+		);
+	}
 );
