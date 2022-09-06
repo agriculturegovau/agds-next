d6606c7ee336fc5c6f3bf14cfacdd5208cff9766
Nathan Simpson <hello@nathansimpson.design>
2022-09-06 12:23:55 +1000

    Single line

--- packages/badge/src/IndicatorDot.stories.tsx
+++ packages/badge/src/IndicatorDot.stories.tsx
@@ -2,6 +2,7 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Flex } from '@ag.ds-next/box';
 import { VisuallyHidden } from '@ag.ds-next/a11y';
 import { Text } from '@ag.ds-next/text';
+import { boxPalette } from '@ag.ds-next/core';
 import { IndicatorDot } from './IndicatorDot';
 
 export default {
@@ -16,43 +17,37 @@ Basic.args = {
 	tone: 'neutral',
 };
 
-const Message = ({ isUnread }) => {
+export const Example = () => {
 	return (
-		<Flex
-			as="li"
-			alignItems="center"
-			gap={1}
-			paddingY={0.5}
-			borderBottom
-			borderColor="muted"
-			justifyContent="space-between"
-			aria-selected="false"
-			role="option"
-		>
-			<Flex flexDirection="column">
-				<Text fontWeight={isUnread ? 'bold' : 'normal'}>Oscar Piastri</Text>
-				<Text>2023 contract</Text>
-				<Text fontSize="xs">
-					Hi Zak. Please find enclosed my signed contract for next year...
-				</Text>
-			</Flex>
+		<Flex flexDirection="column" as="ul">
+			<Flex
+				as="li"
+				alignItems="center"
+				gap={1}
+				paddingY={0.5}
+				paddingX={0.25}
+				borderY
+				borderColor="muted"
+				css={{
+					backgroundColor: boxPalette.systemInfoMuted,
+				}}
+				justifyContent="space-between"
+				aria-selected="false"
+				role="option"
+			>
+				<Flex flexDirection="column">
+					<Text fontWeight="bold">Oscar Piastri</Text>
+					<Text>2023 contract</Text>
+					<Text fontSize="xs">
+						Hi Zak. Please find enclosed my signed contract for next year...
+					</Text>
+				</Flex>
 
-			{isUnread && (
 				<div>
 					<IndicatorDot tone="action" />
 					<VisuallyHidden>Unread message</VisuallyHidden>
 				</div>
-			)}
-		</Flex>
-	);
-};
-export const Example = () => {
-	return (
-		<Flex flexDirection="column" as="ul">
-			<Message />
-			<Message isUnread />
-			<Message />
-			<Message />
+			</Flex>
 		</Flex>
 	);
 };
