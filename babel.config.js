c2d7db21493d98a5bebaf1eb37587b592100d98f
Jordan Overbye <jordanoverbye@gmail.com>
2022-09-12 16:26:09 +1000

    babel

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,14 @@
 module.exports = {
 	presets: [
-		['@babel/preset-env', { targets: { node: 'current' } }],
-		'@babel/preset-typescript',
 		[
-			'@babel/preset-react',
-			{ runtime: 'automatic', importSource: '@emotion/react' },
+			'next/babel',
+			{
+				'preset-react': {
+					runtime: 'automatic',
+					importSource: '@emotion/react',
+				},
+			},
 		],
 	],
-	plugins: ['@emotion/babel-plugin', '@babel/plugin-transform-runtime'],
+	plugins: ['@emotion/babel-plugin'],
 };
