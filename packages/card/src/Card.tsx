fec5852b451b2ee32933f27c2b4ffb06272f6bc1
Jordan Overbye <jordanoverbye@gmail.com>
2022-09-15 11:40:25 +1000

    add listeners

--- packages/card/src/Card.tsx
+++ packages/card/src/Card.tsx
@@ -1,4 +1,10 @@
-import { PropsWithChildren, ElementType, useRef, MouseEvent } from 'react';
+import {
+	PropsWithChildren,
+	ElementType,
+	useRef,
+	createContext,
+	useContext,
+} from 'react';
 import { Box, linkStyles } from '@ag.ds-next/box';
 import { packs } from '@ag.ds-next/core';
 import { cardLinkDataAttr } from './CardLink';
@@ -17,6 +23,18 @@ const componentTokens = {
 	hoverShadow: '0 1px 1px rgba(0, 0, 0, 0.3)',
 };
 
+const CardContext = createContext<
+	{ cardRef: React.RefObject<HTMLDivElement> } | undefined
+>(undefined);
+
+export const useCardContext = () => {
+	const context = useContext(CardContext);
+	if (!context) {
+		throw new Error('Card context not found.');
+	}
+	return context;
+};
+
 export const Card = ({
 	as,
 	background = 'body',
@@ -25,56 +43,39 @@ export const Card = ({
 	shadow,
 	clickable,
 }: CardProps) => {
-	const mousedownTimer = useRef<number>();
-
-	const onMouseDown = () => {
-		mousedownTimer.current = new Date().getTime();
-	};
-
-	// This code ensures that when `CardLink` is used, the text within the card is selectable and the entire card is clickable
-	// Please read more about this technique here: https://inclusive-components.design/cards/#theredundantclickevent
-	const onMouseUp = (event: MouseEvent<HTMLDivElement>) => {
-		if (!clickable || !mousedownTimer.current) return;
-
-		const cardLinkEl = event.currentTarget.querySelector(
-			`[${cardLinkDataAttr}]`
-		) as HTMLAnchorElement | null | undefined;
-		if (!cardLinkEl) return;
-
-		if (new Date().getTime() - mousedownTimer.current < 200) cardLinkEl.click();
-	};
-
+	const cardRef = useRef<HTMLDivElement>(null);
 	return (
-		<Box
-			as={as} // Note: this should be an li when used in a card list
-			onMouseDown={onMouseDown}
-			onMouseUp={onMouseUp}
-			display="block"
-			border
-			borderColor="muted"
-			background={background}
-			rounded
-			className={className}
-			css={{
-				position: 'relative',
-				overflow: 'hidden',
+		<CardContext.Provider value={{ cardRef }}>
+			<Box
+				ref={cardRef}
+				as={as} // Note: this should be an li when used in a card list
+				display="block"
+				border
+				borderColor="muted"
+				background={background}
+				rounded
+				className={className}
+				css={{
+					position: 'relative',
+					overflow: 'hidden',
 
-				...(clickable && {
-					cursor: 'pointer',
-					[`&:hover [${cardLinkDataAttr}]`]: linkStyles['&:hover'],
-					// If any element inside the card receives focus, add a focus ring around the wrapper card div
-					'&:focus-within': packs.outline,
-				}),
+					...(clickable && {
+						cursor: 'pointer',
+						[`&:hover [${cardLinkDataAttr}]`]: linkStyles['&:hover'],
+						// If any element inside the card receives focus, add a focus ring around the wrapper card div
+						'&:focus-within': packs.outline,
+					}),
 
-				...(shadow && {
-					boxShadow: componentTokens.shadow,
-					'&:hover': clickable
-						? { boxShadow: componentTokens.hoverShadow }
-						: undefined,
-				}),
-			}}
-		>
-			{children}
-		</Box>
+					...(shadow && {
+						boxShadow: componentTokens.shadow,
+						'&:hover': clickable
+							? { boxShadow: componentTokens.hoverShadow }
+							: undefined,
+					}),
+				}}
+			>
+				{children}
+			</Box>
+		</CardContext.Provider>
 	);
 };
--- packages/card/src/CardLink.tsx
+++ packages/card/src/CardLink.tsx
@@ -1,5 +1,7 @@
+import { useEffect, useRef } from 'react';
 import { useLinkComponent, LinkProps } from '@ag.ds-next/core';
 import { linkStyles } from '@ag.ds-next/box';
+import { useCardContext } from './Card';
 
 export type CardLinkProps = LinkProps;
 
@@ -7,8 +9,43 @@ export const cardLinkDataAttr = 'data-agds-card-link';
 
 export const CardLink = (props: CardLinkProps) => {
 	const Link = useLinkComponent();
+	const { cardRef } = useCardContext();
+	const cardLinkRef = useRef<HTMLAnchorElement>(null);
+
+	// This code ensures that when this component is used, the text within the `Card` component is selectable AND the entire card is clickable
+	// Please read more about this technique here: https://inclusive-components.design/cards/#theredundantclickevent
+	const mousedownTimer = useRef<number>();
+	useEffect(() => {
+		const parentCardEl = cardRef.current;
+		if (!parentCardEl) return;
+
+		// Set a timer when the user presses the mouse down
+		function onMouseDown() {
+			mousedownTimer.current = new Date().getTime();
+		}
+
+		// If there has been less than 200 milliseconds between the mouse down and mouse up, we assume the user is trying to click the link
+		function onMouseUp() {
+			if (!mousedownTimer.current) return;
+			const linkEl = cardLinkRef.current;
+			if (!linkEl) {
+				throw new Error(
+					'Link ref not found. Please ensure you are forwarding refs to anchor tags which can be configured via the `Core` component.'
+				);
+			}
+			if (new Date().getTime() - mousedownTimer.current < 200) linkEl.click();
+		}
+		parentCardEl.addEventListener('mousedown', onMouseDown);
+		parentCardEl.addEventListener('mouseup', onMouseUp);
+		return () => {
+			parentCardEl.removeEventListener('mousedown', onMouseDown);
+			parentCardEl.removeEventListener('mouseup', onMouseUp);
+		};
+	}, [cardRef]);
+
 	return (
 		<Link
+			ref={cardLinkRef}
 			css={[
 				linkStyles,
 				{
