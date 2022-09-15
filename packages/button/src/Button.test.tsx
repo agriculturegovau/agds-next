991fe439b6426eb4b8b9f8257bb53b3bce063bfb
Jordan Overbye <6265154+jordanoverbye@users.noreply.github.com>
2022-09-15 15:01:44 +1000

    Add basic component tests (#677)

---
+++ packages/button/src/Button.test.tsx
@@ -0,0 +1,60 @@
+import '@testing-library/jest-dom';
+import 'html-validate/jest';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Button, ButtonLink, ButtonProps } from './Button';
+
+function ButtonExample(props: ButtonProps) {
+	return (
+		<Button data-testid="example" {...props}>
+			My button
+		</Button>
+	);
+}
+
+describe('Button', () => {
+	it('renders correctly with minimal props', () => {
+		render(<ButtonExample />);
+		const el = screen.getByTestId('example');
+		expect(el).toBeInTheDocument();
+		expect(el.tagName).toBe('BUTTON');
+		expect(el).toHaveAccessibleName('My button');
+	});
+	it('renders a valid HTML structure', () => {
+		const { container } = render(<ButtonExample />);
+		expect(container).toHTMLValidate({
+			extends: ['html-validate:recommended'],
+		});
+	});
+	it('responds to an onClick event', async () => {
+		const onClick = jest.fn();
+		render(<ButtonExample onClick={onClick} />);
+		await userEvent.click(screen.getByTestId('example'));
+		expect(onClick).toHaveBeenCalledTimes(1);
+	});
+});
+
+function ButtonLinkExample() {
+	return (
+		<ButtonLink href="#" data-testid="example">
+			My button link
+		</ButtonLink>
+	);
+}
+
+describe('ButtonLink', () => {
+	it('renders correctly with minimal props', () => {
+		render(<ButtonLinkExample />);
+		const el = screen.getByTestId('example');
+		expect(el).toBeInTheDocument();
+		expect(el.tagName).toBe('A');
+		expect(el).toHaveAttribute('href', '#');
+		expect(el).toHaveAccessibleName('My button link');
+	});
+	it('renders a valid HTML structure', () => {
+		const { container } = render(<ButtonLinkExample />);
+		expect(container).toHTMLValidate({
+			extends: ['html-validate:recommended'],
+		});
+	});
+});
