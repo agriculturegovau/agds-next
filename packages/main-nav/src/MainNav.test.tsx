9960c164e16e1fe803768dbf95625479cd1111d5
Jordan Overbye <jordanoverbye@gmail.com>
2022-09-16 10:00:25 +1000

    main-nav tests

---
+++ packages/main-nav/src/MainNav.test.tsx
@@ -0,0 +1,36 @@
+import '@testing-library/jest-dom';
+import 'html-validate/jest';
+import { render } from '@testing-library/react';
+import { AvatarIcon } from '@ag.ds-next/icon';
+import { MainNav } from './MainNav';
+
+function Example() {
+	return (
+		<MainNav
+			items={[
+				{ href: '#home', label: 'Home' },
+				{ href: '#content', label: 'Content page' },
+				{ href: '#form', label: 'Form page' },
+				{ href: '#simple', label: 'Simple page' },
+			]}
+			secondaryItems={[
+				{
+					href: '#sign-in',
+					label: 'Sign in',
+					endElement: <AvatarIcon color="action" />,
+				},
+			]}
+			activePath="#content"
+		/>
+	);
+}
+
+describe('MainNav', () => {
+	it('renders a valid HTML structure', () => {
+		const { container } = render(<Example />);
+		expect(container).toHTMLValidate({
+			extends: ['html-validate:recommended'],
+			rules: { 'no-inline-style': 'off' },
+		});
+	});
+});
