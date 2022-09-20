edb196e808e7b8567cff76b128f1b58af17c2364
Nathan Simpson <hello@nathansimpson.design>
2022-09-20 17:58:14 +1000

    Implement basic component

--- packages/detail/src/Detail.tsx
+++ packages/detail/src/Detail.tsx
@@ -1,7 +1,44 @@
-import { Box } from '@ag.ds-next/box';
+import { Box, Flex } from '@ag.ds-next/box';
+import { InfoIcon, ChevronDownIcon } from '@ag.ds-next/icon';
 
-export type DetailProps = {};
+export type DetailProps = {
+	label: string;
+};
 
-export function Detail(props: DetailProps) {
-	return <Box {...props}>Detail</Box>;
+export function Detail({ label = 'Details' }: DetailProps) {
+	return (
+		<details
+			css={{
+				'&[open] summary svg:last-of-type': {
+					transform: 'rotate(180deg)',
+				},
+				'summary::marker, summary::-webkit-details-marker': {
+					display: 'none',
+				},
+			}}
+		>
+			<Flex
+				as="summary"
+				gap={0.5}
+				link
+				focus
+				alignItems="center"
+				fontWeight="bold"
+			>
+				<InfoIcon size="md" />
+				{label}
+				<ChevronDownIcon weight="bold" size="sm" />
+			</Flex>
+
+			<Box
+				background="shade"
+				padding={1.5}
+				borderLeft
+				borderLeftWidth="xl"
+				highContrastOutline
+			>
+				Something small enough to escape casual notice.
+			</Box>
+		</details>
+	);
 }
