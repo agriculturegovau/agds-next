83e42e9f11fb2fa72c8708e24460ef0b3d52641f
Nathan Simpson <hello@nathansimpson.design>
2022-09-21 16:25:47 +1000

    forwardRef

--- packages/detail/src/Detail.tsx
+++ packages/detail/src/Detail.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from 'react';
+import { forwardRef, PropsWithChildren } from 'react';
 import { Box, Flex } from '@ag.ds-next/box';
 import { InfoIcon, ChevronDownIcon } from '@ag.ds-next/icon';
 
@@ -7,42 +7,45 @@ export type DetailProps = PropsWithChildren<{
 	label?: string;
 }>;
 
-export function Detail({ children, label = 'Details' }: DetailProps) {
-	return (
-		<details
-			css={{
-				'&[open] summary svg:last-of-type': {
-					transform: 'rotate(180deg)',
-				},
-				'summary::marker, summary::-webkit-details-marker': {
-					display: 'none',
-				},
-			}}
-		>
-			<Flex
-				as="summary"
-				gap={0.5}
-				link
-				focus
-				alignItems="center"
-				fontWeight="bold"
-				paddingY={0.5}
-				rounded
+export const Detail = forwardRef<HTMLDetailsElement, DetailProps>(
+	function Detail({ children, label = 'Details' }, ref) {
+		return (
+			<details
+				ref={ref}
+				css={{
+					'&[open] summary svg:last-of-type': {
+						transform: 'rotate(180deg)',
+					},
+					'summary::marker, summary::-webkit-details-marker': {
+						display: 'none',
+					},
+				}}
 			>
-				<InfoIcon size="md" aria-hidden="true" />
-				{label}
-				<ChevronDownIcon weight="bold" size="sm" />
-			</Flex>
+				<Flex
+					as="summary"
+					gap={0.5}
+					link
+					focus
+					alignItems="center"
+					fontWeight="bold"
+					paddingY={0.5}
+					rounded
+				>
+					<InfoIcon size="md" aria-hidden="true" />
+					{label}
+					<ChevronDownIcon weight="bold" size="sm" />
+				</Flex>
 
-			<Box
-				background="shade"
-				padding={1.5}
-				borderLeft
-				borderLeftWidth="xl"
-				highContrastOutline
-			>
-				{children}
-			</Box>
-		</details>
-	);
-}
+				<Box
+					background="shade"
+					padding={1.5}
+					borderLeft
+					borderLeftWidth="xl"
+					highContrastOutline
+				>
+					{children}
+				</Box>
+			</details>
+		);
+	}
+);
