1d2d3ee78bc67be40e0b4cb4f31211c4dea126ca
Jordan Overbye <jordanoverbye@gmail.com>
2022-09-21 12:41:01 +1000

    remove debounce

--- packages/combobox/src/Combobox.tsx
+++ packages/combobox/src/Combobox.tsx
@@ -224,26 +224,6 @@ export function Combobox<Option extends DefaultComboboxOption>({
 	);
 }
 
-function debounce<A = unknown, R = void>(
-	fn: (args: A) => R,
-	ms: number
-): (args: A) => Promise<R> {
-	let timer: NodeJS.Timeout;
-
-	const debouncedFunc = (args: A): Promise<R> =>
-		new Promise((resolve) => {
-			if (timer) {
-				clearTimeout(timer);
-			}
-
-			timer = setTimeout(() => {
-				resolve(fn(args));
-			}, ms);
-		});
-
-	return debouncedFunc;
-}
-
 function defaultRenderItem<Option extends DefaultComboboxOption>(
 	item: Option,
 	inputValue: string
