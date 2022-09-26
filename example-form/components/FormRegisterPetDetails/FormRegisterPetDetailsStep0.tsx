254827f569ea5f37156fd8d4fdb3183cee3fe1f5
Jordan Overbye <jordanoverbye@gmail.com>
2022-09-26 16:40:14 +1000

    start clean up of task 2

--- example-form/components/FormRegisterPetDetails/FormRegisterPetDetailsStep0.tsx
+++ example-form/components/FormRegisterPetDetails/FormRegisterPetDetailsStep0.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useRef, useState } from 'react';
 import { useForm, SubmitHandler, SubmitErrorHandler } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
@@ -7,16 +6,16 @@ import { TextInput } from '@ag.ds-next/text-input';
 import { mapSpacing } from '@ag.ds-next/core';
 import { ControlGroup, Radio } from '@ag.ds-next/control-input';
 import { FormStack } from '@ag.ds-next/form-stack';
-import { useScrollToField } from '@ag.ds-next/field';
-import { PageAlert } from '@ag.ds-next/page-alert';
-import { Prose } from '@ag.ds-next/prose';
 import { FormRegisterPetDetailsContainer } from './FormRegisterPetDetailsContainer';
 import { FormRegisterPetDetailsActions } from './FormRegisterPetDetailsActions';
 import { useFormExampleMultiStep } from './FormRegisterPetDetails';
 
 const formSchema = yup
 	.object({
-		typeOfPet: yup.string().required('Enter your first name'),
+		typeOfPet: yup
+			.string()
+			.typeError('Select an option')
+			.required('Select an option'),
 		otherDetails: yup.string().when('typeOfPet', (value, schema) => {
 			if (value === 'other') {
 				return schema.required('Enter details');
@@ -30,9 +29,6 @@ export type FormSchema = yup.InferType<typeof formSchema>;
 
 export const FormRegisterPetDetailsStep0 = () => {
 	const { next, stepFormState } = useFormExampleMultiStep();
-	const scrollToField = useScrollToField();
-	const errorRef = useRef<HTMLDivElement>(null);
-	const [focusedError, setFocusedError] = useState(false);
 
 	const {
 		watch,
@@ -45,23 +41,8 @@ export const FormRegisterPetDetailsStep0 = () => {
 	});
 
 	const onSubmit: SubmitHandler<FormSchema> = (data) => {
-		setFocusedError(false);
 		next(data);
 	};
-
-	const onError: SubmitErrorHandler<FormSchema> = () => {
-		setFocusedError(false);
-	};
-
-	// Only show the page alert if there is more than 1 error
-	const hasErrors = Object.keys(errors).length > 1;
-
-	useEffect(() => {
-		if (hasErrors && !focusedError) {
-			errorRef.current?.focus();
-			setFocusedError(true);
-		}
-	}, [hasErrors, focusedError, errors]);
 
 	const showConditionalField = watch('typeOfPet') === 'other';
 
@@ -70,36 +51,8 @@ export const FormRegisterPetDetailsStep0 = () => {
 			title="Type of pet"
 			introduction="Domestic pets living in urban environments need to be registered."
 		>
-			<Stack
-				as="form"
-				gap={3}
-				onSubmit={handleSubmit(onSubmit, onError)}
-				noValidate
-			>
+			<Stack as="form" gap={3} onSubmit={handleSubmit(onSubmit)} noValidate>
 				<FormStack>
-					{hasErrors && (
-						<PageAlert
-							ref={errorRef}
-							tone="error"
-							title="There is a problem"
-							tabIndex={-1}
-						>
-							<Prose>
-								<p>Please correct the following fields and try again</p>
-								<ul>
-									{Object.entries(errors).map(([key, value]) => (
-										<li key={key}>
-											<a href={`#${key}`} onClick={scrollToField}>
-												{Array.isArray(value)
-													? value[0].message
-													: value.message}
-											</a>
-										</li>
-									))}
-								</ul>
-							</Prose>
-						</PageAlert>
-					)}
 					<ControlGroup
 						id="checkbox"
 						label="What type of animal are you registering?"
