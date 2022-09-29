a6469c769fecd02a14e6db3822f8d2bb37800514
Nathan Simpson <hello@nathansimpson.design>
2022-09-29 15:29:47 +1000

    moved values

--- docs/components/prism-theme.ts
+++ docs/components/prism-theme.ts
@@ -1,12 +1,6 @@
 import { PrismTheme } from 'prism-react-renderer';
-import dracula from 'prism-react-renderer/themes/dracula';
-import { boxPalette, globalPalette, tokens, fontGrid } from '@ag.ds-next/core';
+import { boxPalette, tokens, fontGrid } from '@ag.ds-next/core';
 
-const blue = '#79c0ff';
-const purple = '#d2a8ff';
-
-// Original: https://github.com/dracula/visual-studio-code
-// Converted automatically using ./tools/themeFromVsCode
 export const prismTheme: PrismTheme = {
 	plain: {
 		fontFamily: tokens.font.monospace,
@@ -15,7 +9,6 @@ export const prismTheme: PrismTheme = {
 		backgroundColor: boxPalette.backgroundBodyAlt,
 	},
 	styles: [
-		// ...dracula.styles,
 		{
 			types: [
 				'string',
@@ -33,7 +26,7 @@ export const prismTheme: PrismTheme = {
 				'class-name',
 			],
 			style: {
-				color: blue,
+				color: '#79c0ff',
 			},
 		},
 		{
@@ -72,7 +65,7 @@ export const prismTheme: PrismTheme = {
 		{
 			types: ['atrule', 'attr-name', 'function'],
 			style: {
-				color: purple,
+				color: '#d2a8ff',
 			},
 		},
 		{
@@ -83,21 +76,3 @@ export const prismTheme: PrismTheme = {
 		},
 	],
 };
-
-/**
-
-
-namespace {
-	opacity: .7;
-}
-
-
-
-
-important,
-function,
-bold {
-	font-weight: bold;
-}
-
- */
