40ba4f0ceb635b3f4e454909ddcc3aedb9f44850
Jordan Overbye <jordanoverbye@gmail.com>
2022-10-06 09:51:35 +1000

    remove prose

--- example-site/components/FormExampleMultiStep/FormExampleMultiStepContainer.tsx
+++ example-site/components/FormExampleMultiStep/FormExampleMultiStepContainer.tsx
@@ -2,7 +2,6 @@ import { ReactNode, useEffect, useRef } from 'react';
 import { H1 } from '@ag.ds-next/heading';
 import { Stack } from '@ag.ds-next/box';
 import { PageAlert } from '@ag.ds-next/page-alert';
-import { Prose } from '@ag.ds-next/prose';
 import { Text } from '@ag.ds-next/text';
 import { PageTitle } from '../PageTitle';
 import { useFormExampleMultiStep } from './FormExampleMultiStep';
@@ -49,12 +48,10 @@ export const FormExampleMultiStepContainer = ({
 					tone="warning"
 					title="This section of the form is not ready to be completed"
 				>
-					<Prose>
-						<p>
-							Before starting this part of the form, you will need to go back
-							and complete all of the previous sections.
-						</p>
-					</Prose>
+					<Text as="p">
+						Before starting this part of the form, you will need to go back and
+						complete all of the previous sections.
+					</Text>
 				</PageAlert>
 			)}
 		</Stack>
