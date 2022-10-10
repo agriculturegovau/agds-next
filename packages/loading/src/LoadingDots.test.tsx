0fdb0df5ee2552d87e9bc0209f0d32466be025a1
Nathan Simpson <hello@nathansimpson.design>
2022-10-10 15:16:31 +1100

    Loading: Add tests

---
+++ packages/loading/src/LoadingDots.test.tsx
@@ -0,0 +1,32 @@
+import '@testing-library/jest-dom';
+import 'html-validate/jest';
+import { render, screen } from '@testing-library/react';
+import { LoadingDots, LoadingDotsProps } from './LoadingDots';
+
+function LoadingDotsExample(props: LoadingDotsProps) {
+	return <LoadingDots data-testid="example" label="Busy" {...props} />;
+}
+
+describe('LoadingDots', () => {
+	it('renders correctly with minimal props', () => {
+		render(<LoadingDotsExample />);
+		const el = screen.getByTestId('example');
+		expect(el).toBeInTheDocument();
+		expect(el.tagName).toBe('DIV');
+		expect(el).toHaveAccessibleName('Busy');
+	});
+	it('renders a valid HTML structure', () => {
+		const { container } = render(<LoadingDotsExample />);
+		expect(container).toHTMLValidate({
+			extends: ['html-validate:recommended'],
+			rules: { 'no-inline-style': 'off' },
+		});
+	});
+	it('renders a valid HTML structure with role="status"', () => {
+		const { container } = render(<LoadingDotsExample role="status" />);
+		expect(container).toHTMLValidate({
+			extends: ['html-validate:recommended'],
+			rules: { 'no-inline-style': 'off' },
+		});
+	});
+});
