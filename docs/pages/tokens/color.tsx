b2e86662f919c3626c91376dfd2963af195d0df5
Nathan Simpson <nathan@thinkmill.com.au>
2022-10-13 09:29:25 +1100

    docs: Update tokens page (#769)

--- docs/pages/tokens/color.tsx
+++ docs/pages/tokens/color.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Prose, proseBlockClassname } from '@ag.ds-next/prose';
 import { Switch } from '@ag.ds-next/switch';
 import { Box } from '@ag.ds-next/box';
+import { InpageNav } from '@ag.ds-next/inpage-nav';
 import { DocumentTitle } from '../../components/DocumentTitle';
 import { ColorTable } from '../../components/TokenColorTable';
 import { TokenLayout } from '../../components/TokenLayout';
@@ -14,70 +15,60 @@ export default function TokensColorPage() {
 			<DocumentTitle title="Color tokens" />
 			<TokenLayout
 				title="Color tokens"
-				description="How to do colour"
+				description="How to use colour to design consistent, purposeful, and accessible products."
 				editPath="/docs/pages/tokens/color.tsx"
 			>
 				<Prose>
+					<h2>Colour palettes</h2>
 					<p>
-						The colour palette is designed and tested to provide colour pairings
-						that pass accessibility contrast ratios while still being easy to
-						implement. This means that designers and developers using the system
-						do not need to be concerned about if a colour will pass WCAG
-						requirements in a particular circumstance.
+						There are 2 colour palettes, light and dark. Each colour palette is
+						divided into foreground, background, border, and system colours
+						which all have a specific purpose:
 					</p>
-					<p>
-						We use semantic naming so that all colours labeled as foreground
-						colours in a theme. These colours are manually tested to pass
-						accessibility contrast ratios when used with all background colours
-						within a theme and vice versa.
-					</p>
-
-					<p>
-						Each foreground, background, border, and system colours has a
-						specific purpose...
-					</p>
-
-					<ul>
-						<li>
-							<strong>Foreground colors</strong>: Designed to sit on top of
-							background colours to ensure contrast ratios meet WCAG 2.1 level
-							AA accessibility requirements.
-						</li>
-
-						<li>
-							<strong>Background colors</strong>: Designed to sit under
-							foreground colours to ensure contrast ratios meet WCAG 2.1 level
-							AA accessibility requirements. Each colour palette has 2 main
-							background colours to choose from, the default background (body)
-							and a darker alternative (body-alt). Shades can be used to help
-							differentiate or highlight content against the body background
-							colours.
-						</li>
-
-						<li>
-							<strong>Border colors</strong>: Each colour palette has 2 border
-							colours.
-						</li>
-
-						<li>
-							<strong>System colors</strong>: System colours are used to
-							indicate status. They’re very prominent colours aimed at grabbing
-							the user’s attention. Each system colour has a muted version to be
-							used as a background colour.
-						</li>
-					</ul>
-
 					<div className={proseBlockClassname}>
-						<Switch
-							label="Dark palette"
-							checked={isDarkMode}
-							onChange={setDarkMode}
+						<InpageNav
+							title="In this section"
+							links={[
+								{ href: '#foreground', label: 'Foreground colors' },
+
+								{ href: '#background', label: 'Background colors' },
+
+								{ href: '#border', label: 'Border colors' },
+
+								{ href: '#system', label: 'System colors' },
+							]}
 						/>
 					</div>
-
+					<Box
+						borderTop
+						borderBottom
+						padding={1.5}
+						borderColor="muted"
+						background="shade"
+						className={proseBlockClassname}
+					>
+						<h3>Light vs dark palette</h3>
+						<p>Toggle the dark palette colour tokens via the switch below.</p>
+						<div className={proseBlockClassname}>
+							<Switch
+								label="Enable dark palette"
+								checked={isDarkMode}
+								onChange={setDarkMode}
+							/>
+						</div>
+					</Box>
 					<ColorTable activePalette={isDarkMode ? 'dark' : 'light'} />
-
-					<h3>Tips</h3>
+					<h2>Usage guidelines</h2>
+					<h3>Don’t rely on colour alone to convey meaning</h3>
+					<p>
+						To ensure the colourblind can use our interface, we shouldn’t rely
+						on colour alone to differentiate interface components or convey
+						meaning. Use other visual cues to differentiate interface
+						components. For example, we underline links as well as using the
+						action colour to help differentiate them from other text and clearly
+						indicate they’re interactive.
+					</p>
+					<h3>More tips</h3>
 					<ul>
 						<li>
 							<strong>Do</strong> pair foreground and background colours.
