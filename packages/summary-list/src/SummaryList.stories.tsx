3ef07747ca25329f1fd65c6e0472956e5d4f5009
Nathan Simpson <hello@nathansimpson.design>
2022-10-18 14:41:26 +1100

    update names

--- packages/summary-list/src/SummaryList.stories.tsx
+++ packages/summary-list/src/SummaryList.stories.tsx
@@ -5,9 +5,9 @@ import { Stack } from '@ag.ds-next/box';
 import {
 	SummaryList,
 	SummaryListItem,
-	Term,
-	Description,
-	Action,
+	SummaryListItemTerm,
+	SummaryListItemDescription,
+	SummaryListItemAction,
 } from './SummaryList';
 
 export default {
@@ -18,25 +18,25 @@ export default {
 export const Basic: ComponentStory<typeof SummaryList> = (args) => (
 	<SummaryList {...args}>
 		<SummaryListItem>
-			<Term>First name</Term>
-			<Description>Will</Description>
+			<SummaryListItemTerm>First name</SummaryListItemTerm>
+			<SummaryListItemDescription>Will</SummaryListItemDescription>
 		</SummaryListItem>
 		<SummaryListItem>
-			<Term>Last name</Term>
-			<Description>Power</Description>
+			<SummaryListItemTerm>Last name</SummaryListItemTerm>
+			<SummaryListItemDescription>Power</SummaryListItemDescription>
 		</SummaryListItem>
 		<SummaryListItem>
-			<Term>Contact information</Term>
-			<Description>
+			<SummaryListItemTerm>Contact information</SummaryListItemTerm>
+			<SummaryListItemDescription>
 				<Stack>
 					<Text>+61 9912 3456</Text>
 					<Text>will.power@example.com</Text>
 				</Stack>
-			</Description>
+			</SummaryListItemDescription>
 		</SummaryListItem>
 		<SummaryListItem>
-			<Term>Date of birth</Term>
-			<Description>09/06/1995</Description>
+			<SummaryListItemTerm>Date of birth</SummaryListItemTerm>
+			<SummaryListItemDescription>09/06/1995</SummaryListItemDescription>
 		</SummaryListItem>
 	</SummaryList>
 );
@@ -45,37 +45,37 @@ Basic.args = {};
 export const Updatable: ComponentStory<typeof SummaryList> = (args) => (
 	<SummaryList {...args}>
 		<SummaryListItem>
-			<Term>First name</Term>
-			<Description>Will</Description>
-			<Action>
+			<SummaryListItemTerm>First name</SummaryListItemTerm>
+			<SummaryListItemDescription>Will</SummaryListItemDescription>
+			<SummaryListItemAction>
 				<TextLink href="#">Change</TextLink>
-			</Action>
+			</SummaryListItemAction>
 		</SummaryListItem>
 		<SummaryListItem>
-			<Term>Last name</Term>
-			<Description>Power</Description>
-			<Action>
+			<SummaryListItemTerm>Last name</SummaryListItemTerm>
+			<SummaryListItemDescription>Power</SummaryListItemDescription>
+			<SummaryListItemAction>
 				<TextLink href="#">Change</TextLink>
-			</Action>
+			</SummaryListItemAction>
 		</SummaryListItem>
 		<SummaryListItem>
-			<Term>Contact information</Term>
-			<Description>
+			<SummaryListItemTerm>Contact information</SummaryListItemTerm>
+			<SummaryListItemDescription>
 				<Stack>
 					<Text>+61 9912 3456</Text>
 					<Text>will.power@example.com</Text>
 				</Stack>
-			</Description>
-			<Action>
+			</SummaryListItemDescription>
+			<SummaryListItemAction>
 				<TextLink href="#">Change</TextLink>
-			</Action>
+			</SummaryListItemAction>
 		</SummaryListItem>
 		<SummaryListItem>
-			<Term>Date of birth</Term>
-			<Description>09/06/1995</Description>
-			<Action>
+			<SummaryListItemTerm>Date of birth</SummaryListItemTerm>
+			<SummaryListItemDescription>09/06/1995</SummaryListItemDescription>
+			<SummaryListItemAction>
 				<TextLink href="#">Change</TextLink>
-			</Action>
+			</SummaryListItemAction>
 		</SummaryListItem>
 	</SummaryList>
 );
