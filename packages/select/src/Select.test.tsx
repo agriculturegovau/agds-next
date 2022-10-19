1d0a88d94d740c1cddfb61e5a49d4214d51d808c
Jordan Overbye <6265154+jordanoverbye@users.noreply.github.com>
2022-10-19 14:19:01 +1000

    select: Add basic tests (#785)

---
+++ packages/select/src/Select.test.tsx
@@ -0,0 +1,44 @@
+import '@testing-library/jest-dom';
+import 'html-validate/jest';
+import { cleanup, render } from '@testing-library/react';
+import { Select, SelectProps } from './Select';
+
+afterEach(cleanup);
+
+function renderSelect(props: SelectProps) {
+	return render(<Select {...props} />);
+}
+
+describe('Select', () => {
+	it('renders correctly', () => {
+		const { container } = renderSelect({
+			label: 'Name',
+			placeholder: 'Select an option',
+			options: [
+				{ value: 'a', label: 'Option A' },
+				{ value: 'b', label: 'Option B' },
+				{ value: 'c', label: 'Option C' },
+			],
+		});
+		expect(container).toMatchSnapshot();
+	});
+
+	it('renders a valid HTML structure', () => {
+		const { container } = renderSelect({
+			label: 'Name',
+			placeholder: 'Select an option',
+			options: [
+				{ value: 'a', label: 'Option A' },
+				{ value: 'b', label: 'Option B' },
+				{ value: 'c', label: 'Option C' },
+			],
+		});
+		expect(container).toHTMLValidate({
+			extends: ['html-validate:recommended'],
+			rules: {
+				// react 18s `useId` break this rule
+				'valid-id': 'off',
+			},
+		});
+	});
+});
