f2da67180c34aeda17db275722b9fbfd018e32c3
Jordan Overbye <jordanoverbye@gmail.com>
2022-10-21 08:14:50 +1000

    skeleton

---
+++ packages/skeleton/src/SkeletonBox.test.tsx
@@ -0,0 +1,33 @@
+import '@testing-library/jest-dom';
+import 'html-validate/jest';
+import '@testing-library/jest-dom';
+import 'html-validate/jest';
+import { render, cleanup } from '@testing-library/react';
+import { SkeletonBox, SkeletonBoxProps } from './SkeletonBox';
+
+afterEach(cleanup);
+
+function renderSkeletonBox(props: SkeletonBoxProps) {
+	return render(<SkeletonBox {...props} />);
+}
+
+describe('SkeletonBox', () => {
+	it('renders correctly', () => {
+		const { container } = renderSkeletonBox({
+			width: '200px',
+			height: '200px',
+		});
+		expect(container).toMatchSnapshot();
+	});
+
+	it('renders a valid HTML structure', () => {
+		const { container } = renderSkeletonBox({
+			width: '200px',
+			height: '200px',
+		});
+		expect(container).toHTMLValidate({
+			extends: ['html-validate:recommended'],
+			rules: { 'no-inline-style': 'off' },
+		});
+	});
+});
---
+++ packages/skeleton/src/SkeletonHeading.test.tsx
@@ -0,0 +1,27 @@
+import '@testing-library/jest-dom';
+import 'html-validate/jest';
+import '@testing-library/jest-dom';
+import 'html-validate/jest';
+import { render, cleanup } from '@testing-library/react';
+import { SkeletonHeading, SkeletonHeadingProps } from './SkeletonHeading';
+
+afterEach(cleanup);
+
+function renderSkeletonHeading(props: SkeletonHeadingProps) {
+	return render(<SkeletonHeading {...props} />);
+}
+
+describe('SkeletonHeading', () => {
+	it('renders correctly', () => {
+		const { container } = renderSkeletonHeading({ type: 'h1', width: '100%' });
+		expect(container).toMatchSnapshot();
+	});
+
+	it('renders a valid HTML structure', () => {
+		const { container } = renderSkeletonHeading({ type: 'h1', width: '100%' });
+		expect(container).toHTMLValidate({
+			extends: ['html-validate:recommended'],
+			rules: { 'no-inline-style': 'off' },
+		});
+	});
+});
