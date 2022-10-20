338e78de85ca47e19fc39e8546c778794b9f0125
Jordan Overbye <jordanoverbye@gmail.com>
2022-10-20 13:55:37 +1000

    add badge tests

---
+++ packages/badge/src/StatusBadge.test.tsx
@@ -0,0 +1,36 @@
+import '@testing-library/jest-dom';
+import 'html-validate/jest';
+import { cleanup, render } from '@testing-library/react';
+import { StatusBadge, StatusBadgeProps, StatusBadgeTone } from './StatusBadge';
+
+afterEach(cleanup);
+
+function renderStatusBadge(props?: Partial<StatusBadgeProps>) {
+	return render(<StatusBadge {...props} />);
+}
+
+const tones: StatusBadgeTone[] = [
+	'error',
+	'success',
+	'warning',
+	'info',
+	'neutral',
+];
+
+describe('StatusBadge', () => {
+	tones.forEach((tone) => {
+		describe(`tone: ${tone}`, () => {
+			it('renders correctly', () => {
+				const { container } = renderStatusBadge({ tone, label: `Example` });
+				expect(container).toMatchSnapshot();
+			});
+
+			it('renders a valid HTML structure', () => {
+				const { container } = renderStatusBadge({ tone, label: `Example` });
+				expect(container).toHTMLValidate({
+					extends: ['html-validate:recommended'],
+				});
+			});
+		});
+	});
+});
