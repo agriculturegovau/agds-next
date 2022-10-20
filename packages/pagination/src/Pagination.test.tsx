9a6d63b38b5f0a909ec7b5a6ba42db034682d706
Jordan Overbye <jordanoverbye@gmail.com>
2022-10-20 14:23:24 +1000

    ts

--- packages/pagination/src/Pagination.test.tsx
+++ packages/pagination/src/Pagination.test.tsx
@@ -5,25 +5,26 @@ import { Pagination, PaginationProps } from './Pagination';
 
 afterEach(cleanup);
 
-function renderPagination(props?: Partial<PaginationProps>) {
-	return render(
-		<Pagination
-			currentPage={5}
-			totalPages={10}
-			generateHref={(pageNumber) => `page-${pageNumber}`}
-			{...props}
-		/>
-	);
+function renderPagination(props: PaginationProps) {
+	return render(<Pagination {...props} />);
 }
 
 describe('Pagination', () => {
 	it('renders correctly', () => {
-		const { container } = renderPagination();
+		const { container } = renderPagination({
+			currentPage: 5,
+			totalPages: 10,
+			generateHref: (pageNumber) => `page-${pageNumber}`,
+		});
 		expect(container).toMatchSnapshot();
 	});
 
 	it('renders a valid HTML structure', () => {
-		const { container } = renderPagination();
+		const { container } = renderPagination({
+			currentPage: 5,
+			totalPages: 10,
+			generateHref: (pageNumber) => `page-${pageNumber}`,
+		});
 		expect(container).toHTMLValidate({
 			extends: ['html-validate:recommended'],
 		});
--- packages/pagination/src/PaginationButtons.test.tsx
+++ packages/pagination/src/PaginationButtons.test.tsx
@@ -1,29 +1,30 @@
 import '@testing-library/jest-dom';
 import 'html-validate/jest';
 import { cleanup, render } from '@testing-library/react';
-import { PaginationButtons, PaginationButtonProps } from './PaginationButtons';
+import { PaginationButtons, PaginationButtonsProps } from './PaginationButtons';
 
 afterEach(cleanup);
 
-function renderPaginationButtons(props?: Partial<PaginationButtonProps>) {
-	return render(
-		<PaginationButtons
-			currentPage={5}
-			totalPages={10}
-			generateHref={(pageNumber) => `page-${pageNumber}`}
-			{...props}
-		/>
-	);
+function renderPaginationButtons(props: PaginationButtonsProps) {
+	return render(<PaginationButtons {...props} />);
 }
 
 describe('PaginationButtons', () => {
 	it('renders correctly', () => {
-		const { container } = renderPaginationButtons();
+		const { container } = renderPaginationButtons({
+			currentPage: 5,
+			totalPages: 10,
+			onChange: jest.fn(),
+		});
 		expect(container).toMatchSnapshot();
 	});
 
 	it('renders a valid HTML structure', () => {
-		const { container } = renderPaginationButtons();
+		const { container } = renderPaginationButtons({
+			currentPage: 5,
+			totalPages: 10,
+			onChange: jest.fn(),
+		});
 		expect(container).toHTMLValidate({
 			extends: ['html-validate:recommended'],
 		});
