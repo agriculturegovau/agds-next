a95c967008c9033032031fc6903a1fcae2a333ed
Jordan Overbye <jordanoverbye@gmail.com>
2022-10-21 08:54:30 +1000

    content

---
+++ packages/content/src/SectionContent.test.tsx
@@ -0,0 +1,28 @@
+import '@testing-library/jest-dom';
+import 'html-validate/jest';
+import { cleanup, render } from '@testing-library/react';
+import { SectionContent, SectionContentProps } from './SectionContent';
+
+afterEach(cleanup);
+
+function renderSectionContent(props?: Partial<SectionContentProps>) {
+	return render(
+		<SectionContent {...props}>
+			<p>Section content</p>
+		</SectionContent>
+	);
+}
+
+describe('SectionContent', () => {
+	it('renders correctly', () => {
+		const { container } = renderSectionContent();
+		expect(container).toMatchSnapshot();
+	});
+
+	it('renders a valid HTML structure', () => {
+		const { container } = renderSectionContent();
+		expect(container).toHTMLValidate({
+			extends: ['html-validate:recommended'],
+		});
+	});
+});
