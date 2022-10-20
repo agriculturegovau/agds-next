ea8c01529f9149d3d23d6f92568d5ff176190060
Jordan Overbye <jordanoverbye@gmail.com>
2022-10-20 15:50:54 +1000

    skip links

---
+++ packages/skip-link/src/SkipLinks.test.tsx
@@ -0,0 +1,34 @@
+import '@testing-library/jest-dom';
+import 'html-validate/jest';
+import { cleanup, render } from '@testing-library/react';
+import { SkipLinks, SkipLinksProps } from './SkipLinks';
+
+afterEach(cleanup);
+
+function renderSkipLinks(props: SkipLinksProps) {
+	return render(<SkipLinks {...props} />);
+}
+
+describe('SkipLinks', () => {
+	it('renders correctly', () => {
+		const { container } = renderSkipLinks({
+			links: [
+				{ href: '#main-content', label: 'Skip to main content' },
+				{ href: '#main-nav', label: 'Skip to main navigation' },
+			],
+		});
+		expect(container).toMatchSnapshot();
+	});
+
+	it('renders a valid HTML structure', () => {
+		const { container } = renderSkipLinks({
+			links: [
+				{ href: '#main-content', label: 'Skip to main content' },
+				{ href: '#main-nav', label: 'Skip to main navigation' },
+			],
+		});
+		expect(container).toHTMLValidate({
+			extends: ['html-validate:recommended'],
+		});
+	});
+});
