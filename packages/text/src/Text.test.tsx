318b0bbe05f525485480421959517d43a8eb7ea2
Jordan Overbye <jordanoverbye@gmail.com>
2022-10-20 15:42:28 +1000

    text tests

---
+++ packages/text/src/Text.test.tsx
@@ -0,0 +1,33 @@
+import '@testing-library/jest-dom';
+import 'html-validate/jest';
+import { cleanup, render, screen } from '@testing-library/react';
+import { Text, TextProps } from './Text';
+
+afterEach(cleanup);
+
+function renderText(props: Partial<TextProps>) {
+	return render(<Text {...props}>Text element</Text>);
+}
+
+describe('Text', () => {
+	it('renders correctly', () => {
+		const { container } = renderText({});
+		expect(container).toMatchSnapshot();
+	});
+
+	it('renders a valid HTML structure', () => {
+		const { container } = renderText({});
+		expect(container).toHTMLValidate({
+			extends: ['html-validate:recommended'],
+		});
+	});
+
+	it('can render a paragraph tag using the `as` prop', () => {
+		render(
+			<Text as="p" data-testid="example">
+				Paragraph element
+			</Text>
+		);
+		expect(screen.getByTestId('example').tagName).toBe('P');
+	});
+});
