b4602369382d61e98e21fbd934aca47584833d00
Jordan Overbye <6265154+jordanoverbye@users.noreply.github.com>
2022-10-20 15:16:59 +1000

    Add tests for the `pagination` package (#801)

---
+++ packages/pagination/src/Pagination.test.tsx
@@ -0,0 +1,32 @@
+import '@testing-library/jest-dom';
+import 'html-validate/jest';
+import { cleanup, render } from '@testing-library/react';
+import { Pagination, PaginationProps } from './Pagination';
+
+afterEach(cleanup);
+
+function renderPagination(props: PaginationProps) {
+	return render(<Pagination {...props} />);
+}
+
+describe('Pagination', () => {
+	it('renders correctly', () => {
+		const { container } = renderPagination({
+			currentPage: 5,
+			totalPages: 10,
+			generateHref: (pageNumber) => `page-${pageNumber}`,
+		});
+		expect(container).toMatchSnapshot();
+	});
+
+	it('renders a valid HTML structure', () => {
+		const { container } = renderPagination({
+			currentPage: 5,
+			totalPages: 10,
+			generateHref: (pageNumber) => `page-${pageNumber}`,
+		});
+		expect(container).toHTMLValidate({
+			extends: ['html-validate:recommended'],
+		});
+	});
+});
