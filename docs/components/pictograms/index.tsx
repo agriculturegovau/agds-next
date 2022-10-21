14d7291219972885cdb1186290a97a8b98f8d62b
Nathan Simpson <hello@nathansimpson.design>
2022-10-21 11:49:45 +1100

    Add some pictograms

--- docs/components/pictograms/index.tsx
+++ docs/components/pictograms/index.tsx
@@ -12,17 +12,20 @@ import content from './content.svg';
 import controlInput from './control-input.svg';
 import core from './core.svg';
 import datePicker from './date-picker.svg';
+import details from './details.svg';
 import directionLink from './direction-link.svg';
 import fileUpload from './file-upload.svg';
 import field from './field.svg';
 import fieldset from './fieldset.svg';
 import footer from './footer.svg';
+import formStack from './form-stack.svg';
 import guides from './guides.svg';
 import header from './header.svg';
 import heading from './heading.svg';
 import icon from './icon.svg';
 import inpageNav from './inpage-nav.svg';
 import keywordList from './keyword-list.svg';
+import loading from './loading.svg';
 import linkList from './link-list.svg';
 import mainNav from './main-nav.svg';
 import modal from './modal.svg';
@@ -35,16 +38,19 @@ import select from './select.svg';
 import sideNav from './side-nav.svg';
 import skipLink from './skip-link.svg';
 import starter from './starter.svg';
+import summaryList from './summary-list.svg';
 import switchPictogram from './switch.svg';
 import table from './table.svg';
 import tags from './tags.svg';
 import taskList from './task-list.svg';
 import text from './text.svg';
 import textarea from './textarea.svg';
+import textLink from './text-link.svg';
 import textInput from './text-input.svg';
 
 export const pictograms = {
 	accordion,
+	autocomplete: select,
 	'ag-branding': agBranding,
 	badge,
 	box,
@@ -55,15 +61,18 @@ export const pictograms = {
 	callout,
 	card,
 	columns,
+	combobox: select,
 	content,
 	'control-input': controlInput,
 	core,
 	'direction-link': directionLink,
 	'date-picker': datePicker,
+	details,
 	'file-upload': fileUpload,
 	field,
 	fieldset,
 	footer,
+	'form-stack': formStack,
 	guides,
 	header,
 	heading,
@@ -71,6 +80,7 @@ export const pictograms = {
 	'inpage-nav': inpageNav,
 	'keyword-list': keywordList,
 	'link-list': linkList,
+	loading,
 	'main-nav': mainNav,
 	modal,
 	packages,
@@ -83,11 +93,13 @@ export const pictograms = {
 	'side-nav': sideNav,
 	'skip-link': skipLink,
 	starter,
+	'summary-list': summaryList,
 	switch: switchPictogram,
 	table,
 	'task-list': taskList,
 	tags,
 	text,
+	'text-link': textLink,
 	textarea,
 	'text-input': textInput,
 };
