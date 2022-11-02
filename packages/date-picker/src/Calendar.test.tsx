279f04e02fe0daecea40d236cdedc47f5953f2ba
Nathan Simpson <hello@nathansimpson.design>
2022-11-03 10:00:48 +1100

    June

--- packages/date-picker/src/Calendar.test.tsx
+++ packages/date-picker/src/Calendar.test.tsx
@@ -17,16 +17,16 @@ function renderCalendarSingle(props: CalendarSingleProps) {
 describe('Calendar Single', () => {
 	it('renders correctly', () => {
 		const { container } = renderCalendarSingle({
-			defaultMonth: new Date(2022, 10, 1),
-			selected: new Date(2022, 10, 1),
+			defaultMonth: new Date(2022, 5, 1),
+			selected: new Date(2022, 5, 1),
 		});
 		expect(container).toMatchSnapshot();
 	});
 
 	it('renders a valid HTML structure', () => {
 		const { container } = renderCalendarSingle({
-			defaultMonth: new Date(2022, 10, 1),
-			selected: new Date(2022, 10, 1),
+			defaultMonth: new Date(2022, 5, 1),
+			selected: new Date(2022, 5, 1),
 		});
 		expect(container).toHTMLValidate({
 			extends: ['html-validate:recommended'],
@@ -42,16 +42,16 @@ function renderCalendarRange(props: CalendarRangeProps) {
 describe('Calendar Range', () => {
 	it('renders correctly', () => {
 		const { container } = renderCalendarRange({
-			defaultMonth: new Date(2022, 10, 1),
-			selected: { from: new Date(2022, 10, 1), to: new Date(2022, 10, 7) },
+			defaultMonth: new Date(2022, 5, 1),
+			selected: { from: new Date(2022, 5, 1), to: new Date(2022, 5, 7) },
 		});
 		expect(container).toMatchSnapshot();
 	});
 
 	it('renders a valid HTML structure', () => {
 		const { container } = renderCalendarRange({
-			defaultMonth: new Date(2022, 10, 1),
-			selected: { from: new Date(2022, 10, 1), to: new Date(2022, 10, 7) },
+			defaultMonth: new Date(2022, 5, 1),
+			selected: { from: new Date(2022, 5, 1), to: new Date(2022, 5, 7) },
 		});
 		expect(container).toHTMLValidate({
 			extends: ['html-validate:recommended'],
