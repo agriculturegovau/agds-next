e802b3e78ff29734b29cd03935363fe7f64a5e13
Jordan Overbye <jordanoverbye@gmail.com>
2022-11-28 08:56:40 +1000

    base path

--- example-form/next.config.js
+++ example-form/next.config.js
@@ -5,7 +5,7 @@ const withPreconstruct = require('@preconstruct/next');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	basePath: '/agds-next/example-form',
+	basePath: '/example-form',
 };
 
 module.exports = withPreconstruct(nextConfig);
