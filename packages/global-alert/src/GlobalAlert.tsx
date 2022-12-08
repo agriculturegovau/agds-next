9589a88d20d3580bd6dd7484fcdff88848d18e58
Jordan Overbye <jordanoverbye@gmail.com>
2022-12-08 14:00:47 +1000

    updates with nathan

--- packages/global-alert/src/GlobalAlert.tsx
+++ packages/global-alert/src/GlobalAlert.tsx
@@ -1,7 +1,7 @@
 import { MouseEventHandler, PropsWithChildren } from 'react';
 import { Box, Flex, Stack } from '@ag.ds-next/box';
 import { Button } from '@ag.ds-next/button';
-import { boxPalette, mq } from '@ag.ds-next/core';
+import { boxPalette } from '@ag.ds-next/core';
 import { CloseIcon, WarningFilledIcon } from '@ag.ds-next/icon';
 import { Content } from '@ag.ds-next/content';
 import { Heading } from '@ag.ds-next/heading';
@@ -15,35 +15,8 @@ export type GlobalAlertProps = PropsWithChildren<{
 
 export function GlobalAlert({ children, onDismiss, title }: GlobalAlertProps) {
 	return (
-		<GlobalAlertContainer aria-label={title}>
-			{title ? (
-				<Stack gap={0.5}>
-					<Flex alignItems="center">
-						<GlobalAlertIcon />
-						<Heading as="h2" type="h3">
-							{title}
-						</Heading>
-					</Flex>
-					<GlobalAlertText title={true}>{children}</GlobalAlertText>
-				</Stack>
-			) : (
-				<Flex gap={[0.5, 0]} flexDirection={['column', 'row']}>
-					<GlobalAlertIcon />
-					<GlobalAlertText title={false}>{children}</GlobalAlertText>
-				</Flex>
-			)}
-			{onDismiss ? <GlobalAlertDismissButton onClick={onDismiss} /> : null}
-		</GlobalAlertContainer>
-	);
-}
-
-function GlobalAlertContainer({
-	['aria-label']: ariaLabel,
-	children,
-}: PropsWithChildren<{ 'aria-label'?: string }>) {
-	return (
 		<section
-			aria-label={ariaLabel}
+			aria-label={title}
 			css={{ backgroundColor: boxPalette.systemWarningMuted }}
 		>
 			<Content>
@@ -51,57 +24,38 @@ function GlobalAlertContainer({
 					flexDirection={['column', 'row']}
 					alignItems="flex-start"
 					justifyContent="space-between"
-					gap={[1, 2]}
+					gap={[1, 1.5]}
 					paddingY={1}
 				>
-					{children}
+					<Flex flexDirection={['column', 'row']} gap={[0.5, 1]}>
+						<Box
+							flexShrink={0}
+							css={{ color: boxPalette.systemWarning }}
+							paddingTop={title ? [0, 0.25] : undefined}
+						>
+							<WarningFilledIcon
+								aria-hidden="false"
+								aria-label="Warning"
+								color="inherit"
+								css={{ display: 'block' }}
+							/>
+						</Box>
+						<Stack gap={0.5}>
+							{title ? (
+								<Heading as="h2" type="h3">
+									{title}
+								</Heading>
+							) : null}
+							<Box flexGrow={1}>{children}</Box>
+						</Stack>
+					</Flex>
+					{onDismiss ? (
+						<Button onClick={onDismiss} iconAfter={CloseIcon} variant="text">
+							Close
+						</Button>
+					) : null}
 				</Flex>
 			</Content>
 		</section>
-	);
-}
-
-const COLUMN_WIDTH = '2.5rem';
-
-function GlobalAlertIcon() {
-	return (
-		<Box
-			width={COLUMN_WIDTH}
-			flexShrink={0}
-			css={{ color: boxPalette.systemWarning }}
-		>
-			<WarningFilledIcon
-				aria-hidden="false"
-				aria-label="Warning"
-				color="inherit"
-				css={{ display: 'block' }}
-			/>
-		</Box>
-	);
-}
-
-function GlobalAlertText({
-	children,
-	title,
-}: PropsWithChildren<{ title: boolean }>) {
-	return (
-		<Box
-			flexGrow={1}
-			css={mq({ paddingLeft: [undefined, title ? COLUMN_WIDTH : undefined] })}
-		>
-			{children}
-		</Box>
-	);
-}
-
-function GlobalAlertDismissButton({
-	onClick,
-}: {
-	onClick: MouseEventHandler<HTMLButtonElement>;
-}) {
-	return (
-		<Button onClick={onClick} iconAfter={CloseIcon} variant="text">
-			Close
-		</Button>
 	);
 }
