fbdef3bbda4dcd4a99ca1a557dddd2d3e9807c52
Jordan Overbye <jordanoverbye@gmail.com>
2022-12-09 11:49:55 +1000

    reset applayout

--- docs/components/AppLayout.tsx
+++ docs/components/AppLayout.tsx
@@ -3,8 +3,6 @@ import { Box, Flex } from '@ag.ds-next/box';
 import { SkipLinks } from '@ag.ds-next/skip-link';
 import { SiteHeader } from './SiteHeader';
 import { SiteFooter } from './SiteFooter';
-import { GlobalAlert } from '@ag.ds-next/global-alert';
-import { Text } from '@ag.ds-next/text';
 
 type AppLayoutProps = {
 	children?: ReactNode;
@@ -20,13 +18,6 @@ export const AppLayout = ({ children }: AppLayoutProps) => {
 				]}
 			/>
 			<Flex flexDirection="column" fontFamily="body" minHeight="100vh">
-				<GlobalAlert onDismiss={console.log}>
-					<Text as="p">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce massa
-						nibh, aliquam vel dolor non, gravida porttitor nulla. Pellentesque
-						cursus orci vulputate nibh sagittis blandit.
-					</Text>
-				</GlobalAlert>
 				<SiteHeader />
 				<Box flexGrow={1}>{children}</Box>
 				<SiteFooter />
