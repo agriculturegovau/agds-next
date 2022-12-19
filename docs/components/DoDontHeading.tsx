d21b9605f705617b5e90021229a2270bc034fd55
Jordan Overbye <jordanoverbye@gmail.com>
2022-12-19 15:34:42 +1000

    remove proseBlockClassname

--- docs/components/DoDontHeading.tsx
+++ docs/components/DoDontHeading.tsx
@@ -1,17 +1,20 @@
 import { Flex } from '@ag.ds-next/box';
 import { SuccessFilledIcon, AlertFilledIcon } from '@ag.ds-next/icon';
-import { proseBlockClassname } from '@ag.ds-next/prose';
 
 export const DoHeading = () => (
-	<Flex as="p" gap={0.5} className={proseBlockClassname}>
-		<SuccessFilledIcon color="success" />
-		<strong>Do</strong>
-	</Flex>
+	<p>
+		<Flex as="span" alignItems="center" gap={0.5}>
+			<SuccessFilledIcon color="success" />
+			<strong>Do</strong>
+		</Flex>
+	</p>
 );
 
 export const DontHeading = () => (
-	<Flex as="p" gap={0.5} className={proseBlockClassname}>
-		<AlertFilledIcon color="error" />
-		<strong>Don&apos;t</strong>
-	</Flex>
+	<p>
+		<Flex as="span" alignItems="center" gap={0.5}>
+			<AlertFilledIcon color="error" />
+			<strong>Don&apos;t</strong>
+		</Flex>
+	</p>
 );
