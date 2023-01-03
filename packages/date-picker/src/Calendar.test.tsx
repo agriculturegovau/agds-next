49748d6be874e3a351122c0239743d83a70ae692
Jordan Overbye <6265154+jordanoverbye@users.noreply.github.com>
2023-01-03 10:27:06 +1000

    date-picker: Fix calendar tests (#904)

--- packages/date-picker/src/Calendar.test.tsx
+++ packages/date-picker/src/Calendar.test.tsx
@@ -14,19 +14,32 @@ function renderCalendarSingle(props: CalendarSingleProps) {
 	return render(<CalendarSingle {...props} />);
 }
 
+const dateRange = {
+	from: new Date(2022, 5, 1),
+	to: new Date(2022, 5, 7),
+};
+
 describe('Calendar Single', () => {
 	it('renders correctly', () => {
 		const { container } = renderCalendarSingle({
-			defaultMonth: new Date(2022, 5, 1),
-			selected: new Date(2022, 5, 1),
+			defaultMonth: dateRange.from,
+			selected: dateRange.from,
+			yearRange: {
+				from: dateRange.from.getFullYear() - 10,
+				to: dateRange.from.getFullYear() + 10,
+			},
 		});
 		expect(container).toMatchSnapshot();
 	});
 
 	it('renders a valid HTML structure', () => {
 		const { container } = renderCalendarSingle({
-			defaultMonth: new Date(2022, 5, 1),
-			selected: new Date(2022, 5, 1),
+			defaultMonth: dateRange.from,
+			selected: dateRange.from,
+			yearRange: {
+				from: dateRange.from.getFullYear() - 10,
+				to: dateRange.from.getFullYear() + 10,
+			},
 		});
 		expect(container).toHTMLValidate({
 			extends: ['html-validate:recommended'],
@@ -46,16 +59,24 @@ function renderCalendarRange(props: CalendarRangeProps) {
 describe('Calendar Range', () => {
 	it('renders correctly', () => {
 		const { container } = renderCalendarRange({
-			defaultMonth: new Date(2022, 5, 1),
-			selected: { from: new Date(2022, 5, 1), to: new Date(2022, 5, 7) },
+			defaultMonth: dateRange.from,
+			selected: dateRange,
+			yearRange: {
+				from: dateRange.from.getFullYear() - 10,
+				to: dateRange.from.getFullYear() + 10,
+			},
 		});
 		expect(container).toMatchSnapshot();
 	});
 
 	it('renders a valid HTML structure', () => {
 		const { container } = renderCalendarRange({
-			defaultMonth: new Date(2022, 5, 1),
-			selected: { from: new Date(2022, 5, 1), to: new Date(2022, 5, 7) },
+			defaultMonth: dateRange.from,
+			selected: dateRange,
+			yearRange: {
+				from: dateRange.from.getFullYear() - 10,
+				to: dateRange.from.getFullYear() + 10,
+			},
 		});
 		expect(container).toHTMLValidate({
 			extends: ['html-validate:recommended'],
