03eb707c9d7ee0758cca5ba18bc4a87fd7b65caa
Nathan Simpson <hello@nathansimpson.design>
2023-01-19 15:35:04 +1100

    colour and size

--- packages/react/src/avatar/Avatar.stories.tsx
+++ packages/react/src/avatar/Avatar.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { Stack } from '../box';
+import { Flex, Stack } from '../box';
+import { Text } from '../text';
 import { Avatar } from './Avatar';
 
 export default {
@@ -12,14 +13,52 @@ const Template: ComponentStory<typeof Avatar> = (args) => <Avatar {...args} />;
 export const Basic = Template.bind({});
 Basic.args = {
 	name: 'Oscar Piastri',
+	color: 'default',
+	size: 'md',
 };
 
-export const MultipleExamples = () => {
+export const Sizes: ComponentStory<typeof Avatar> = (args) => {
 	return (
 		<Stack gap={0.5}>
-			<Avatar name="Jordan Overbye" />
-			<Avatar name="Sarah-Jane Ricciardo" />
-			<Avatar name="Admin" />
+			<Avatar {...args} name="Max Merstappen" size="sm" />
+			<Avatar {...args} name="Max Merstappen" size="md" />
+			<Avatar {...args} name="Max Merstappen" size="lg" />
+			<Avatar {...args} name="Max Merstappen" size="xl" />
+			<Avatar {...args} name="Max Merstappen" size="xxl" />
+			<Avatar {...args} name="Max Merstappen" size="xxxl" />
 		</Stack>
 	);
+};
+Sizes.args = {
+	color: 'default',
+};
+
+export const CompositeNames: ComponentStory<typeof Avatar> = (args) => {
+	const names = [
+		'Jordan Overbye',
+		'Sarah-Jane Ricciardo',
+		'Billy Bob Ricciardo',
+		'John Doe-Ray-Mee',
+		'Admin',
+	];
+
+	return (
+		<Stack gap={0.5}>
+			{names.map((name) => (
+				<Flex
+					key={name}
+					gap={0.5}
+					alignItems="center"
+					justifyContent="flex-start"
+				>
+					<Avatar {...args} name={name} />
+					<Text>{name}</Text>
+				</Flex>
+			))}
+		</Stack>
+	);
+};
+CompositeNames.args = {
+	color: 'default',
+	size: 'md',
 };
--- packages/react/src/avatar/Avatar.tsx
+++ packages/react/src/avatar/Avatar.tsx
@@ -2,23 +2,51 @@ import { Flex } from '@ag.ds-next/react/box';
 import { boxPalette, mapSpacing } from '@ag.ds-next/react/core';
 import { getInitialsFromName } from './getInitialsFromName';
 
+const colorMap = {
+	action: boxPalette.foregroundAction,
+	default: boxPalette.foregroundMuted,
+};
+
+const sizeMap = {
+	sm: { fontSize: 'xs', objectSize: mapSpacing(2) },
+	md: { fontSize: 'sm', objectSize: mapSpacing(2.5) },
+	lg: { fontSize: 'md', objectSize: mapSpacing(3) },
+	xl: { fontSize: 'lg', objectSize: mapSpacing(4) },
+	xxl: { fontSize: 'xl', objectSize: mapSpacing(4.5) },
+	xxxl: { fontSize: 'xl', objectSize: mapSpacing(5) },
+};
+
 export type AvatarProps = {
 	name: string;
+	color?: keyof typeof colorMap;
+	size?: keyof typeof sizeMap;
 };
 
-const avatarStyles = {
-	height: mapSpacing(2),
-	width: mapSpacing(2),
-	borderRadius: mapSpacing(2),
-	backgroundColor: boxPalette.foregroundAction,
-	color: boxPalette.backgroundBody,
-};
-
-export function Avatar({ name }: AvatarProps) {
+export function Avatar({
+	name,
+	color: _color = 'default',
+	size = 'md',
+}: AvatarProps) {
 	const initials = getInitialsFromName(name);
+	const color = colorMap[_color];
+	const { objectSize, fontSize } = sizeMap[size];
 
 	return (
-		<Flex alignItems="center" justifyContent="center" css={avatarStyles}>
+		<Flex
+			alignItems="center"
+			justifyContent="center"
+			css={{
+				height: objectSize,
+				width: objectSize,
+				borderRadius: objectSize,
+				borderStyle: 'solid',
+				borderWidth: 1,
+				borderColor: color,
+				color: color,
+			}}
+			fontSize={fontSize}
+			fontWeight="bold"
+		>
 			{initials}
 		</Flex>
 	);
