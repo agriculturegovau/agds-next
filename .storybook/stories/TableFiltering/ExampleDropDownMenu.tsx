e7515d6420abbf88ab4b1e1d4f5d8a5caf4f9a38
Nathan Simpson <hello@nathansimpson.design>
2023-02-17 14:21:32 +1100

    alternate pagination

--- .storybook/stories/TableFiltering/ExampleDropDownMenu.tsx
+++ .storybook/stories/TableFiltering/ExampleDropDownMenu.tsx
@@ -1,7 +1,8 @@
-import { Flex, Stack } from '@ag.ds-next/react/box';
+import { Box, Flex, Stack } from '@ag.ds-next/react/box';
 import { ButtonLink } from '@ag.ds-next/react/button';
 import { PageContent } from '@ag.ds-next/react/content';
 import { PlusIcon } from '@ag.ds-next/react/icon';
+import { Text } from '@ag.ds-next/react/text';
 import { PaginationButtons } from '@ag.ds-next/react/pagination';
 import { Select } from '@ag.ds-next/react/select';
 import { AppShell } from '../../components/ExampleShell';
@@ -11,6 +12,8 @@ import { DashboardTable } from './components/DashboardTable';
 import { FilterMenu } from './components/FilterMenu';
 import { GetDataFilters, GetDataPagination, GetDataSort } from './lib/data';
 import { BusinessForAudit } from './lib/generateBusinessData';
+import { Tags } from '@ag.ds-next/react/tags';
+import { DirectionButton } from '@ag.ds-next/react/direction-link';
 
 type ExampleDropDownMenuProps = {
 	filters: GetDataFilters;
@@ -46,26 +49,56 @@ export const ExampleDropDownMenu = ({
 			<DashboardPageTitle />
 			<PageContent>
 				<Stack gap={2}>
-					<Flex
-						gap={1}
-						justifyContent="space-between"
-						alignItems={{ sm: 'flex-end' }}
-						flexDirection={{ xs: 'column', sm: 'row' }}
-					>
+					<Box>
+						<ButtonLink href="#new" iconBefore={PlusIcon}>
+							New item
+						</ButtonLink>
+					</Box>
+					<Stack gap={0.5}>
 						<Flex
 							gap={1}
-							flexDirection={{ xs: 'column', md: 'row' }}
-							alignItems={{ md: 'flex-end' }}
-							flexWrap="wrap"
+							alignItems={{ sm: 'flex-end' }}
+							flexDirection={{ xs: 'column', sm: 'row' }}
 						>
 							<DashboardSortBySelect
 								sort={sort}
 								setSort={setSort}
 								resetPagination={resetPagination}
 							/>
+							<FilterMenu
+								filters={filters}
+								setFilters={setFilters}
+								resetPagination={resetPagination}
+								resetFilters={resetFilters}
+							/>
+						</Flex>
+						<Tags
+							heading="Active filters"
+							items={[
+								{
+									label: 'Assignee: Daniel Ricciardo',
+									onRemove: console.log,
+								},
+								{
+									label: 'Assignee: Oscar Piastri',
+									onRemove: console.log,
+								},
+								{ label: 'Status: Completed', onRemove: console.log },
+								{ label: 'State: NSW', onRemove: console.log },
+							]}
+						/>
+					</Stack>
+					<DashboardTable
+						data={data}
+						loading={loading}
+						caption={tableCaption}
+						itemsPerPage={pagination.perPage}
+					/>
+
+					{data.length ? (
+						<Flex justifyContent="space-between" alignItems="center">
 							<Select
 								label="Items per page"
-								maxWidth="sm"
 								value={pagination.perPage}
 								onChange={(event) =>
 									setPagination({
@@ -81,31 +114,26 @@ export const ExampleDropDownMenu = ({
 									{ label: '100', value: '100' },
 								]}
 							/>
-							<FilterMenu
-								filters={filters}
-								setFilters={setFilters}
-								resetPagination={resetPagination}
-								resetFilters={resetFilters}
-							/>
+							<Flex gap={1} justifyContent="center">
+								<DirectionButton direction="left">Back</DirectionButton>
+								<Select
+									label="Page"
+									value={pagination.page}
+									onChange={(event) =>
+										setPagination({
+											...pagination,
+											page: parseInt(event.target.value),
+										})
+									}
+									hideOptionalLabel
+									options={Array.from({ length: totalPages }, (_, i) => ({
+										label: `${i + 1}`,
+										value: `${i + 1}`,
+									}))}
+								/>
+								<DirectionButton direction="right">Next</DirectionButton>
+							</Flex>
 						</Flex>
-						<ButtonLink href="#new" iconBefore={PlusIcon}>
-							New item
-						</ButtonLink>
-					</Flex>
-					<DashboardTable
-						data={data}
-						loading={loading}
-						caption={tableCaption}
-						itemsPerPage={pagination.perPage}
-					/>
-					{data.length ? (
-						<Stack>
-							<PaginationButtons
-								currentPage={pagination.page}
-								onChange={(page) => setPagination({ ...pagination, page })}
-								totalPages={totalPages}
-							/>
-						</Stack>
 					) : null}
 				</Stack>
 			</PageContent>
