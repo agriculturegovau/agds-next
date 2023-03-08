25ea9a1ab3fbb36552b64aa631731f0d8292f893
Jordan Overbye <jordanoverbye@gmail.com>
2023-03-08 15:00:01 +1000

    eslint disable

--- packages/react/src/core/utils/useId/useId.ts
+++ packages/react/src/core/utils/useId/useId.ts
@@ -76,6 +76,7 @@ function genId() {
 
 // Workaround for https://github.com/webpack/webpack/issues/14814
 // https://github.com/eps1lon/material-ui/blob/8d5f135b4d7a58253a99ab56dce4ac8de61f5dc1/packages/mui-utils/src/useId.ts#L21
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const maybeReactUseId: undefined | (() => string) = (React as any)[
 	'useId'.toString()
 ];
