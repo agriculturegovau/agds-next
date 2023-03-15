1ba80442409eb3032c6ce858d403840da2f2ccfc
Jordan Overbye <jordanoverbye@gmail.com>
2023-03-15 10:31:41 +1000

    remove modular, update formatting and headings

--- packages/react/src/table/Table.stories.tsx
+++ packages/react/src/table/Table.stories.tsx
@@ -74,7 +74,9 @@ const exampleData = [
 	},
 ];
 
-const Example: ComponentStory<typeof Table> = (args) => {
+const numberFormatter = new Intl.NumberFormat('en-AU');
+
+export const Basic: ComponentStory<typeof Table> = (args) => {
 	return (
 		<TableWrapper>
 			<Table {...args}>
@@ -102,9 +104,11 @@ const Example: ComponentStory<typeof Table> = (args) => {
 						({ location, population, growthYear, growthDecade }, index) => (
 							<tr key={index}>
 								<TableCell>{location}</TableCell>
-								<TableCell textAlign="right">{population}</TableCell>
-								<TableCell textAlign="right">{growthYear}</TableCell>
-								<TableCell textAlign="right">{growthDecade}</TableCell>
+								<TableCell textAlign="right">
+									{numberFormatter.format(population)}
+								</TableCell>
+								<TableCell textAlign="right">{growthYear}%</TableCell>
+								<TableCell textAlign="right">{growthDecade}%</TableCell>
 							</tr>
 						)
 					)}
@@ -114,92 +118,12 @@ const Example: ComponentStory<typeof Table> = (args) => {
 	);
 };
 
-export const Basic: ComponentStory<typeof Table> = (args) => (
-	<Example {...args} />
-);
-
-export const Modular: ComponentStory<typeof Table> = (args) => (
-	<TableWrapper>
-		<Table {...args}>
-			<TableCaption>
-				Population of Australian states and territories, December 2015
-			</TableCaption>
-			<TableHead>
-				<tr>
-					<TableHeader width="50%" scope="col">
-						Location
-					</TableHeader>
-					<TableHeader textAlign="right" scope="col">
-						Population
-					</TableHeader>
-					<TableHeader textAlign="right" scope="col">
-						Change over previous year %
-					</TableHeader>
-					<TableHeader textAlign="right" scope="col">
-						Change over previous decade %
-					</TableHeader>
-				</tr>
-			</TableHead>
-			<TableBody>
-				<tr>
-					<TableCell>New South Wales</TableCell>
-					<TableCell textAlign="right">7,670,700</TableCell>
-					<TableCell textAlign="right">3.1%</TableCell>
-					<TableCell textAlign="right">12.9%</TableCell>
-				</tr>
-				<tr>
-					<TableCell>Victoria</TableCell>
-					<TableCell textAlign="right">5,996,400</TableCell>
-					<TableCell textAlign="right">2.5%</TableCell>
-					<TableCell textAlign="right">9.3%</TableCell>
-				</tr>
-				<tr>
-					<TableCell>Queensland</TableCell>
-					<TableCell textAlign="right">4,808,800</TableCell>
-					<TableCell textAlign="right">1.7%</TableCell>
-					<TableCell textAlign="right">13.3%</TableCell>
-				</tr>
-				<tr>
-					<TableCell>Western Australia</TableCell>
-					<TableCell textAlign="right">2,603,900</TableCell>
-					<TableCell textAlign="right">2.3%</TableCell>
-					<TableCell textAlign="right">11.6%</TableCell>
-				</tr>
-				<tr>
-					<TableCell>South Australia</TableCell>
-					<TableCell textAlign="right">1,702,800</TableCell>
-					<TableCell textAlign="right">2.0%</TableCell>
-					<TableCell textAlign="right">6.8%</TableCell>
-				</tr>
-				<tr>
-					<TableCell>Tasmania</TableCell>
-					<TableCell textAlign="right">517,400</TableCell>
-					<TableCell textAlign="right">4%</TableCell>
-					<TableCell textAlign="right">5.3%</TableCell>
-				</tr>
-				<tr>
-					<TableCell>Northern Territory</TableCell>
-					<TableCell textAlign="right">244,400</TableCell>
-					<TableCell textAlign="right">1.2%</TableCell>
-					<TableCell textAlign="right">4.5%</TableCell>
-				</tr>
-				<tr>
-					<TableCell>Australian Capital Territory</TableCell>
-					<TableCell textAlign="right">393,000</TableCell>
-					<TableCell textAlign="right">2.4%</TableCell>
-					<TableCell textAlign="right">9.6%</TableCell>
-				</tr>
-			</TableBody>
-		</Table>
-	</TableWrapper>
-);
-
 export const WithHeading = () => {
 	return (
 		<Stack gap={1}>
-			<H1 id="table-heading">Applications</H1>
+			<H1 id="table-heading">Population</H1>
 			<Text id="table-description">
-				This list of active establishment registrations
+				Population of Australian states and territories, December 2015.
 			</Text>
 			<TableWrapper>
 				<Table
