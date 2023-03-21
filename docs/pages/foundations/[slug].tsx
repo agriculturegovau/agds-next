cc82366c667a4295c2cb0d60b06c56f3f75a7a14
Jordan Overbye <jordanoverbye@gmail.com>
2023-03-22 08:41:00 +1000

    update foundations

--- docs/pages/foundations/[slug].tsx
+++ docs/pages/foundations/[slug].tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps, InferGetStaticPropsType } from 'next';
 import { MDXRemote } from 'next-mdx-remote';
+import { InpageNav } from '@ag.ds-next/react/inpage-nav';
 import { Prose } from '@ag.ds-next/react/prose';
 import { mdxComponents } from '../../components/mdxComponents';
 import { AppLayout } from '../../components/AppLayout';
@@ -12,11 +13,14 @@ import {
 	getFoundationBreadcrumbs,
 	Foundation,
 } from '../../lib/mdx/foundations';
+import { generateToc } from '../../lib/generateToc';
 
 export default function FoundationsPage({
 	breadcrumbs,
 	foundation,
+	toc,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+	console.log({ toc });
 	return (
 		<>
 			<DocumentTitle
@@ -32,6 +36,12 @@ export default function FoundationsPage({
 						title={foundation.title}
 						introduction={foundation.description}
 					/>
+					{toc?.length > 1 ? (
+						<InpageNav
+							title="On this page"
+							links={toc.map((i) => ({ label: i.title, href: `#${i.slug}` }))}
+						/>
+					) : null}
 					<Prose>
 						<MDXRemote {...foundation.source} components={mdxComponents} />
 					</Prose>
@@ -45,22 +55,25 @@ export const getStaticProps: GetStaticProps<
 	{
 		breadcrumbs: Awaited<ReturnType<typeof getFoundationBreadcrumbs>>;
 		foundation: Foundation;
+		toc: Awaited<ReturnType<typeof generateToc>>;
 	},
 	{ slug: string }
 > = async ({ params }) => {
 	const { slug } = params ?? {};
-
 	const foundation = slug ? await getFoundation(slug) : undefined;
-	const breadcrumbs = slug ? await getFoundationBreadcrumbs(slug) : undefined;
 
-	if (!(foundation && breadcrumbs)) {
+	if (!(foundation && slug)) {
 		return { notFound: true };
 	}
+
+	const breadcrumbs = await getFoundationBreadcrumbs(slug);
+	const toc = await generateToc(foundation.content);
 
 	return {
 		props: {
 			breadcrumbs,
 			foundation,
+			toc,
 		},
 	};
 };
