9704291aaf0669513d1ead288c6eccde95b6de65
Jordan Overbye <jordanoverbye@gmail.com>
2023-03-27 10:48:30 +1000

    update function

--- packages/react/src/field/useScrollToField.ts
+++ packages/react/src/field/useScrollToField.ts
@@ -1,25 +1,42 @@
 import { useCallback, MouseEvent } from 'react';
 
 export function useScrollToField() {
-	return useCallback((event: MouseEvent<HTMLAnchorElement>) => {
-		if (focusTarget(event)) {
-			// Prevent default browser behaviour
-			event.preventDefault();
-		}
-	}, []);
+	return useCallback(
+		(eventOrTargetId: MouseEvent<HTMLAnchorElement> | string) => {
+			const targetId = getScrollTargetId(eventOrTargetId);
+			if (!targetId) return;
+
+			const targetEl = document.getElementById(targetId);
+			if (!targetEl) return;
+
+			scrollAndFocusTarget(targetId, targetEl);
+
+			// Prevent default browser behaviour if user clicked on a lin
+			if (typeof eventOrTargetId !== 'string') {
+				eventOrTargetId.preventDefault();
+			}
+		},
+		[]
+	);
 }
 
-function focusTarget(event: MouseEvent<HTMLAnchorElement>) {
-	const target = event.target;
-	if (!(target instanceof HTMLAnchorElement)) return false;
+function getScrollTargetId(
+	eventOrTargetId: MouseEvent<HTMLAnchorElement> | string
+) {
+	if (typeof eventOrTargetId === 'string') {
+		return eventOrTargetId;
+	}
+
 	// Attempt to the find target ID from the anchor tag href
-	const targetId = getTargetId(event);
-	if (!targetId) return false;
-	// Attempt to find the target element using the target Id
-	const targetEl = document.getElementById(targetId);
-	if (!targetEl) return false;
+	const target = eventOrTargetId.target;
+	if (!(target instanceof HTMLAnchorElement)) return;
+	return target.hash.substring(1);
+}
+
+function scrollAndFocusTarget(targetId: string, targetEl: HTMLElement) {
 	const targetLabel = document.querySelector("label[for='" + targetId + "']");
 	const targetLabelParent = targetLabel?.parentElement;
+
 	if (targetEl.tagName.toLowerCase() === 'div') {
 		// If the target element is a div (e.g. a `ControlGroup`), focus the first child input
 		targetEl.querySelector('input')?.focus();
@@ -33,11 +50,4 @@ function focusTarget(event: MouseEvent<HTMLAnchorElement>) {
 	} else {
 		targetEl.scrollIntoView();
 	}
-	return true;
-}
-
-function getTargetId(event: MouseEvent<HTMLAnchorElement>) {
-	const target = event.target;
-	if (!(target instanceof HTMLAnchorElement)) return;
-	return target.hash.substring(1);
 }
