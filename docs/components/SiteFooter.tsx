6b4769bfedd8a58766405a793ce17fc12a8ae953
Nathan Simpson <hello@nathansimpson.design>
2023-04-03 10:45:18 +1000

    docs: Add link lists in site footer

--- docs/components/SiteFooter.tsx
+++ docs/components/SiteFooter.tsx
@@ -1,31 +1,41 @@
 import { useMemo } from 'react';
 import { Footer, FooterDivider } from '@ag.ds-next/react/footer';
 import { Text } from '@ag.ds-next/react/text';
-import { LinkList } from '@ag.ds-next/react/link-list';
-import { Box } from '@ag.ds-next/react/box';
+import { LinkList, LinkListLink } from '@ag.ds-next/react/link-list';
+import { Box, Stack } from '@ag.ds-next/react/box';
 import { tokens } from '@ag.ds-next/react/core';
+import { Column, Columns } from '@ag.ds-next/react/columns';
+import { Heading } from '@ag.ds-next/react/heading';
+import { TextLink } from '@ag.ds-next/react/text-link';
 
-const footerLinks = [
-	{ label: 'Home', href: '/' },
+const aboutLinks: LinkListLink[] = [
 	{ label: 'About', href: '/about' },
 	{ label: 'Roadmap', href: '/roadmap' },
+	{
+		label: 'Github',
+		href: 'https://github.com/steelthreads/agds-next',
+	},
+	{
+		label: 'Exports Service',
+		href: 'https://exports.agriculture.gov.au/',
+	},
 	{
 		label: 'Privacy',
 		href: 'https://www.agriculture.gov.au/about/commitment/privacy',
 	},
-	{
-		label: 'Storybook',
-		href: process.env.NEXT_PUBLIC_STORYBOOK_URL,
-	},
-	{
-		label: 'Playroom',
-		href: process.env.NEXT_PUBLIC_PLAYROOM_URL,
-	},
-	{
-		label: 'Starter kit',
-		href: 'https://github.com/steelthreads/agds-starter-kit',
-	},
 ];
+
+const toolLinks: LinkListLink[] = [
+	{ label: 'Figma', href: '' },
+	{ label: 'Storybook', href: process.env.NEXT_PUBLIC_STORYBOOK_URL as string },
+	{ label: 'Playroom', href: process.env.NEXT_PUBLIC_PLAYROOM_URL as string },
+];
+
+const columnWidths = {
+	xs: 12,
+	sm: 4,
+	lg: 3,
+} as const;
 
 export const SiteFooter = () => {
 	const year = useMemo(() => new Date().getFullYear(), []);
@@ -33,7 +43,38 @@ export const SiteFooter = () => {
 		<Box palette="dark">
 			<Footer background="bodyAlt">
 				<nav aria-label="footer">
-					<LinkList links={footerLinks} horizontal />
+					<Columns>
+						<Column columnSpan={columnWidths}>
+							<Stack gap={0.5}>
+								<Heading as="h2" type="h3">
+									About
+								</Heading>
+								<LinkList links={aboutLinks} />
+							</Stack>
+						</Column>
+						<Column columnSpan={columnWidths}>
+							<Stack gap={0.5}>
+								<Heading as="h2" type="h3">
+									Tools
+								</Heading>
+								<LinkList links={toolLinks} />
+							</Stack>
+						</Column>
+						<Column columnSpan={columnWidths}>
+							<Stack gap={0.5}>
+								<Heading as="h2" type="h3">
+									Section
+								</Heading>
+								<LinkList
+									links={[
+										{ href: '#', label: 'Link 1' },
+										{ href: '#', label: 'Link 2' },
+										{ href: '#', label: 'Link 3' },
+									]}
+								/>
+							</Stack>
+						</Column>
+					</Columns>
 				</nav>
 				<FooterDivider />
 				<Text fontSize="xs" maxWidth={tokens.maxWidth.bodyText}>
@@ -42,7 +83,11 @@ export const SiteFooter = () => {
 					We pay our respects to their Elders past, present and emerging.
 				</Text>
 				<Text fontSize="xs" maxWidth={tokens.maxWidth.bodyText}>
-					&copy; {year} Department of Agriculture, Fisheries and Forestry
+					&copy; {year} Department of Agriculture, Fisheries and Forestry.{' '}
+					<TextLink href="https://www.agriculture.gov.au/about/commitment/privacy">
+						Privacy Policy
+					</TextLink>
+					.
 				</Text>
 			</Footer>
 		</Box>
