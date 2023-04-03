475c9f0877ba255dae8b493008950a8068c10f59
Jordan Overbye <jordanoverbye@gmail.com>
2023-04-03 10:48:50 +1000

    lint

--- .storybook/components/PageTemplate.tsx
+++ .storybook/components/PageTemplate.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, ReactNode } from 'react';
+import { PropsWithChildren } from 'react';
 import { Logo } from '@ag.ds-next/react/ag-branding';
 import { Box, Stack } from '@ag.ds-next/react/box';
 import { tokens } from '@ag.ds-next/react/core';
