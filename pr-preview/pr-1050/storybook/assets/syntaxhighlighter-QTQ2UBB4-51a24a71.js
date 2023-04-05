c84ad1a6fa0b03e7bc669dd65b74809a0dccd313
nathsimpson <nathsimpson@users.noreply.github.com>
2023-04-05 07:47:19 +0000

    Remove preview for PR 1050 🛬

--- pr-preview/pr-1050/storybook/assets/syntaxhighlighter-QTQ2UBB4-51a24a71.js
+++
@@ -1,2 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-7FRJXR4N-84de7825.js";import"./iframe-5563b91e.js";import"../sb-preview/runtime.mjs";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
-//# sourceMappingURL=syntaxhighlighter-QTQ2UBB4-51a24a71.js.map
