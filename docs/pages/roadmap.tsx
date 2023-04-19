ac2163d7f26af34f4e023d0a9e41da3e98d4f130
Jordan Overbye <6265154+jordanoverbye@users.noreply.github.com>
2023-04-19 17:12:08 +1000

    docs: Update roadmap (#1070)

--- docs/pages/roadmap.tsx
+++ docs/pages/roadmap.tsx
@@ -23,27 +23,21 @@ export default function RoadmapPage() {
 				>
 					<PageTitle
 						title="Roadmap"
-						introduction="Our roadmap is always evolving with the needs of our product teams."
+						introduction="Our roadmap is always evolving with the needs of our product teams. This page should be used as a general guide to see the big pieces
+						of work we are currently looking at, or almost ready to start."
 					/>
 					<Prose>
-						<p>
-							This page should be used as a general guide to see the big pieces
-							of work we are currently looking at, or almost ready to start.
-						</p>
-
 						<h2>Work in progress</h2>
 						<p>We are currently working on the following features:</p>
 
 						<ul>
 							<li>Table filtering and sort patterns</li>
-							<li>Multi-select patterns</li>
 							<li>DatePicker and DateRangePicker improvements</li>
 							<li>Application shell for Export Service authenticated space</li>
-							<li>Improving SideNav design</li>
 						</ul>
 
 						<h2>Coming up soon</h2>
-						<p>These features are in the pipeline for the coming weeks:</p>
+						<p>These features are in the pipeline for the near future:</p>
 						<ul>
 							<li>DateInput component</li>
 							<li>Content guidance on the AgDS Website</li>
@@ -57,24 +51,32 @@ export default function RoadmapPage() {
 						</p>
 						<ul>
 							<li>
+								<TextLink href="/updates/2023-01-09-beta">
+									Beta release
+								</TextLink>
+							</li>
+							<li>
 								<TextLink href="/components/avatar">Avatar</TextLink>
 							</li>
 							<li>
 								<TextLink href="/components/file-input">File input</TextLink>
 							</li>
 							<li>
-								<TextLink href="/updates/2023-01-09-beta">
-									Our Beta release
+								<TextLink href="/patterns/selecting-multiple-options">
+									Multi-select pattern
+								</TextLink>
+							</li>
+							<li>
+								<TextLink href="/components/side-nav">
+									Updated Side nav design
 								</TextLink>
 							</li>
 						</ul>
-
 						<p>
-							For more detailed release notes, see{' '}
-							<TextLink href="/updates">Updates</TextLink>.
+							For more detailed release notes,{' '}
+							<TextLink href="/updates">view updates</TextLink>.
 						</p>
-
-						<p>Last updated March 8th 2023.</p>
+						<p>Last updated April 19th 2023.</p>
 					</Prose>
 				</PageLayout>
 			</AppLayout>
