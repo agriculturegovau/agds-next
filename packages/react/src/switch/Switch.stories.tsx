9be20b147a1788d0a6a9f5cc196b4ea87fc719c9
Jordan Overbye <jordanoverbye@gmail.com>
2023-04-27 12:38:26 +1000

    update Switch

--- packages/react/src/switch/Switch.stories.tsx
+++ packages/react/src/switch/Switch.stories.tsx
@@ -1,23 +1,23 @@
-import { StoryObj, Meta, StoryFn } from '@storybook/react';
+import { StoryObj, Meta } from '@storybook/react';
 import { useState } from 'react';
 import { Switch } from './Switch';
 
 const meta: Meta<typeof Switch> = {
 	title: 'forms/Switch',
 	component: Switch,
+	render: function Render(props) {
+		const [isChecked, setChecked] = useState(false);
+		return <Switch {...props} checked={isChecked} onChange={setChecked} />;
+	},
 };
 
 export default meta;
 
-const Template: StoryFn<typeof Switch> = (args) => {
-	const [isChecked, setChecked] = useState(false);
-	return <Switch {...args} checked={isChecked} onChange={setChecked} />;
-};
+type Story = StoryObj<typeof Switch>;
 
-export const Basic: StoryObj<typeof Switch> = {
+export const Basic: Story = {
 	args: {
 		size: 'md',
 		label: 'Show establishments',
 	},
-	render: Template,
 };
