c28af6c02f15608d4586fa8ef6e95ff1f9305a63
Nathan Simpson <hello@nathansimpson.design>
2023-04-27 11:32:18 +1000

    core

--- packages/react/src/core/utils/usePrefersReducedMotion.ts
+++ packages/react/src/core/utils/usePrefersReducedMotion.ts
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 
+/** @deprecated */
 export function usePrefersReducedMotion() {
 	const [prefersReducedMotion, setPrefersReducedMotion] = useState(false);
 
