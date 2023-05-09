f061e525307982a1ca884d5ed58b32460add455f
Nathan Simpson <hello@nathansimpson.design>
2023-05-09 22:39:39 +1000

    WIP: Author how to write guidance content

---
+++ docs/pages/guides/how-to-write-guidance/[slug].tsx
@@ -0,0 +1,96 @@
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import { MDXRemote } from 'next-mdx-remote';
+import { InpageNav } from '@ag.ds-next/react/inpage-nav';
+import { Prose } from '@ag.ds-next/react/prose';
+import { Guide } from '../../../lib/mdx/guides';
+import {
+	getNestedGuide,
+	getNestedGuideSlugs,
+	getNestedGuidesBreadcrumbs,
+	getNestedGuidesNavLinks,
+} from '../../../lib/mdx/guidesNested';
+import { mdxComponents } from '../../../components/mdxComponents';
+import { AppLayout } from '../../../components/AppLayout';
+import { DocumentTitle } from '../../../components/DocumentTitle';
+import { PageLayout } from '../../../components/PageLayout';
+import { PageTitle } from '../../../components/PageTitle';
+import { generateToc } from '../../../lib/generateToc';
+
+export default function Guides({
+	guide,
+	toc,
+	breadcrumbs,
+	navLinks,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+	return (
+		<>
+			<DocumentTitle title={guide.title} description={guide.opener} />
+			<AppLayout applyMainElement={false}>
+				<PageLayout
+					applyMainElement={true}
+					sideNav={{
+						title: 'How to write guidance',
+						titleLink: '/guides/how-to-write-guidance',
+						items: navLinks,
+					}}
+					editPath={`/docs/content/guides/how-to-write-guidance/${guide.slug}.mdx`}
+					breadcrumbs={breadcrumbs}
+				>
+					<PageTitle title={guide.title} introduction={guide.opener} />
+					{toc?.length > 1 ? (
+						<InpageNav
+							title="On this page"
+							links={toc.map((i) => ({ label: i.title, href: `#${i.slug}` }))}
+						/>
+					) : null}
+					<Prose>
+						<MDXRemote {...guide.source} components={mdxComponents} />
+					</Prose>
+				</PageLayout>
+			</AppLayout>
+		</>
+	);
+}
+
+export const getStaticProps: GetStaticProps<
+	{
+		guide: Guide;
+		breadcrumbs: Awaited<ReturnType<typeof getNestedGuidesBreadcrumbs>>;
+		navLinks: Awaited<ReturnType<typeof getNestedGuidesNavLinks>>;
+		toc: Awaited<ReturnType<typeof generateToc>>;
+	},
+	{ slug: string }
+> = async ({ params }) => {
+	const { slug } = params ?? {};
+	const guide = slug ? await getNestedGuide(slug) : undefined;
+
+	if (!(slug && guide)) {
+		return { notFound: true };
+	}
+
+	const toc = await generateToc(guide.content);
+	const breadcrumbs = await getNestedGuidesBreadcrumbs(slug);
+	const navLinks = await getNestedGuidesNavLinks();
+
+	return {
+		props: {
+			guide,
+			breadcrumbs,
+			navLinks,
+			slug,
+			toc,
+		},
+	};
+};
+
+export const getStaticPaths = async () => {
+	const slugs = await getNestedGuideSlugs();
+	console.log({ slugs });
+
+	return {
+		paths: slugs.map((slug) => ({
+			params: { slug },
+		})),
+		fallback: false,
+	};
+};
