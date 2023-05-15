eef0eb9581f9073710596e9920311f9411efe585
Jordan Overbye <jordanoverbye@gmail.com>
2023-05-15 15:34:52 +1000

    update storybook format

--- packages/react/src/tags/Tags.stories.tsx
+++ packages/react/src/tags/Tags.stories.tsx
@@ -1,12 +1,13 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 import { Text } from '../text';
 import { Tags, TagsContainer, TagsList, Tag } from './index';
 
-export default {
+const meta: Meta<typeof Tags> = {
 	title: 'content/Tags',
 	component: Tags,
-	subcomponents: { TagsContainer, TagsList, Tag },
-} as ComponentMeta<typeof Tags>;
+};
+
+export default meta;
 
 const exampleItems = [{ label: 'Foo' }, { label: 'Bar' }, { label: 'Baz' }];
 
@@ -16,40 +17,43 @@ const exampleLinks = [
 	{ href: '#', label: 'Baz' },
 ];
 
-const Template: ComponentStory<typeof Tags> = (args) => <Tags {...args} />;
+type Story = StoryObj<typeof Tags>;
 
-export const Basic = Template.bind({});
-Basic.args = {
-	heading: (
-		<Text as="h2" fontWeight="bold">
-			Tags:
-		</Text>
-	),
-	items: exampleItems,
+export const Basic: Story = {
+	args: {
+		heading: (
+			<Text as="h2" fontWeight="bold">
+				Tags:
+			</Text>
+		),
+		items: exampleItems,
+	},
 };
 
-export const Links = Template.bind({});
-Links.args = {
-	heading: (
-		<Text as="h2" fontWeight="bold">
-			Tags:
-		</Text>
-	),
-	items: exampleLinks,
+export const Links: Story = {
+	args: {
+		heading: (
+			<Text as="h2" fontWeight="bold">
+				Tags:
+			</Text>
+		),
+		items: exampleLinks,
+	},
 };
 
-export const Removable = Template.bind({});
-Removable.args = {
-	heading: (
-		<Text as="h2" fontWeight="bold">
-			Tags:
-		</Text>
-	),
-	items: [
-		{ label: 'Foo', onRemove: console.log },
-		{ label: 'Bar', onRemove: console.log },
-		{ label: 'Baz', onRemove: console.log },
-	],
+export const Removable: Story = {
+	args: {
+		heading: (
+			<Text as="h2" fontWeight="bold">
+				Tags:
+			</Text>
+		),
+		items: [
+			{ label: 'Foo', onRemove: console.log },
+			{ label: 'Bar', onRemove: console.log },
+			{ label: 'Baz', onRemove: console.log },
+		],
+	},
 };
 
 export const Modular = () => (
@@ -58,9 +62,11 @@ export const Modular = () => (
 			Tags:
 		</Text>
 		<TagsList>
-			<Tag href="#">Foo</Tag>
-			<Tag href="#">Bar</Tag>
-			<Tag href="#">Baz</Tag>
+			{exampleLinks.map(({ href, label }, idx) => (
+				<Tag key={idx} href={href}>
+					{label}
+				</Tag>
+			))}
 		</TagsList>
 	</TagsContainer>
 );
