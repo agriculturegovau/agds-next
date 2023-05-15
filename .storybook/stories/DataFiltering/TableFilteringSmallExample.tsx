d0cd4b8ca59dc45da1c639adf33d22f131ca89ba
Jordan Overbye <jordanoverbye@gmail.com>
2023-05-15 11:11:56 +1000

    move stories

--- .storybook/stories/DataFiltering/TableFilteringSmallExample.tsx
+++ .storybook/stories/DataFiltering/TableFilteringSmallExample.tsx
@@ -1,21 +1,8 @@
-import { Meta, StoryObj } from '@storybook/react';
 import { TableFilteringSmall } from './TableFilteringSmall';
 import { useSortAndFilter } from './lib/useSortAndFilter';
 import { generateTableCaption, useData } from './lib/utils';
 
-const meta: Meta<typeof TableFilteringSmall> = {
-	title: 'Patterns/Data filtering and sorting',
-	component: TableFilteringSmall,
-	parameters: {
-		layout: 'fullscreen',
-	},
-};
-
-export default meta;
-
-type Story = StoryObj<typeof TableFilteringSmall>;
-
-const TableFilteringSmallExample = () => {
+export const TableFilteringSmallExample = () => {
 	const { sort, filters, pagination, setSort, setFilters, setPagination } =
 		useSortAndFilter();
 
@@ -46,9 +33,4 @@ const TableFilteringSmallExample = () => {
 			totalItems={totalItems}
 		/>
 	);
-};
-
-export const Small: Story = {
-	name: 'Tables (Small)',
-	render: TableFilteringSmallExample,
 };
