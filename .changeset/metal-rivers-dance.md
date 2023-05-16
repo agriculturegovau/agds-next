7956c675a8ff951b2f5c1bfe23422becb8f7c2fc
Jordan Overbye <6265154+jordanoverbye@users.noreply.github.com>
2023-05-16 10:22:57 +1000

    tags: Update wrapping behaviour (#1116)

---
+++ .changeset/metal-rivers-dance.md
@@ -0,0 +1,5 @@
+---
+'@ag.ds-next/react': patch
+---
+
+tags: Enabled wrapping behaviour to allow tags to flow across multiple lines.
--- packages/react/src/tags/Tags.stories.tsx
+++ packages/react/src/tags/Tags.stories.tsx
@@ -1,12 +1,21 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
+import { Box } from '../box';
 import { Text } from '../text';
 import { Tags, TagsContainer, TagsList, Tag } from './index';
 
-export default {
+const meta: Meta<typeof Tags> = {
 	title: 'content/Tags',
 	component: Tags,
-	subcomponents: { TagsContainer, TagsList, Tag },
-} as ComponentMeta<typeof Tags>;
+	args: {
+		heading: (
+			<Text as="h2" fontWeight="bold">
+				Tags:
+			</Text>
+		),
+	},
+};
+
+export default meta;
 
 const exampleItems = [{ label: 'Foo' }, { label: 'Bar' }, { label: 'Baz' }];
 
@@ -16,40 +25,28 @@ const exampleLinks = [
 	{ href: '#', label: 'Baz' },
 ];
 
-const Template: ComponentStory<typeof Tags> = (args) => <Tags {...args} />;
+type Story = StoryObj<typeof Tags>;
 
-export const Basic = Template.bind({});
-Basic.args = {
-	heading: (
-		<Text as="h2" fontWeight="bold">
-			Tags:
-		</Text>
-	),
-	items: exampleItems,
+export const Basic: Story = {
+	args: {
+		items: exampleItems,
+	},
 };
 
-export const Links = Template.bind({});
-Links.args = {
-	heading: (
-		<Text as="h2" fontWeight="bold">
-			Tags:
-		</Text>
-	),
-	items: exampleLinks,
+export const Links: Story = {
+	args: {
+		items: exampleLinks,
+	},
 };
 
-export const Removable = Template.bind({});
-Removable.args = {
-	heading: (
-		<Text as="h2" fontWeight="bold">
-			Tags:
-		</Text>
-	),
-	items: [
-		{ label: 'Foo', onRemove: console.log },
-		{ label: 'Bar', onRemove: console.log },
-		{ label: 'Baz', onRemove: console.log },
-	],
+export const Removable: Story = {
+	args: {
+		items: [
+			{ label: 'Foo', onRemove: console.log },
+			{ label: 'Bar', onRemove: console.log },
+			{ label: 'Baz', onRemove: console.log },
+		],
+	},
 };
 
 export const Modular = () => (
@@ -58,9 +55,11 @@ export const Modular = () => (
 			Tags:
 		</Text>
 		<TagsList>
-			<Tag href="#">Foo</Tag>
-			<Tag href="#">Bar</Tag>
-			<Tag href="#">Baz</Tag>
+			{exampleLinks.map(({ href, label }, idx) => (
+				<Box key={idx} as="li">
+					<Tag href={href}>{label}</Tag>
+				</Box>
+			))}
 		</TagsList>
 	</TagsContainer>
 );
