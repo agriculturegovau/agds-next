20fd43baa644c42aff25f44ee50478f7ec8abbfd
jordanoverbye <jordanoverbye@users.noreply.github.com>
2023-05-16 06:43:45 +0000

    Remove preview for PR 1120 🛬

--- pr-preview/pr-1120/storybook/assets/syntaxhighlighter-QTQ2UBB4-8f624ed4.js
+++
@@ -1,2 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-7FRJXR4N-cc62c4c8.js";import"./iframe-d05b2f9e.js";import"../sb-preview/runtime.mjs";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
-//# sourceMappingURL=syntaxhighlighter-QTQ2UBB4-8f624ed4.js.map
