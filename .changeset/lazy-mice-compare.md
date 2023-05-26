67f8b4f102d288c73d13b9b87df1eb31ec59c30c
Jordan Overbye <jordanoverbye@gmail.com>
2023-05-26 12:13:03 +1000

    changeset

---
+++ .changeset/lazy-mice-compare.md
@@ -0,0 +1,17 @@
+---
+'@ag.ds-next/react': minor
+---
+
+`IndicatorDot`, `NotificationBadge` and `StatusBadge` has been moved into their own entrypoints. 
+
+These components will continue to work out of the the `@ag.ds-next/react/badge` entrypoint, but this usage has been marked as deprecated and will be removed in the next major release.
+
+To upgrade, simply update the import when using these components.
+
+```diff
+- import { IndicatorDot, NotificationBadge, StatusBadge } from '@ag.ds-next/react/badge';
++ import { IndicatorDot } from '@ag.ds-next/react/indicator-dot'`
++ import { NotificationBadge } from '@ag.ds-next/react/notification-badge'`
++ import { StatusBadge } from '@ag.ds-next/react/status-badge'`
+```
+
--- packages/react/src/badge/index.ts
+++ packages/react/src/badge/index.ts
@@ -3,21 +3,24 @@ import { NotificationBadge as AgDsNotificationBadge } from '../notification-badg
 import { StatusBadge as AgDsStatusBadge } from '../status-badge';
 
 /**
- * @deprecated Use `IndicatorDot` from the `indicator-dot` entrypoint.
+ * @deprecated This component has been moved.
+ * Use `IndicatorDot` from the `indicator-dot` entrypoint instead.
  * Example: `import { IndicatorDot } from '@ag.ds-next/react/indicator-dot'`".
  * This will be removed in the next major version.
  */
 export const IndicatorDot = AgDsIndicatorDot;
 
 /**
- * @deprecated Use `NotificationBadge` from the `notification-badge` entrypoint.
+ * @deprecated This component has been moved.
+ * Use `NotificationBadge` from the `notification-badge` entrypoint instead.
  * Example: `import { NotificationBadge } from '@ag.ds-next/react/notification-badge'`".
  * This will be removed in the next major version.
  */
 export const NotificationBadge = AgDsNotificationBadge;
 
 /**
- * @deprecated Use `StatusBadge` from the `status-badge` entrypoint.
+ * @deprecated This component has been moved.
+ * Use `StatusBadge` from the `status-badge` entrypoint instead.
  * Example: `import { StatusBadge } from '@ag.ds-next/react/status-badge'`".
  * This will be removed in the next major version.
  */
