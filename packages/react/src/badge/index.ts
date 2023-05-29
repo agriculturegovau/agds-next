7a0afbe9e3c738e0cd7f2a7728b9364915979062
Jordan Overbye <6265154+jordanoverbye@users.noreply.github.com>
2023-05-29 12:28:42 +1000

    badge: Re-export and deprecate types (#1164)

--- packages/react/src/badge/index.ts
+++ packages/react/src/badge/index.ts
@@ -1,6 +1,15 @@
-import { IndicatorDot as _IndicatorDot } from '../indicator-dot';
-import { NotificationBadge as _NotificationBadge } from '../notification-badge';
-import { StatusBadge as _StatusBadge } from '../status-badge';
+import {
+	IndicatorDot as _IndicatorDot,
+	IndicatorDotProps as _IndicatorDotProps,
+} from '../indicator-dot';
+import {
+	NotificationBadge as _NotificationBadge,
+	NotificationBadgeProps as _NotificationBadgeProps,
+} from '../notification-badge';
+import {
+	StatusBadge as _StatusBadge,
+	StatusBadgeProps as _StatusBadgeProps,
+} from '../status-badge';
 
 /**
  * @deprecated This component has been moved.
@@ -11,6 +20,14 @@ import { StatusBadge as _StatusBadge } from '../status-badge';
 export const IndicatorDot = _IndicatorDot;
 
 /**
+ * @deprecated This type has been moved.
+ * Use `IndicatorDotProps` from the `indicator-dot` entrypoint instead.
+ * Example: `import { IndicatorDotProps } from '@ag.ds-next/react/indicator-dot'`".
+ * This will be removed in the next major version.
+ */
+export type IndicatorDotProps = _IndicatorDotProps;
+
+/**
  * @deprecated This component has been moved.
  * Use `NotificationBadge` from the `notification-badge` entrypoint instead.
  * Example: `import { NotificationBadge } from '@ag.ds-next/react/notification-badge'`".
@@ -19,9 +36,25 @@ export const IndicatorDot = _IndicatorDot;
 export const NotificationBadge = _NotificationBadge;
 
 /**
+ * @deprecated This type has been moved.
+ * Use `NotificationBadgeProps` from the `indicator-dot` entrypoint instead.
+ * Example: `import { NotificationBadgeProps } from '@ag.ds-next/react/indicator-dot'`".
+ * This will be removed in the next major version.
+ */
+export type NotificationBadgeProps = _NotificationBadgeProps;
+
+/**
  * @deprecated This component has been moved.
  * Use `StatusBadge` from the `status-badge` entrypoint instead.
  * Example: `import { StatusBadge } from '@ag.ds-next/react/status-badge'`".
  * This will be removed in the next major version.
  */
 export const StatusBadge = _StatusBadge;
+
+/**
+ * @deprecated This type has been moved.
+ * Use `StatusBadgeProps` from the `status-badge` entrypoint instead.
+ * Example: `import { StatusBadgeProps } from '@ag.ds-next/react/status-badge'`".
+ * This will be removed in the next major version.
+ */
+export type StatusBadgeProps = _StatusBadgeProps;
