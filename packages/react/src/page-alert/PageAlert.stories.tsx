768b98090e702b543fe8fa6a8f480aad4d9dba7c
Nathan Simpson <nathan@thinkmill.com.au>
2023-06-06 09:59:40 +1000

    page-alert: Add support for React element in title (#1177)
    * Page alert: Added support for element in title prop
    
    * Update Stories to SB7 format
    
    * document
    
    * Update tests
    
    ---------
    
    Co-authored-by: Jordan Overbye <6265154+jordanoverbye@users.noreply.github.com>

--- packages/react/src/page-alert/PageAlert.stories.tsx
+++ packages/react/src/page-alert/PageAlert.stories.tsx
@@ -1,38 +1,56 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 import { Text } from '../text';
 import { Prose } from '../prose';
 import { PageAlert } from './PageAlert';
+import { PageAlertTitle } from './PageAlertTitle';
 
-export default {
+const meta: Meta<typeof PageAlert> = {
 	title: 'content/PageAlert',
 	component: PageAlert,
-} as ComponentMeta<typeof PageAlert>;
-
-export const Basic: ComponentStory<typeof PageAlert> = (args) => (
-	<PageAlert {...args}>
-		<Text as="p">This is a Page alert component.</Text>
-	</PageAlert>
-);
-Basic.args = {
-	title: 'Page alert',
-	tone: 'success',
+	render: (args) => (
+		<PageAlert {...args}>
+			<Text as="p">This is a Page alert component.</Text>
+		</PageAlert>
+	),
 };
 
-export const WithProse: ComponentStory<typeof PageAlert> = (args) => (
-	<PageAlert {...args}>
-		<Prose>
-			<ul>
-				<li>
-					<a href="#">Full name must not be empty</a>
-				</li>
-				<li>
-					<a href="#">Phone number must not be empty</a>
-				</li>
-			</ul>
-		</Prose>
-	</PageAlert>
-);
-WithProse.args = {
-	title: 'Page alert',
-	tone: 'error',
+export default meta;
+
+type Story = StoryObj<typeof PageAlert>;
+
+export const Basic: Story = {
+	args: {
+		title: 'Page alert',
+		tone: 'success',
+	},
+};
+
+export const WithProse: Story = {
+	render: (args) => (
+		<PageAlert {...args}>
+			<Prose>
+				<ul>
+					<li>
+						<a href="#">Full name must not be empty</a>
+					</li>
+					<li>
+						<a href="#">Phone number must not be empty</a>
+					</li>
+				</ul>
+			</Prose>
+		</PageAlert>
+	),
+	args: {
+		title: 'Page alert',
+		tone: 'error',
+	},
+};
+
+export const WithTitleElement: Story = {
+	args: {
+		tone: 'success',
+		title: (
+			<PageAlertTitle as="h2">Descriptive success message (H2)</PageAlertTitle>
+		),
+	},
 };
