1dc26a9f486824197f48da40b2edfc96bcb3a1b1
Mike Hazell <814227+mikehazell@users.noreply.github.com>
2023-06-14 14:28:46 +1000

    Update 404 example site page

--- example-site/pages/404.tsx
+++ example-site/pages/404.tsx
@@ -1,26 +1,43 @@
+import { Callout } from '@ag.ds-next/react/callout';
 import { PageContent } from '@ag.ds-next/react/content';
 import { Prose } from '@ag.ds-next/react/prose';
+import { Stack } from '@ag.ds-next/react/stack';
+import { Text } from '@ag.ds-next/react/text';
 import { DocumentTitle } from '../components/DocumentTitle';
 import { SiteLayout } from '../components/SiteLayout';
 
 export default function NotFoundPage() {
 	return (
 		<>
-			<DocumentTitle title="Error 404" />
-			<SiteLayout template={{ name: '404', slug: '404' }}>
+			<DocumentTitle title="Page not found" />
+			<SiteLayout template={{ name: 'Error page', slug: 'error-page' }}>
 				<PageContent>
-					<Prose>
-						<h1>Page not found</h1>
-						<p>If you typed the web address, check it is correct.</p>
-						<p>
-							If you pasted the web address, check you copied the entire
-							address.
-						</p>
-						<p>
-							If the address is correct and you believe this page should exist,
-							please contact support.
-						</p>
-					</Prose>
+					<Stack gap={1.5}>
+						<Prose>
+							<h1>Page not found</h1>
+							<p>
+								Check the web address is correct or go back to the{' '}
+								<a href="#">Export Service</a>.
+							</p>
+						</Prose>
+						<Callout title="Need help?">
+							<Prose>
+								<ul>
+									<li>
+										Call <a href="tel:1800 571 125">1800 571 125</a>, Monday to
+										Friday, 9 am to 5 pm AEST
+									</li>
+									<li>
+										Email{' '}
+										<a href="exportservive@agriculture.gov.au">
+											exportservive@agriculture.gov.au
+										</a>
+									</li>
+								</ul>
+							</Prose>
+						</Callout>
+						<Text color="muted">Error code: 404</Text>
+					</Stack>
 				</PageContent>
 			</SiteLayout>
 		</>
