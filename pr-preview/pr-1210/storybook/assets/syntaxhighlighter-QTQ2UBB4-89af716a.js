52e0a33e4927aef8a1a7aa68325eae24dda9b7c1
jordanoverbye <jordanoverbye@users.noreply.github.com>
2023-06-23 02:01:58 +0000

    Remove preview for PR 1210 🛬

--- pr-preview/pr-1210/storybook/assets/syntaxhighlighter-QTQ2UBB4-89af716a.js
+++
@@ -1,2 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-7FRJXR4N-ce3cbea2.js";import"./iframe-7c692f56.js";import"../sb-preview/runtime.mjs";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
-//# sourceMappingURL=syntaxhighlighter-QTQ2UBB4-89af716a.js.map
