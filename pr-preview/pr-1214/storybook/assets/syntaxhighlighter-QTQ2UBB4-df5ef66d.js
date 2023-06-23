c47ccb9e03d8bdcbe341e850219bf11a255cee69
jordanoverbye <jordanoverbye@users.noreply.github.com>
2023-06-23 02:59:48 +0000

    Remove preview for PR 1214 🛬

--- pr-preview/pr-1214/storybook/assets/syntaxhighlighter-QTQ2UBB4-df5ef66d.js
+++
@@ -1,2 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-7FRJXR4N-d54e2076.js";import"./iframe-40b88f68.js";import"../sb-preview/runtime.mjs";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
-//# sourceMappingURL=syntaxhighlighter-QTQ2UBB4-df5ef66d.js.map
