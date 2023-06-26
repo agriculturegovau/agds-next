4e8326fc15bc57a998e64a8aed208355dcc0d582
Jordan Overbye <jordanoverbye@gmail.com>
2023-06-26 14:18:24 +1000

    comment

--- example-site/next.config.js
+++ example-site/next.config.js
@@ -8,6 +8,9 @@ const basePath = [process.env.BASE_PATH, '/example-site']
 const nextConfig = {
 	reactStrictMode: true,
 	basePath,
+	// Disabling esmExternals fixes an issue with `@floating-ui/react-dom` which is a dependency of `@ag.ds-next/react`
+	// This was done in collaboration with https://github.com/emmatown
+	// Consumers of the design system DO NOT need to worry about this
 	experimental: {
 		esmExternals: false,
 	},
