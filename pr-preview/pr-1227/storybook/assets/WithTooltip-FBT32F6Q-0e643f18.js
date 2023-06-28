d290318c5cce396940777bb480a7df71446c608e
jordanoverbye <jordanoverbye@users.noreply.github.com>
2023-06-28 00:27:32 +0000

    Remove preview for PR 1227 🛬

--- pr-preview/pr-1227/storybook/assets/WithTooltip-FBT32F6Q-0e643f18.js
+++
@@ -1,2 +0,0 @@
-import{b as r,b as a,d as T}from"./DocsRenderer-7FRJXR4N-ee6980c2.js";import"./iframe-02075345.js";import"../sb-preview/runtime.mjs";export{r as WithToolTipState,a as WithTooltip,T as WithTooltipPure};
-//# sourceMappingURL=WithTooltip-FBT32F6Q-0e643f18.js.map
--- pr-preview/pr-1227/storybook/assets/syntaxhighlighter-QTQ2UBB4-b1fb51ca.js
+++
@@ -1,2 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-7FRJXR4N-ee6980c2.js";import"./iframe-02075345.js";import"../sb-preview/runtime.mjs";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
-//# sourceMappingURL=syntaxhighlighter-QTQ2UBB4-b1fb51ca.js.map
