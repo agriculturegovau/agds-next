9f8459df07bddde9e8d51bd8a761f0c3f9a4c512
Nathan Simpson <hello@nathansimpson.design>
2023-07-10 11:20:24 +1000

    Use blue

--- packages/react/src/ag-branding/theme.ts
+++ packages/react/src/ag-branding/theme.ts
@@ -2,7 +2,7 @@ import { Theme } from '../core';
 
 export const theme: Theme = {
 	lightForegroundAction: '#00558b',
-	lightForegroundSelected: '#9263de',
+	lightForegroundSelected: '#00558b',
 	lightAccent: '#F36C52',
 	lightSystemError: '#D10000',
 	lightSystemErrorMuted: '#FFF0F0',
@@ -17,7 +17,7 @@ export const theme: Theme = {
 	darkBackgroundBodyAlt: '#162846',
 	darkBackgroundShadeAlt: '#20365B',
 	darkForegroundAction: '#9EE8FF',
-	darkForegroundSelected: '#c390f9',
+	darkForegroundSelected: '#9EE8FF',
 	darkAccent: '#F36C52',
 	darkSystemError: '#FF4F4F',
 	darkSystemErrorMuted: '#422F47',
