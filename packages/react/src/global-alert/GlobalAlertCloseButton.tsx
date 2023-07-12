77c3961c5c354645fc67746537d729427c869e29
Nathan Simpson <hello@nathansimpson.design>
2023-07-12 13:57:04 +1000

    New layout

--- packages/react/src/global-alert/GlobalAlertCloseButton.tsx
+++ packages/react/src/global-alert/GlobalAlertCloseButton.tsx
@@ -1,11 +1,15 @@
 import { MouseEventHandler } from 'react';
 import { Button } from '../button';
-import { mapSpacing, tokens } from '../core';
+import { mapSpacing, mq, tokens } from '../core';
 import { CloseIcon } from '../icon';
 
 type GlobalAlertCloseButtonProps = {
 	onClick?: MouseEventHandler<HTMLButtonElement>;
 };
+
+export const GLOBAL_ALERT_CLOSE_BUTTON_LABEL_VISIBLE_BREAKPOINT = 'lg';
+export const GLOBAL_ALERT_CLOSE_BUTTON_LABEL_VISIBLE_MQ =
+	tokens.mediaQuery.min[GLOBAL_ALERT_CLOSE_BUTTON_LABEL_VISIBLE_BREAKPOINT];
 
 export const GlobalAlertCloseButton = ({
 	onClick,
@@ -15,18 +19,20 @@ export const GlobalAlertCloseButton = ({
 			onClick={onClick}
 			iconAfter={CloseIcon}
 			variant="text"
-			css={{
+			aria-label="Close"
+			css={mq({
 				position: 'absolute',
 				top: mapSpacing(0.75),
-				right: 0,
+				right: mapSpacing(0.75),
 				flexShrink: 0,
-				// Reset the position on larger screens
-				[tokens.mediaQuery.min.sm]: {
-					position: 'relative',
-					top: 'unset',
-					right: 'unset',
+				// Hide the Close label on small screens
+				'& > span:first-of-type': {
+					display: 'none',
+					[GLOBAL_ALERT_CLOSE_BUTTON_LABEL_VISIBLE_MQ]: {
+						display: 'block',
+					},
 				},
-			}}
+			})}
 		>
 			Close
 		</Button>
