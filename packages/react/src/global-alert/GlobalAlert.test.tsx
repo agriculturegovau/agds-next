5ad955d2646c0d69a0ffa0ec9ee4efa8dff9f5c9
Nathan Simpson <hello@nathansimpson.design>
2023-07-12 14:08:35 +1000

    Add tone support

--- packages/react/src/global-alert/GlobalAlert.test.tsx
+++ packages/react/src/global-alert/GlobalAlert.test.tsx
@@ -12,74 +12,78 @@ function renderGlobalAlert(props: GlobalAlertProps) {
 }
 
 describe('GlobalAlert', () => {
-	it('renders correctly', () => {
-		const { container } = renderGlobalAlert({
-			title: 'Alert title',
-			children: <Text as="p">Alert description.</Text>,
-		});
-		expect(container).toMatchSnapshot();
-	});
-
-	it('renders a valid HTML structure', () => {
-		const { container } = renderGlobalAlert({
-			title: 'Alert title',
-			children: <Text as="p">Alert description.</Text>,
-		});
-		expect(container).toHTMLValidate({
-			extends: ['html-validate:recommended'],
-			rules: {
-				// react 18s `useId` break this rule
-				'valid-id': 'off',
-			},
-		});
-	});
-
-	it('has correct aria attributes', () => {
-		renderGlobalAlert({
-			title: 'Alert title',
-			children: <Text as="p">Alert description.</Text>,
-		});
-		expect(screen.getByRole('region')).toHaveAttribute(
-			'aria-label',
-			'Alert title'
-		);
-	});
-
-	describe('with a close button', () => {
-		it('renders correctly', () => {
-			const { container } = renderGlobalAlert({
-				title: 'Alert title',
-				children: <Text as="p">Alert description.</Text>,
-				onDismiss: jest.fn(),
-			});
-			expect(container).toMatchSnapshot();
-		});
-
-		it('renders a valid HTML structure', () => {
-			const { container } = renderGlobalAlert({
-				title: 'Alert title',
-				children: <Text as="p">Alert description.</Text>,
-				onDismiss: jest.fn(),
-			});
-			expect(container).toHTMLValidate({
-				extends: ['html-validate:recommended'],
-				rules: {
-					// react 18s `useId` break this rule
-					'valid-id': 'off',
-				},
-			});
-		});
-
-		it('responds to an onDismiss event', async () => {
-			const onDismiss = jest.fn();
-			renderGlobalAlert({
-				title: 'Alert title',
-				children: <Text as="p">Alert description.</Text>,
-				onDismiss,
+	['warning', 'info'].forEach((tone) => {
+		describe(`with tone ${tone}`, () => {
+			it('renders correctly', () => {
+				const { container } = renderGlobalAlert({
+					title: 'Alert title',
+					children: <Text as="p">Alert description.</Text>,
+				});
+				expect(container).toMatchSnapshot();
 			});
 
-			await userEvent.click(screen.getByText('Close'));
-			expect(onDismiss).toHaveBeenCalledTimes(1);
+			it('renders a valid HTML structure', () => {
+				const { container } = renderGlobalAlert({
+					title: 'Alert title',
+					children: <Text as="p">Alert description.</Text>,
+				});
+				expect(container).toHTMLValidate({
+					extends: ['html-validate:recommended'],
+					rules: {
+						// react 18s `useId` break this rule
+						'valid-id': 'off',
+					},
+				});
+			});
+
+			it('has correct aria attributes', () => {
+				renderGlobalAlert({
+					title: 'Alert title',
+					children: <Text as="p">Alert description.</Text>,
+				});
+				expect(screen.getByRole('region')).toHaveAttribute(
+					'aria-label',
+					'Alert title'
+				);
+			});
+
+			describe('and a close button', () => {
+				it('renders correctly', () => {
+					const { container } = renderGlobalAlert({
+						title: 'Alert title',
+						children: <Text as="p">Alert description.</Text>,
+						onDismiss: jest.fn(),
+					});
+					expect(container).toMatchSnapshot();
+				});
+
+				it('renders a valid HTML structure', () => {
+					const { container } = renderGlobalAlert({
+						title: 'Alert title',
+						children: <Text as="p">Alert description.</Text>,
+						onDismiss: jest.fn(),
+					});
+					expect(container).toHTMLValidate({
+						extends: ['html-validate:recommended'],
+						rules: {
+							// react 18s `useId` break this rule
+							'valid-id': 'off',
+						},
+					});
+				});
+
+				it('responds to an onDismiss event', async () => {
+					const onDismiss = jest.fn();
+					renderGlobalAlert({
+						title: 'Alert title',
+						children: <Text as="p">Alert description.</Text>,
+						onDismiss,
+					});
+
+					await userEvent.click(screen.getByText('Close'));
+					expect(onDismiss).toHaveBeenCalledTimes(1);
+				});
+			});
 		});
 	});
 });
