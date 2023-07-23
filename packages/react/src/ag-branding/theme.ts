caf0fdd0a53b4e13f5aa10eabc000c32b4ee110f
Nathan Simpson <hello@nathansimpson.design>
2023-07-24 09:11:18 +1000

    Core: Create primitive colour layer

--- packages/react/src/ag-branding/theme.ts
+++ packages/react/src/ag-branding/theme.ts
@@ -1,28 +1,96 @@
 import { Theme } from '../core';
 
+// Internal colours only. Not for use in the UI.
+const primitives = {
+	// shades for neutrals
+	// // grey
+	white: '#ffffff', // body
+	grey1: '#f5f5f5', // body-alt
+	grey2: '#ebebeb', // shade
+	grey3: '#e0e0e0', // shade-alt
+	grey4: '#D3D3D3', // border muted
+	grey5: '#808080', // border
+	grey6: '#626262', // text-muted
+	grey7: '#313131', // text
+	// // slate
+	slate1: '#bdd2d7',
+	slate2: '#95b7bf',
+	slate3: '#4D7680',
+	// // navy
+	navy1: '#0A1629',
+	navy2: '#122440',
+	navy3: '#162846',
+	navy4: '#20365B',
+
+	// colours
+	exportServiceOrange: '#F36C52',
+	deepBlue: '#00558b',
+	red: '#D10000',
+	redPale: '#FFF0F0',
+	green: '#00754E',
+	greenPale: '#E5FFF6',
+	yellow: '#D16900',
+	yellowPale: '#FFF2E5',
+	blue: '#008BD1',
+	bluePale: '#E5F6FF',
+	purple: '#9263de',
+	blackOverlay: 'rgba(0, 0, 0, 0.8)',
+
+	// alt colours for dark palette
+	skyBlue: '#9EE8FF',
+	tomato: '#FF4F4F',
+	tomatoDim: '#422F47',
+	cucumber: '#00A36D',
+	cucumberDim: '#133A4B',
+	pumpkin: '#FF9933',
+	pumpkinDim: '#393942',
+	blueberry: '#33BAFF',
+	blueberryDim: '#193A5B',
+	eggplant: '#c390f9',
+};
+
 export const theme: Theme = {
-	lightForegroundAction: '#00558b',
-	lightAccent: '#F36C52',
-	lightSystemError: '#D10000',
-	lightSystemErrorMuted: '#FFF0F0',
-	lightSystemSuccess: '#00754E',
-	lightSystemSuccessMuted: '#E5FFF6',
-	lightSystemWarning: '#D16900',
-	lightSystemWarningMuted: '#FFF2E5',
-	lightSystemInfo: '#008BD1',
-	lightSystemInfoMuted: '#E5F6FF',
-	darkBackgroundBody: '#0A1629',
-	darkBackgroundShade: '#122440',
-	darkBackgroundBodyAlt: '#162846',
-	darkBackgroundShadeAlt: '#20365B',
-	darkForegroundAction: '#9EE8FF',
-	darkAccent: '#F36C52',
-	darkSystemError: '#FF4F4F',
-	darkSystemErrorMuted: '#422F47',
-	darkSystemSuccess: '#00A36D',
-	darkSystemSuccessMuted: '#133A4B',
-	darkSystemWarning: '#FF9933',
-	darkSystemWarningMuted: '#393942',
-	darkSystemInfo: '#33BAFF',
-	darkSystemInfoMuted: '#193A5B',
+	// gold values
+	lightForegroundText: primitives.grey7,
+	lightForegroundFocus: primitives.purple,
+	lightForegroundMuted: primitives.grey6,
+	lightBackgroundBody: primitives.white,
+	lightBackgroundShade: primitives.grey1,
+	lightBackgroundBodyAlt: primitives.grey2,
+	lightBackgroundShadeAlt: primitives.grey3,
+	lightBorder: primitives.grey5,
+	lightBorderMuted: primitives.grey4,
+	lightOverlay: primitives.blackOverlay,
+	darkForegroundText: primitives.white,
+	darkForegroundFocus: primitives.eggplant,
+	darkForegroundMuted: primitives.slate1,
+	darkBorder: primitives.slate2,
+	darkBorderMuted: primitives.slate3,
+	darkOverlay: primitives.blackOverlay,
+
+	// agds overrides
+	lightForegroundAction: primitives.deepBlue,
+	lightAccent: primitives.exportServiceOrange,
+	lightSystemError: primitives.red,
+	lightSystemErrorMuted: primitives.redPale,
+	lightSystemSuccess: primitives.green,
+	lightSystemSuccessMuted: primitives.greenPale,
+	lightSystemWarning: primitives.yellow,
+	lightSystemWarningMuted: primitives.yellowPale,
+	lightSystemInfo: primitives.blue,
+	lightSystemInfoMuted: primitives.bluePale,
+	darkBackgroundBody: primitives.navy1,
+	darkBackgroundShade: primitives.navy2,
+	darkBackgroundBodyAlt: primitives.navy3,
+	darkBackgroundShadeAlt: primitives.navy4,
+	darkForegroundAction: primitives.skyBlue,
+	darkAccent: primitives.exportServiceOrange,
+	darkSystemError: primitives.tomato,
+	darkSystemErrorMuted: primitives.tomatoDim,
+	darkSystemSuccess: primitives.cucumber,
+	darkSystemSuccessMuted: primitives.cucumberDim,
+	darkSystemWarning: primitives.pumpkin,
+	darkSystemWarningMuted: primitives.pumpkinDim,
+	darkSystemInfo: primitives.blueberry,
+	darkSystemInfoMuted: primitives.blueberryDim,
 };
