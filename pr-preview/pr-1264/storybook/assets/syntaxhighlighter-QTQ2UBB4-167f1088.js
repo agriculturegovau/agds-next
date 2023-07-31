fc6f832413edd46c11b0cca41e15c38bbfa25268
jordanoverbye <jordanoverbye@users.noreply.github.com>
2023-07-31 03:30:02 +0000

    Remove preview for PR 1264 🛬

--- pr-preview/pr-1264/storybook/assets/syntaxhighlighter-QTQ2UBB4-167f1088.js
+++
@@ -1,2 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-EYKKDMVH-59d926b9.js";import"./iframe-e6000cef.js";import"../sb-preview/runtime.js";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
-//# sourceMappingURL=syntaxhighlighter-QTQ2UBB4-167f1088.js.map
