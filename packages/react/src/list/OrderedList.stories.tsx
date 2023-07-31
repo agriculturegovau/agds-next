da4822a497ea535c38b2668237b56e9365fef19a
Jordan Overbye <jordanoverbye@gmail.com>
2023-08-01 09:52:38 +1000

    update docs and stories

--- packages/react/src/list/OrderedList.stories.tsx
+++ packages/react/src/list/OrderedList.stories.tsx
@@ -1,36 +1,32 @@
-import { StoryObj, Meta, StoryFn } from '@storybook/react';
+import { StoryObj, Meta } from '@storybook/react';
 import { Stack } from '../stack';
 import { Text } from '../text';
 import { OrderedList } from './OrderedList';
 import { ListItem } from './ListItem';
 
-const Example: StoryFn<typeof OrderedList> = (args) => (
-	<Stack gap={1}>
-		<Text as="p">This is a paragraph, wrapped in a Stack.</Text>
-		<OrderedList {...args}>
-			<ListItem>Item 1</ListItem>
-			<ListItem>Item 2</ListItem>
-			<ListItem>Item 3</ListItem>
-		</OrderedList>
-	</Stack>
-);
-
 const meta: Meta<typeof OrderedList> = {
-	title: 'Content/List',
+	title: 'Foundations/List/OrderedList',
 	component: OrderedList,
-	render: Example,
 };
 
 export default meta;
 
 type Story = StoryObj<typeof OrderedList>;
 
-export const Ordered: Story = {
-	storyName: 'OrderedList',
+export const Basic: Story = {
+	render: (args) => (
+		<Stack gap={1}>
+			<Text as="p">This is a paragraph, wrapped in a Stack.</Text>
+			<OrderedList {...args}>
+				<ListItem>Item 1</ListItem>
+				<ListItem>Item 2</ListItem>
+				<ListItem>Item 3</ListItem>
+			</OrderedList>
+		</Stack>
+	),
 };
 
-export const NestedOl: Story = {
-	storyName: 'OrderedList (nested)',
+export const Nested: Story = {
 	render: (args) => (
 		<OrderedList {...args}>
 			<ListItem>
