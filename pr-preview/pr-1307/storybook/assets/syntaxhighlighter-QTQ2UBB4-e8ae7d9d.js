6b8b44b498ae2e1136f2334e12a7c18023759984
nathsimpson <nathsimpson@users.noreply.github.com>
2023-07-31 23:49:05 +0000

    Remove preview for PR 1307 🛬

--- pr-preview/pr-1307/storybook/assets/syntaxhighlighter-QTQ2UBB4-e8ae7d9d.js
+++
@@ -1,2 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-EYKKDMVH-b38d324d.js";import"./iframe-fa5f57bd.js";import"../sb-preview/runtime.js";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
-//# sourceMappingURL=syntaxhighlighter-QTQ2UBB4-e8ae7d9d.js.map
