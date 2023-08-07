73482b63f387cb3b9224674b51e1e55a69d163f7
Nathan Simpson <hello@nathansimpson.design>
2023-08-07 15:07:38 +1000

    scale icons towards point

--- packages/react/src/icon/icons/ChevronDownIcon.tsx
+++ packages/react/src/icon/icons/ChevronDownIcon.tsx
@@ -1,6 +1,6 @@
 import { createIcon } from '../Icon';
 
 export const ChevronDownIcon = createIcon(
-	<path d="M21 8L12 17L3 8" />,
+	<path d="m20 9-7.646 7.646a.5.5 0 0 1-.708 0L4 9" />,
 	'ChevronDownIcon'
 );
--- packages/react/src/icon/icons/ChevronUpIcon.tsx
+++ packages/react/src/icon/icons/ChevronUpIcon.tsx
@@ -1,6 +1,6 @@
 import { createIcon } from '../Icon';
 
 export const ChevronUpIcon = createIcon(
-	<path d="M3 16L12 7L21 16" />,
+	<path d="m4 15 7.646-7.646a.5.5 0 0 1 .708 0L20 15" />,
 	'ChevronUpIcon'
 );
