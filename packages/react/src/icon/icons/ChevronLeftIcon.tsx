027e2b8e0270bbf9b9caae80aeb697af3081d83c
Nathan Simpson <hello@nathansimpson.design>
2023-08-07 15:07:38 +1000

    scale icons towards point

--- packages/react/src/icon/icons/ChevronLeftIcon.tsx
+++ packages/react/src/icon/icons/ChevronLeftIcon.tsx
@@ -1,6 +1,6 @@
 import { createIcon } from '../Icon';
 
 export const ChevronLeftIcon = createIcon(
-	<path d="M16 21L7 12L16 3" />,
+	<path d="m15 20-7.646-7.646a.5.5 0 0 1 0-.708L15 4" />,
 	'ChevronLeftIcon'
 );
