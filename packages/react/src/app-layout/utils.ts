7db1e18716b64031f31d4935998cf64258ac2304
Nathan Simpson <hello@nathansimpson.design>
2023-08-07 15:17:57 +1000

    Reduce from 280px to 256px

--- packages/react/src/app-layout/utils.ts
+++ packages/react/src/app-layout/utils.ts
@@ -1,4 +1,4 @@
 // The breakpoint where we show the sidebar in a two column layout
 export const APP_LAYOUT_DESKTOP_BREAKPOINT = 'xl';
 
-export const APP_LAYOUT_SIDEBAR_WIDTH = '17.5rem'; // 280 px
+export const APP_LAYOUT_SIDEBAR_WIDTH = '16rem'; // 256 px
