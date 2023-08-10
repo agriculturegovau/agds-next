7e047025abc3382fc522f2d764000918c16548dd
Jordan Overbye <jordanoverbye@gmail.com>
2023-08-10 11:52:57 +1000

    update icons

--- packages/react/src/icon/icons/ChevronDownIcon.tsx
+++ packages/react/src/icon/icons/ChevronDownIcon.tsx
@@ -1,6 +1,6 @@
 import { createIcon } from '../Icon';
 
 export const ChevronDownIcon = createIcon(
-	<path d="m20 9-7.646 7.646a.5.5 0 0 1-.708 0L4 9" />,
+	<polyline points="6 9 12 15 18 9" />,
 	'ChevronDownIcon'
 );
--- packages/react/src/icon/icons/ChevronRightIcon.tsx
+++ packages/react/src/icon/icons/ChevronRightIcon.tsx
@@ -1,6 +1,6 @@
 import { createIcon } from '../Icon';
 
 export const ChevronRightIcon = createIcon(
-	<path d="m9 4 7.646 7.646a.5.5 0 0 1 0 .708L9 20" />,
+	<polyline points="9 18 15 12 9 6" />,
 	'ChevronRightIcon'
 );
