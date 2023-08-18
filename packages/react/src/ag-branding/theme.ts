177d33175c93c06a4438e6b528ddd39e788354b2
Nathan Simpson <hello@nathansimpson.design>
2023-08-18 14:15:12 +1000

    1,2,3

--- packages/react/src/ag-branding/theme.ts
+++ packages/react/src/ag-branding/theme.ts
@@ -3,8 +3,9 @@ import { Theme } from '../core';
 // Internal colours only. Not for use in the UI.
 const primitives = {
 	// shades for neutrals
-	// // grey
 	white: '#ffffff', // body
+
+	// grey
 	grey1: '#f5f5f5', // body-alt
 	grey2: '#ebebeb', // shade
 	grey3: '#e0e0e0', // shade-alt
@@ -12,11 +13,11 @@ const primitives = {
 	grey5: '#808080', // border
 	grey6: '#626262', // text-muted
 	grey7: '#313131', // text
-	// // slate
+	// slate
 	slate1: '#bdd2d7',
 	slate2: '#95b7bf',
 	slate3: '#4D7680',
-	// // navy
+	// navy
 	navy1: '#0A1629',
 	navy2: '#122440',
 	navy3: '#162846',
@@ -26,42 +27,42 @@ const primitives = {
 
 	// green
 	// TODO: We should consider regenerating these shades to ensure they are consistent hues
-	green100: '#E5FFF6',
-	green400: '#00A36D',
-	green600: '#00754E',
-	green900: '#133A4B',
+	green1: '#E5FFF6',
+	green2: '#00A36D',
+	green3: '#00754E',
+	green4: '#133A4B',
 
 	// red
-	red100: '#FFF0F0',
-	red400: '#D10000',
+	red1: '#FFF0F0',
+	red2: '#D10000',
 
 	// tomato: used in dark mode but are very different to reds
-	tomato400: '#FF4F4F',
-	tomato900: '#422F47',
+	tomato1: '#FF4F4F',
+	tomato2: '#422F47',
 
 	// yellow
 	// TODO: We should consider regenerating these shades to ensure they are consistent hues
-	yellow100: '#FFF2E5',
-	yellow400: '#FF9933',
-	yellow600: '#D16900',
-	yellow900: '#393942',
+	yellow1: '#FFF2E5',
+	yellow2: '#FF9933',
+	yellow3: '#D16900',
+	yellow4: '#393942',
 
 	// blue
 	// TODO: We should consider regenerating these shades to ensure they are consistent hues
-	blue100: '#E5F6FF',
-	blue400: '#008BD1',
-	blue600: '#00558b',
+	blue1: '#E5F6FF',
+	blue2: '#008BD1',
+	blue3: '#00558b',
 
 	// purple
 	// TODO: We should consider regenerating these shades to ensure they are consistent hues
-	purple400: '#c390f9',
-	purple600: '#9263de',
+	purple1: '#c390f9',
+	purple2: '#9263de',
 
-	// blueberry
+	// sky
 	// TODO: We should consider regenerating these shades to ensure they are consistent hues
-	blueberry100: '#9EE8FF',
-	blueberry400: '#33BAFF',
-	blueberry900: '#193A5B',
+	sky1: '#9EE8FF',
+	sky2: '#33BAFF',
+	sky3: '#193A5B',
 
 	blackOverlay: 'rgba(0, 0, 0, 0.8)',
 };
@@ -69,7 +70,7 @@ const primitives = {
 export const theme: Theme = {
 	// gold values:  light
 	lightForegroundText: primitives.grey7,
-	lightForegroundFocus: primitives.purple600,
+	lightForegroundFocus: primitives.purple2,
 	lightForegroundMuted: primitives.grey6,
 	lightBackgroundBody: primitives.white,
 	lightBackgroundShade: primitives.grey1,
@@ -81,37 +82,37 @@ export const theme: Theme = {
 
 	// gold values: dark
 	darkForegroundText: primitives.white,
-	darkForegroundFocus: primitives.purple400,
+	darkForegroundFocus: primitives.purple1,
 	darkForegroundMuted: primitives.slate1,
 	darkBorder: primitives.slate2,
 	darkBorderMuted: primitives.slate3,
 	darkOverlay: primitives.blackOverlay,
 
 	// agds overrides: light
-	lightForegroundAction: primitives.blue600,
+	lightForegroundAction: primitives.blue3,
 	lightAccent: primitives.exportServiceOrange,
-	lightSystemError: primitives.red400,
-	lightSystemErrorMuted: primitives.red100,
-	lightSystemSuccess: primitives.green600,
-	lightSystemSuccessMuted: primitives.green100,
-	lightSystemWarning: primitives.yellow600,
-	lightSystemWarningMuted: primitives.yellow100,
-	lightSystemInfo: primitives.blue400,
-	lightSystemInfoMuted: primitives.blue100,
+	lightSystemError: primitives.red2,
+	lightSystemErrorMuted: primitives.red1,
+	lightSystemSuccess: primitives.green3,
+	lightSystemSuccessMuted: primitives.green1,
+	lightSystemWarning: primitives.yellow3,
+	lightSystemWarningMuted: primitives.yellow1,
+	lightSystemInfo: primitives.blue2,
+	lightSystemInfoMuted: primitives.blue1,
 
 	// agds overrides: dark
 	darkBackgroundBody: primitives.navy1,
 	darkBackgroundShade: primitives.navy2,
 	darkBackgroundBodyAlt: primitives.navy3,
 	darkBackgroundShadeAlt: primitives.navy4,
-	darkForegroundAction: primitives.blueberry100,
+	darkForegroundAction: primitives.sky1,
 	darkAccent: primitives.exportServiceOrange,
-	darkSystemError: primitives.tomato400,
-	darkSystemErrorMuted: primitives.tomato900,
-	darkSystemSuccess: primitives.green400,
-	darkSystemSuccessMuted: primitives.green900,
-	darkSystemWarning: primitives.yellow400,
-	darkSystemWarningMuted: primitives.yellow900,
-	darkSystemInfo: primitives.blueberry400,
-	darkSystemInfoMuted: primitives.blueberry900,
+	darkSystemError: primitives.tomato1,
+	darkSystemErrorMuted: primitives.tomato2,
+	darkSystemSuccess: primitives.green2,
+	darkSystemSuccessMuted: primitives.green4,
+	darkSystemWarning: primitives.yellow2,
+	darkSystemWarningMuted: primitives.yellow4,
+	darkSystemInfo: primitives.sky2,
+	darkSystemInfoMuted: primitives.sky3,
 };
