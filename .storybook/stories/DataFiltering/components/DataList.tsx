a5b92bde7f57018561b337ffed22572dad8e5789
Nathan Simpson <hello@nathansimpson.design>
2023-08-21 14:49:48 +1000

    loading and empty state

--- .storybook/stories/DataFiltering/components/DataList.tsx
+++ .storybook/stories/DataFiltering/components/DataList.tsx
@@ -1,10 +1,13 @@
 import { VisuallyHidden } from '@ag.ds-next/react/a11y';
 import { Stack } from '@ag.ds-next/react/stack';
 import { Card, CardInner, CardLink } from '@ag.ds-next/react/card';
-import { H3 } from '@ag.ds-next/react/heading';
+import { H3, Heading } from '@ag.ds-next/react/heading';
 import { SkeletonHeading, SkeletonText } from '@ag.ds-next/react/skeleton';
 import { Text } from '@ag.ds-next/react/text';
+import { HelpIcon } from '@ag.ds-next/react/icon';
+import { Button } from '@ag.ds-next/react/button';
 import { BusinessForAuditWithIndex } from '../lib/generateBusinessData';
+import { useSortAndFilterContext } from '../lib/contexts';
 
 type DataListProps = {
 	loading: boolean;
@@ -13,13 +16,37 @@ type DataListProps = {
 };
 
 export const DataList = ({ loading, data }: DataListProps) => {
+	const { resetFilters } = useSortAndFilterContext();
+
+	if (loading) {
+		return (
+			<Stack as="ul" gap={1}>
+				{Array.from({ length: 10 }, (_, i) => i).map((i) => (
+					<DataListItemSkeleton key={i} />
+				))}
+			</Stack>
+		);
+	}
+
+	if (!data.length)
+		return (
+			<Stack gap={2} alignItems="flex-start">
+				<Stack gap={1}>
+					<HelpIcon size="lg" color="muted" />
+					<Heading type="h2" fontSize="lg">
+						No results found
+					</Heading>
+					<Text>Try adjusting your filter options.</Text>
+				</Stack>
+				<Button onClick={resetFilters}>Clear filters</Button>
+			</Stack>
+		);
+
 	return (
 		<Stack as="ul" gap={1}>
-			{loading
-				? Array.from({ length: 10 }, (_, i) => i).map((i) => (
-						<DataListItemSkeleton key={i} />
-				  ))
-				: data.map((item) => <DataListItem key={item.id} data={item} />)}
+			{data.map((item) => (
+				<DataListItem key={item.id} data={item} />
+			))}
 		</Stack>
 	);
 };
