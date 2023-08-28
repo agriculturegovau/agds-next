26b3d5596d7de8393226b4567c1ac2a922bf7023
Jordan Overbye <jordanoverbye@gmail.com>
2023-08-28 12:06:04 +1000

    Revert "app-layout: Decrease gap between icon and label (#1302)"
    This reverts commit e6b654ef3503ec43da19aef9c7222b455a3591e0.
    
    # Conflicts:
    #	packages/react/src/app-layout/__snapshots__/AppLayout.test.tsx.snap

--- packages/react/src/app-layout/AppLayoutSidebarNav.tsx
+++ packages/react/src/app-layout/AppLayoutSidebarNav.tsx
@@ -116,7 +116,7 @@ function AppLayoutSidebarNavItemInner({
 				' a, button': {
 					display: 'flex',
 					alignItems: 'center',
-					gap: mapSpacing(0.75),
+					gap: mapSpacing(1),
 					width: '100%',
 					boxSizing: 'border-box',
 					paddingLeft: mapSpacing(1.5),
