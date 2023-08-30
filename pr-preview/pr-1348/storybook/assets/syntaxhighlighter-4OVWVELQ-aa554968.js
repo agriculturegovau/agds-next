28fa0560a79ad023422fe61d07291dd13741a18c
jordanoverbye <jordanoverbye@users.noreply.github.com>
2023-08-30 23:28:49 +0000

    Remove preview for PR 1348 🛬

--- pr-preview/pr-1348/storybook/assets/syntaxhighlighter-4OVWVELQ-aa554968.js
+++
@@ -1,2 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-3PUGWF3O-3a1c8389.js";import"./iframe-27db31f6.js";import"../sb-preview/runtime.js";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
-//# sourceMappingURL=syntaxhighlighter-4OVWVELQ-aa554968.js.map
