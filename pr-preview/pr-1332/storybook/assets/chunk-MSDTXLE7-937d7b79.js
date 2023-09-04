0b4059c61bb9e56bb718e626a8b4c2ee5c253cf6
jordanoverbye <jordanoverbye@users.noreply.github.com>
2023-09-04 23:30:29 +0000

    Deploy preview for PR 1332 🛫

---
+++ pr-preview/pr-1332/storybook/assets/chunk-MSDTXLE7-937d7b79.js
@@ -0,0 +1,2 @@
+import{r as a}from"./iframe-77519c9e.js";var l=({icon:e,color:t,size:o,...i})=>{const n=t||"currentColor",r=o||"14px";return a.createElement("span",{role:"img","aria-hidden":"true",style:{color:n,width:r,height:r,display:"inline-flex",fontSize:"inherit"},...i},e)};export{l as I};
+//# sourceMappingURL=chunk-MSDTXLE7-937d7b79.js.map
--- pr-preview/pr-1332/storybook/assets/syntaxhighlighter-4OVWVELQ-95398dab.js
+++
@@ -1,2 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-3PUGWF3O-9b938983.js";import"./iframe-f44f2655.js";import"../sb-preview/runtime.js";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
-//# sourceMappingURL=syntaxhighlighter-4OVWVELQ-95398dab.js.map
