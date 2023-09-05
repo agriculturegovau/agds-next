7e3ef574e2294bd66d852df739ea6a84c0b33d6b
nathsimpson <nathsimpson@users.noreply.github.com>
2023-09-05 02:45:37 +0000

    Remove preview for PR 1342 🛬

--- pr-preview/pr-1342/storybook/assets/syntaxhighlighter-4OVWVELQ-12de35ab.js
+++
@@ -1,2 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-3PUGWF3O-50991a74.js";import"./iframe-e167d36a.js";import"../sb-preview/runtime.js";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
-//# sourceMappingURL=syntaxhighlighter-4OVWVELQ-12de35ab.js.map
