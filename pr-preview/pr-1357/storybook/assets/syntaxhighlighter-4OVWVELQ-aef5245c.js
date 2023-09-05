db2551c0b6ac518ad7eae7e05f23571628c2b892
nathsimpson <nathsimpson@users.noreply.github.com>
2023-09-05 02:56:53 +0000

    Remove preview for PR 1357 🛬

--- pr-preview/pr-1357/storybook/assets/syntaxhighlighter-4OVWVELQ-aef5245c.js
+++
@@ -1,2 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-3PUGWF3O-8e9a4459.js";import"./iframe-4f165327.js";import"../sb-preview/runtime.js";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
-//# sourceMappingURL=syntaxhighlighter-4OVWVELQ-aef5245c.js.map
