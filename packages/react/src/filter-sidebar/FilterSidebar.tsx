7587b8f8fb286966b6ae686673e2a6d959c78dec
Nathan Simpson <hello@nathansimpson.design>
2023-09-18 17:44:58 +1000

    title prop

--- packages/react/src/filter-sidebar/FilterSidebar.tsx
+++ packages/react/src/filter-sidebar/FilterSidebar.tsx
@@ -3,15 +3,18 @@ import { FormStack } from '@ag.ds-next/react/form-stack';
 import { CollapsingSideBar } from '../_collapsing-side-bar';
 
 export type FilterSidebarProps = PropsWithChildren<{
-	/** The aria-label for the FilterSidebar. */
-	'aria-label'?: string;
 	activeFiltersCount?: number;
+	/** The aria-label for the Filter sidebar. */
+	'aria-label'?: string;
+	/** Shown at the top of Filter sidebar. */
+	title?: string;
 }>;
 
 export function FilterSidebar({
+	activeFiltersCount,
 	'aria-label': ariaLabel = 'filters',
 	children,
-	activeFiltersCount,
+	title = 'Filters',
 }: FilterSidebarProps) {
 	const collapseButtonLabel = `Filters${
 		activeFiltersCount ? ` (${activeFiltersCount})` : ''
@@ -19,7 +22,7 @@ export function FilterSidebar({
 
 	return (
 		<CollapsingSideBar
-			title="Filters"
+			title={title}
 			collapseButtonLabel={collapseButtonLabel}
 			as="aside"
 			aria-label={ariaLabel}
