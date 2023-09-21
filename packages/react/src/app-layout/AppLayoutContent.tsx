da9bf7628c55a6133e9cd4f7de098d6f90a755c8
Jordan Overbye <jordanoverbye@gmail.com>
2023-09-21 13:08:42 +1000

    min-width

--- packages/react/src/app-layout/AppLayoutContent.tsx
+++ packages/react/src/app-layout/AppLayoutContent.tsx
@@ -5,7 +5,12 @@ export type AppLayoutContentProps = PropsWithChildren<{}>;
 
 export function AppLayoutContent({ children }: AppLayoutContentProps) {
 	return (
-		<Flex flexDirection="column" css={{ overflow: 'hidden' }}>
+		<Flex
+			flexDirection="column"
+			// `min-width: 0` Fixes wide elements (like large tables) not respecting the width of their container
+			// https://css-tricks.com/flexbox-truncated-text/#aa-the-solution-is-min-width-0-on-the-flex-child
+			css={{ minWidth: `0` }}
+		>
 			{children}
 		</Flex>
 	);
