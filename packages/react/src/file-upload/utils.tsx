8edc07fd5151cc5efcef2fb5643f48bc019b0e1a
Nathan Simpson <hello@nathansimpson.design>
2023-09-26 12:56:23 +1000

    move imageMimeTypes to func

--- packages/react/src/file-upload/utils.tsx
+++ packages/react/src/file-upload/utils.tsx
@@ -167,13 +167,13 @@ export function getErrorSummary(
 	return 'There’s an issue with one or more of your files';
 }
 
-const imageMimeTypes = /image\/(png|jpg|jpeg)/i;
 /** Creates and returns a URL of the image thumbnail in browser memory.
  * Returns undefined if the file is not an image. */
 export const getImageThumbnail = (file: FileWithPath) => {
 	// URL is supported in browser only
 	if (!window.URL) return undefined;
 
+	const imageMimeTypes = /image\/(png|jpg|jpeg)/i;
 	const isImageType = file.type.match(imageMimeTypes);
 	return isImageType ? URL.createObjectURL(file) : undefined;
 };
