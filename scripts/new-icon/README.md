e05e6e04ce81c455b2a97e71d1d49216e4d45b36
Nathan Simpson <nathan@thinkmill.com.au>
2023-09-27 12:00:08 +1000

    Create script to generate icons (#1392)

---
+++ scripts/new-icon/README.md
@@ -0,0 +1,12 @@
+# new-icon script
+
+This script uses plop.js to generate the files and changes needed to add a new icon to AgDS.
+
+We prefer to use icons from [Feather icons](https://feathericons.com) to maintain a consistent style.
+
+Any SVG added to the icon set must meet this criteria:
+
+- icon must be visually consistent with the rest of the suite
+- SVG dimensions must be 24px x 24px
+- lines must be based on strokes and not outlined
+- when not using a feather icon, SVG must be cleaned up using [SVGOMG](https://jakearchibald.github.io/svgomg/)
---
+++ scripts/new-icon/script.mjs
@@ -0,0 +1,60 @@
+export default function newIcon(
+	/** @type {import('plop').NodePlopAPI} */
+	plop
+) {
+	plop.setGenerator('icon', {
+		description: 'Add new icon',
+		prompts: [
+			{
+				type: 'input',
+				name: 'iconName',
+				message:
+					'Icon name, please use appropriate uppercase (e.g. ArrowDownIcon)',
+				validate: (answer) => answer.length > 0,
+			},
+		],
+		actions: (answers) => {
+			const actions = [];
+			if (!answers) return actions;
+
+			const { iconName } = answers;
+
+			actions.push({
+				type: 'addMany',
+				templateFiles: '../../plop-templates/icon/**',
+				base: '../../plop-templates/icon/',
+				destination: `../../packages/react/src/icon/icons`,
+				data: { iconName },
+			});
+
+			actions.push({
+				type: 'append',
+				path: '../../packages/react/src/icon/index.ts',
+				template: "export { {{iconName}} } from './icons/{{iconName}}';",
+			});
+
+			actions.push({
+				type: 'append',
+				path: '../../packages/react/src/icon/utils.tsx',
+				template:
+					"import { {{iconName}} } from './icons/{{iconName}}'; // Please add this to object above",
+			});
+
+			actions.push({
+				type: 'append',
+				path: '../../docs/components/designSystemComponents.tsx',
+				template:
+					'// Please add {{iconName}} to "@ag.ds-next/react/icons above"\n',
+			});
+
+			actions.push({
+				type: 'add',
+				path: '../../.changeset/plop-create-{{iconName}}.md',
+				template:
+					"---\n'@ag.ds-next/react': minor\n---\n\nicon: Created {{iconName}}\n",
+			});
+
+			return actions;
+		},
+	});
+}
