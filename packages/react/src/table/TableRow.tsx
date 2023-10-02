3f5157cff4f57e75d746cc1ecfc1a5f2379684d9
Jordan Overbye <jordanoverbye@gmail.com>
2023-10-03 08:34:36 +1000

    clean up styles

--- packages/react/src/table/TableRow.tsx
+++ packages/react/src/table/TableRow.tsx
@@ -12,43 +12,49 @@ export function TableRow({ children, selected }: TableRowProps) {
 	return (
 		<tr
 			aria-selected={selected}
-			css={{
-				...(selected && {
-					position: 'relative',
-					backgroundColor: `${boxPalette.selectedMuted}`,
+			css={
+				selected
+					? {
+							...(tableLayout === 'auto' && {
+								position: 'relative',
+								backgroundColor: boxPalette.selectedMuted,
 
-					// Add outline
-					'&:after': {
-						content: '""',
-						pointerEvents: 'none',
-						position: 'absolute',
-						inset: 0,
-						borderWidth: tokens.borderWidth.md,
-						borderColor: boxPalette.selected,
-						borderStyle: 'solid',
-					},
+								// Add outline
+								'&:after': {
+									content: '""',
+									pointerEvents: 'none',
+									position: 'absolute',
+									inset: 0,
+									borderWidth: tokens.borderWidth.md,
+									borderColor: boxPalette.selected,
+									borderStyle: 'solid',
+								},
 
-					// Remove the border top (if next table row is selected)
-					":has(+ tr[aria-selected='true']):after": {
-						borderBottomWidth: 0,
-					},
+								// Remove the border top (if next table row is selected)
+								":has(+ tr[aria-selected='true']):after": {
+									borderBottomWidth: 0,
+								},
 
-					// Remove the border top from the next table row (if next table row is selected)
-					'+ tr:after': {
-						borderTopWidth: 0,
-					},
+								// Remove the border top from the next table row (if next table row is selected)
+								'+ tr:after': {
+									borderTopWidth: 0,
+								},
+							}),
 
-					// Chrome and Firefox doesn't support :after elements in fixed table layouts
-					// FIXME Once Chrome Firefox fixes this issue, this code should be removed
-					...(tableLayout === 'fixed' ? alternativeSelectedStyles : undefined),
+							// Chrome and Firefox doesn't support :after elements in fixed table layouts
+							// FIXME Once Chrome Firefox fixes this issue, these alternative styles should be removed
+							...(tableLayout === 'fixed'
+								? alternativeSelectedStyles
+								: undefined),
 
-					// Safari does not support relative positioning on `tr` elements
-					// FIXME Once safari fixes this issue, this code should be removed
-					// More info https://www.reddit.com/r/css/comments/s195xg/safari_alternative_to_positionrelative_on_tr/?rdt=41288
-					'@supports (-webkit-appearance: -apple-pay-button)':
-						alternativeSelectedStyles,
-				}),
-			}}
+							// Safari does not support relative positioning on `tr` elements
+							// FIXME Once safari fixes this issue, these alternative styles should be removed
+							// More info https://www.reddit.com/r/css/comments/s195xg/safari_alternative_to_positionrelative_on_tr/?rdt=41288
+							'@supports (-webkit-appearance: -apple-pay-button)':
+								alternativeSelectedStyles,
+					  }
+					: undefined
+			}
 		>
 			{children}
 		</tr>
@@ -57,6 +63,7 @@ export function TableRow({ children, selected }: TableRowProps) {
 
 // Use an outline instead of an :after element
 const alternativeSelectedStyles = {
+	backgroundColor: boxPalette.selectedMuted,
 	outlineWidth: '2px',
 	outlineStyle: 'solid',
 	outlineColor: boxPalette.selected,
