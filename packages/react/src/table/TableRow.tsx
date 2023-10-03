49a35307bff010cee47ab551c2554d91a8715ebd
Jordan Overbye <6265154+jordanoverbye@users.noreply.github.com>
2023-10-03 15:13:25 +1000

    table: Fix selectable rows when using fixed table layout (#1407)

--- packages/react/src/table/TableRow.tsx
+++ packages/react/src/table/TableRow.tsx
@@ -1,5 +1,6 @@
 import { PropsWithChildren } from 'react';
 import { boxPalette, tokens } from '../core';
+import { useTableContext } from './TableContext';
 
 export type TableRowProps = PropsWithChildren<{
 	/** Indicates the current selected state of the table row. */
@@ -7,51 +8,65 @@ export type TableRowProps = PropsWithChildren<{
 }>;
 
 export function TableRow({ children, selected }: TableRowProps) {
+	const { tableLayout } = useTableContext();
 	return (
 		<tr
 			aria-selected={selected}
-			css={{
-				...(selected && {
-					position: 'relative',
-					backgroundColor: `${boxPalette.selectedMuted}`,
+			css={
+				selected
+					? {
+							...(tableLayout === 'auto' && {
+								position: 'relative',
+								backgroundColor: boxPalette.selectedMuted,
 
-					// Add outline
-					'&:after': {
-						content: '""',
-						pointerEvents: 'none',
-						position: 'absolute',
-						inset: 0,
-						borderWidth: tokens.borderWidth.md,
-						borderColor: boxPalette.selected,
-						borderStyle: 'solid',
-					},
+								// Add outline
+								'&:after': {
+									content: '""',
+									pointerEvents: 'none',
+									position: 'absolute',
+									inset: 0,
+									borderWidth: tokens.borderWidth.md,
+									borderColor: boxPalette.selected,
+									borderStyle: 'solid',
+								},
 
-					// Remove the border top (if next table row is selected)
-					":has(+ tr[aria-selected='true']):after": {
-						borderBottomWidth: 0,
-					},
+								// Remove the border top (if next table row is selected)
+								":has(+ tr[aria-selected='true']):after": {
+									borderBottomWidth: 0,
+								},
 
-					// Remove the border top from the next table row (if next table row is selected)
-					'+ tr:after': {
-						borderTopWidth: 0,
-					},
+								// Remove the border top from the next table row (if next table row is selected)
+								'+ tr:after': {
+									borderTopWidth: 0,
+								},
+							}),
 
-					// Safari does not support relative positioning on `tr` elements
-					// So we're using an outline instead of the :after element to prevent the outline from being rendered incorrectly
-					// FIXME Once safari fixes this issue, this code should be removed
-					// More info https://www.reddit.com/r/css/comments/s195xg/safari_alternative_to_positionrelative_on_tr/?rdt=41288
-					'@supports (-webkit-appearance: -apple-pay-button)': {
-						outlineWidth: '2px',
-						outlineStyle: 'solid',
-						outlineColor: boxPalette.selected,
-						outlineOffset: '-3px',
+							// Chrome and Firefox doesn't support :after elements in fixed table layouts
+							// FIXME Once Chrome Firefox fixes this issue, these alternative styles should be removed
+							...(tableLayout === 'fixed'
+								? alternativeSelectedStyles
+								: undefined),
 
-						'&:after': { display: 'none' },
-					},
-				}),
-			}}
+							// Safari does not support relative positioning on `tr` elements
+							// FIXME Once safari fixes this issue, these alternative styles should be removed
+							// More info https://www.reddit.com/r/css/comments/s195xg/safari_alternative_to_positionrelative_on_tr/?rdt=41288
+							'@supports (-webkit-appearance: -apple-pay-button)':
+								alternativeSelectedStyles,
+					  }
+					: undefined
+			}
 		>
 			{children}
 		</tr>
 	);
 }
+
+// Use an outline instead of an :after element
+const alternativeSelectedStyles = {
+	backgroundColor: boxPalette.selectedMuted,
+	outlineWidth: '2px',
+	outlineStyle: 'solid',
+	outlineColor: boxPalette.selected,
+	outlineOffset: '-3px',
+	'&:after': { display: 'none' },
+};
