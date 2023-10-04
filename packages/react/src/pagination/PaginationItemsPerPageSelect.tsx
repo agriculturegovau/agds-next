0a1eac0d57c2c33e1f19812cdaaff81a5897a118
Nathan Simpson <hello@nathansimpson.design>
2023-10-04 17:29:24 +1100

    Use ID for accessibility

--- packages/react/src/pagination/PaginationItemsPerPageSelect.tsx
+++ packages/react/src/pagination/PaginationItemsPerPageSelect.tsx
@@ -5,6 +5,7 @@ export const PaginationItemsPerPageSelect = ({
 	value,
 	options = [10, 20, 50, 100],
 	onChange,
+	id = 'pagination-items-per-page',
 }: {
 	/** The selected number of items per page. */
 	value: number;
@@ -12,6 +13,8 @@ export const PaginationItemsPerPageSelect = ({
 	options?: number[];
 	/** Callback when the items per page is changed. */
 	onChange: (value: number) => void;
+	/** The id of the items-per-page select. */
+	id?: string;
 }) => {
 	const uid = useId();
 	const id = `pagination-per-page-select-${uid}`;
