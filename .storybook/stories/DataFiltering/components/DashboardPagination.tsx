9cc700ce17a34840d9e578823bbabfef2deda75e
Nathan Simpson <hello@nathansimpson.design>
2023-10-04 16:24:36 +1100

    Update table filtering examples

--- .storybook/stories/DataFiltering/components/DashboardPagination.tsx
+++ .storybook/stories/DataFiltering/components/DashboardPagination.tsx
@@ -3,6 +3,7 @@ import {
 	generatePaginationRangeText,
 } from '@ag.ds-next/react/pagination';
 import { useDataContext, useSortAndFilterContext } from '../lib/contexts';
+import { GetDataPagination } from '../lib/getData';
 
 export const DashboardPagination = () => {
 	const { pagination, setPagination } = useSortAndFilterContext();
@@ -28,4 +29,26 @@ export const DashboardPagination = () => {
 			}
 		/>
 	);
+};
+
+export const getPaginationText = ({
+	loading,
+	totalItems,
+	pagination,
+}: {
+	loading: boolean;
+	totalItems: number;
+	pagination: GetDataPagination;
+}) => {
+	if (loading) return 'Audits';
+
+	const totalItemsPlural = totalItems === 1 ? '1 item' : `${totalItems} items`;
+	const firstItem = (pagination.page - 1) * pagination.perPage + 1;
+	const lastItem =
+		(pagination.page - 1) * pagination.perPage + pagination.perPage;
+
+	const rangeExceedsTotalItems = lastItem > totalItems;
+	if (rangeExceedsTotalItems) return `Audits (${totalItemsPlural})`;
+
+	return `${firstItem} - ${lastItem} of ${totalItemsPlural} Audits`;
 };
