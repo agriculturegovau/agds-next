639537c4ac97b0de465e3762646f9690c6ba18e2
Nathan Simpson <hello@nathansimpson.design>
2023-10-09 12:32:54 +1100

    PaginationSecondaryControlContainer

--- packages/react/src/pagination/PaginationContainer.tsx
+++ packages/react/src/pagination/PaginationContainer.tsx
@@ -1,4 +1,4 @@
-import type { PropsWithChildren } from 'react';
+import type { PropsWithChildren, ReactNode } from 'react';
 import { Flex } from '../flex';
 
 export type PaginationItemContainerProps = PropsWithChildren<{
