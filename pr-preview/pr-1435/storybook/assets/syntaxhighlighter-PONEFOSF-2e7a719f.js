19055fb387becc400cd1f0efeb719b20ed97c9f1
nathsimpson <nathsimpson@users.noreply.github.com>
2023-10-16 22:22:39 +0000

    Remove preview for PR 1435 🛬

--- pr-preview/pr-1435/storybook/assets/syntaxhighlighter-PONEFOSF-2e7a719f.js
+++
@@ -1,2 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-3PUGWF3O-2d99b17b.js";import"./iframe-4cb4b5e8.js";import"../sb-preview/runtime.js";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
-//# sourceMappingURL=syntaxhighlighter-PONEFOSF-2e7a719f.js.map
