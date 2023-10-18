3a66a0df9b3c6820750cffebe0bbc785bc62fa2b
Luke Bennett <hello@lukebennett.com.au>
2023-10-18 12:39:26 +1100

    dropdown: Minor updates to state reducer (#1442)
    Co-authored-by: Jordan Overbye <6265154+jordanoverbye@users.noreply.github.com>

---
+++ .changeset/wet-ways-dance.md
@@ -0,0 +1,5 @@
+---
+'@ag.ds-next/react': patch
+---
+
+dropdown-menu: Minor improvements to internal state reducer
--- packages/react/src/dropdown-menu/reducer.tsx
+++ packages/react/src/dropdown-menu/reducer.tsx
@@ -27,29 +27,53 @@ type Action =
 	  };
 
 type State = {
-	/** The visibility of the dropdown menu. */
-	isMenuOpen: boolean;
-	/** The index of the active descendantIndex. -1 means there is no active descendant. */
+	/**
+	 * The index of the active descendantIndex.
+	 * -1 means there is no active descendant.
+	 *
+	 * @default -1
+	 */
 	activeDescendantIndex: number;
+
 	/** The HTML node elements containing each menu item (`DropdownMenuItem`, `DropdownMenuLInk` etc). */
 	descendantNodes: NodeListOf<HTMLDivElement> | undefined;
-	/** The count of descendant nodes. */
-	descendantCount: number;
+
 	/**
-	 * Typing any a-z character should focus the menu item with a label that starts with the typed character (if exists)
-	 * https://www.w3.org/WAI/ARIA/apg/patterns/menu-button/examples/menu-button-actions-active-descendant/
+	 * The count of descendant nodes.
+	 *
+	 * @default 0
+	 */
+	descendantCount: number;
+
+	/**
+	 * Typing any a-z character should focus the menu item with a label that starts
+	 * with the typed character (if exists).
+	 * @see https://www.w3.org/WAI/ARIA/apg/patterns/menu-button/examples/menu-button-actions-active-descendant/
 	 */
 	descendantSearchTerm: string;
-	/** Time of the last keypress for handling quick successive keypresses. Value is a timestamp in milliseconds. */
+
+	/**
+	 * Indicates if the menu is open.
+	 *
+	 * @default false
+	 */
+	isMenuOpen: boolean;
+
+	/**
+	 * Time of the last key press for handling quick successive key presses.
+	 * Value is a timestamp in milliseconds.
+	 *
+	 * @default 0
+	 */
 	lastKeyPressTime: number;
 };
 
 export const initialState: State = {
-	isMenuOpen: false,
 	activeDescendantIndex: -1,
-	descendantNodes: undefined,
 	descendantCount: 0,
+	descendantNodes: undefined,
 	descendantSearchTerm: '',
+	isMenuOpen: false,
 	lastKeyPressTime: 0,
 };
 
@@ -57,50 +81,53 @@ export function reducer(state: State, action: Action): State {
 	const currentTime = new Date().getTime();
 
 	switch (action.type) {
-		case 'OPEN_MENU':
-			return { ...state, isMenuOpen: true, descendantSearchTerm: '' };
-
-		case 'CLOSE_MENU':
-			// When the menu closes, the state of the dropdown menu should be reset
-			return initialState;
-
-		case 'SET_DESCENDANT_NODES':
+		case 'OPEN_MENU': {
 			return {
 				...state,
+				isMenuOpen: true,
+			};
+		}
+
+		case 'CLOSE_MENU': {
+			// When the menu closes, the state of the dropdown menu should be reset
+			return initialState;
+		}
+
+		case 'SET_DESCENDANT_NODES': {
+			return {
+				...state,
+				descendantCount: action.payload.nodes?.length ?? 0,
 				descendantNodes: action.payload.nodes,
-				descendantCount: action.payload.nodes?.length
-					? action.payload.nodes.length - 1
-					: 0,
 				descendantSearchTerm: '',
 				lastKeyPressTime: 0,
 			};
+		}
 
-		case 'ACTIVATE_FIRST_DESCENDANT':
+		case 'ACTIVATE_FIRST_DESCENDANT': {
 			return {
 				...state,
 				activeDescendantIndex: 0,
 				descendantSearchTerm: '',
 				lastKeyPressTime: 0,
 			};
+		}
 
-		case 'ACTIVATE_LAST_DESCENDANT':
+		case 'ACTIVATE_LAST_DESCENDANT': {
 			return {
 				...state,
-				activeDescendantIndex: state.descendantCount,
+				activeDescendantIndex: state.descendantCount - 1,
 				descendantSearchTerm: '',
 				lastKeyPressTime: 0,
 			};
+		}
 
 		case 'ACTIVATE_NEXT_DESCENDANT': {
-			let newActiveDescendantIndex = 0;
-
 			// If current active descendant is not the last, increment index by 1
-			if (state.activeDescendantIndex < state.descendantCount) {
-				newActiveDescendantIndex = state.activeDescendantIndex + 1;
-			} else {
-				// If last descendant is currently active, loop back to the first
-				newActiveDescendantIndex = 0;
-			}
+			const newActiveDescendantIndex =
+				state.activeDescendantIndex < state.descendantCount - 1
+					? state.activeDescendantIndex + 1
+					: // If last descendant is currently active, loop back to the first
+					  0;
 
 			// Update the state with the new active descendant index and clear search term
 			return {
@@ -112,15 +139,12 @@ export function reducer(state: State, action: Action): State {
 		}
 
 		case 'ACTIVATE_PREVIOUS_DESCENDANT': {
-			let newActiveDescendantIndex = 0;
-
 			// If current active descendant is not the first, decrement index by 1
-			if (state.activeDescendantIndex > 0) {
-				newActiveDescendantIndex = state.activeDescendantIndex - 1;
-			} else {
-				// If first descendant is currently active, loop to the last
-				newActiveDescendantIndex = state.descendantCount;
-			}
+			const newActiveDescendantIndex =
+				state.activeDescendantIndex > 0
+					? state.activeDescendantIndex - 1
+					: // If first descendant is currently active, loop to the last
+					  state.descendantCount - 1;
 
 			// Update the state with the new active descendant index and clear search term
 			return {
@@ -179,7 +203,8 @@ export function reducer(state: State, action: Action): State {
 			};
 		}
 
-		default:
-			return state;
+		default: {
+			throw new Error(`Unhandled action type: ${(action as Action).type}`);
+		}
 	}
 }
