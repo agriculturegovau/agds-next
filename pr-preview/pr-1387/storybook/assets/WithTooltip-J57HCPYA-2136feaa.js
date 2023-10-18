4a3b7c1d2544b7699611f9736427ebb241416194
Jordan Overbye <jordanoverbye@gmail.com>
2023-10-18 11:30:09 +1000

    delete old pr previews

--- pr-preview/pr-1387/storybook/assets/WithTooltip-J57HCPYA-2136feaa.js
+++
@@ -1,2 +0,0 @@
-import{b as r,b as a,d as T}from"./DocsRenderer-3PUGWF3O-13e48756.js";import"./iframe-d9ff37cf.js";import"../sb-preview/runtime.js";export{r as WithToolTipState,a as WithTooltip,T as WithTooltipPure};
-//# sourceMappingURL=WithTooltip-J57HCPYA-2136feaa.js.map
--- pr-preview/pr-1387/storybook/assets/syntaxhighlighter-PONEFOSF-071b7dca.js
+++
@@ -1,2 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-3PUGWF3O-13e48756.js";import"./iframe-d9ff37cf.js";import"../sb-preview/runtime.js";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
-//# sourceMappingURL=syntaxhighlighter-PONEFOSF-071b7dca.js.map
