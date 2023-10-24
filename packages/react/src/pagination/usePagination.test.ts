eced5863244a1b87c991b0f240a08609241dbf10
Jordan Overbye <6265154+jordanoverbye@users.noreply.github.com>
2023-10-24 10:33:47 +1000

    pagination: Improve divider logic (#1452)

--- packages/react/src/pagination/usePagination.test.ts
+++ packages/react/src/pagination/usePagination.test.ts
@@ -16,6 +16,7 @@ describe('usePagination', () => {
 			{ type: 'direction', pageNumber: 2, direction: 'right' },
 		]);
 	});
+
 	test('middle of list', () => {
 		const result = usePagination({
 			currentPage: 5,
@@ -34,6 +35,7 @@ describe('usePagination', () => {
 			{ type: 'direction', pageNumber: 6, direction: 'right' },
 		]);
 	});
+
 	test('end of list', () => {
 		const result = usePagination({
 			currentPage: 10,
@@ -48,5 +50,85 @@ describe('usePagination', () => {
 			{ type: 'page', pageNumber: 9, isActive: false },
 			{ type: 'page', pageNumber: 10, isActive: true },
 		]);
+	});
+
+	describe('removed unwanted separators from start of list', () => {
+		test('from start of list', () => {
+			const result = usePagination({
+				currentPage: 4,
+				totalPages: 10,
+				windowLimit: 3,
+			});
+			expect(result).toEqual([
+				{ type: 'direction', pageNumber: 3, direction: 'left' },
+				{ type: 'page', pageNumber: 1, isActive: false },
+				{ type: 'page', pageNumber: 2, isActive: false },
+				{ type: 'page', pageNumber: 3, isActive: false },
+				{ type: 'page', pageNumber: 4, isActive: true },
+				{ type: 'page', pageNumber: 5, isActive: false },
+				{ type: 'separator' },
+				{ type: 'page', pageNumber: 10, isActive: false },
+				{ type: 'direction', pageNumber: 5, direction: 'right' },
+			]);
+		});
+
+		test('from start of list with alt window limit', () => {
+			const result = usePagination({
+				currentPage: 4,
+				totalPages: 10,
+				windowLimit: 5,
+			});
+			expect(result).toEqual([
+				{ type: 'direction', pageNumber: 3, direction: 'left' },
+				{ type: 'page', pageNumber: 1, isActive: false },
+				{ type: 'page', pageNumber: 2, isActive: false },
+				{ type: 'page', pageNumber: 3, isActive: false },
+				{ type: 'page', pageNumber: 4, isActive: true },
+				{ type: 'page', pageNumber: 5, isActive: false },
+				{ type: 'page', pageNumber: 6, isActive: false },
+				{ type: 'separator' },
+				{ type: 'page', pageNumber: 10, isActive: false },
+				{ type: 'direction', pageNumber: 5, direction: 'right' },
+			]);
+		});
+
+		test('from end of list', () => {
+			const result = usePagination({
+				currentPage: 7,
+				totalPages: 10,
+				windowLimit: 3,
+			});
+			expect(result).toEqual([
+				{ type: 'direction', pageNumber: 6, direction: 'left' },
+				{ type: 'page', pageNumber: 1, isActive: false },
+				{ type: 'separator' },
+				{ type: 'page', pageNumber: 6, isActive: false },
+				{ type: 'page', pageNumber: 7, isActive: true },
+				{ type: 'page', pageNumber: 8, isActive: false },
+				{ type: 'page', pageNumber: 9, isActive: false },
+				{ type: 'page', pageNumber: 10, isActive: false },
+				{ type: 'direction', pageNumber: 8, direction: 'right' },
+			]);
+		});
+
+		test('from end of list with alt window limit', () => {
+			const result = usePagination({
+				currentPage: 7,
+				totalPages: 10,
+				windowLimit: 5,
+			});
+			expect(result).toEqual([
+				{ type: 'direction', pageNumber: 6, direction: 'left' },
+				{ type: 'page', pageNumber: 1, isActive: false },
+				{ type: 'separator' },
+				{ type: 'page', pageNumber: 5, isActive: false },
+				{ type: 'page', pageNumber: 6, isActive: false },
+				{ type: 'page', pageNumber: 7, isActive: true },
+				{ type: 'page', pageNumber: 8, isActive: false },
+				{ type: 'page', pageNumber: 9, isActive: false },
+				{ type: 'page', pageNumber: 10, isActive: false },
+				{ type: 'direction', pageNumber: 8, direction: 'right' },
+			]);
+		});
 	});
 });
