3f44358483dd4e426f65a725c732b3dcbcf0bdfd
Jordan Overbye <jordanoverbye@gmail.com>
2023-10-25 13:42:07 +1000

    docs

--- packages/react/src/autocomplete/Autocomplete.stories.tsx
+++ packages/react/src/autocomplete/Autocomplete.stories.tsx
@@ -1,6 +1,8 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { useState } from 'react';
-import { COUNTRY_OPTIONS } from '../combobox/test-utils';
+import { Avatar } from '../avatar';
+import { COUNTRY_OPTIONS, NAME_OPTIONS } from '../combobox/test-utils';
+import { NotificationBadge } from '../notification-badge';
 import { Autocomplete } from './Autocomplete';
 import { AutocompleteRenderItem } from './AutocompleteRenderItem';
 
@@ -66,56 +68,58 @@ export const ExternalAPI = {
 	args: {
 		hideOptionalLabel: true,
 		label: 'Choose a Star Wars character',
-		loadOptions: loadOptionsFromStarWarsApi,
+		loadOptions: async function loadOptionsFromStarWarsApi(inputValue: string) {
+			const response = await fetch(
+				`https://swapi.dev/api/people/?search=${inputValue}`
+			).then((r) => r.json());
+			return response.results.map(
+				(result: { name: string; birth_year: string; hair_color: string }) => ({
+					label: result.name,
+					value: result.name,
+					name: result.name,
+					birthYear: result.birth_year,
+					hairColor: result.hair_color,
+				})
+			);
+		},
 	},
 };
 
+type NameOption = (typeof NAME_OPTIONS)[number];
+
 export const CustomRender: Story = {
 	render: function Render() {
-		const [value, onChange] = useState<StarWarsCharacterOption | null>(null);
+		const [value, onChange] = useState<NameOption | null>(null);
 		return (
 			<Autocomplete
-				label="Choose a Star Wars character"
-				hint="Start typing to see results"
-				hideOptionalLabel
+				label="Search users"
 				value={value}
 				onChange={onChange}
-				loadOptions={loadOptionsFromStarWarsApi}
+				loadOptions={async function loadOptions() {
+					// Simulate a slow network connection
+					await new Promise((resolve) => setTimeout(resolve, 1000));
+					return NAME_OPTIONS;
+				}}
 				renderItem={(item, inputValue) => (
 					<AutocompleteRenderItem
 						itemLabel={item.label}
 						inputValue={inputValue}
-						secondaryText={`Birth year: ${item.birthYear}`}
-						tertiaryText={`Job: ${item.hairColor}`}
+						secondaryText={`Role: ${item.jobTitle}`}
+						tertiaryText={`Job: ${item.status}`}
+						beforeElement={
+							<Avatar name={item.fullName} size="sm" tone="action" />
+						}
+						endElement={
+							item.unreadMessageCount > 0 ? (
+								<NotificationBadge
+									value={item.unreadMessageCount}
+									tone="action"
+								/>
+							) : null
+						}
 					/>
 				)}
 			/>
 		);
 	},
 };
-
-type StarWarsCharacterOption = {
-	label: string;
-	value: string;
-	name: string;
-	birthYear: string;
-	hairColor: string;
-};
-
-async function loadOptionsFromStarWarsApi(
-	inputValue: string
-): Promise<StarWarsCharacterOption[]> {
-	const response = await fetch(
-		`https://swapi.dev/api/people/?search=${inputValue}`
-	).then((r) => r.json());
-	console.log({ response });
-	return response.results.map(
-		(result: { name: string; birth_year: string; hair_color: string }) => ({
-			label: result.name,
-			value: result.name,
-			name: result.name,
-			birthYear: result.birth_year,
-			hairColor: result.hair_color,
-		})
-	);
-}
