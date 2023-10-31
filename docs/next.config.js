f94f19afbcb0665d1d0d8d6081879b50cc3c47eb
Jordan Overbye <jordanoverbye@gmail.com>
2023-11-01 09:31:50 +1000

    update next config

--- docs/next.config.js
+++ docs/next.config.js
@@ -14,6 +14,17 @@ const nextConfig = {
 		NEXT_PUBLIC_GITHUB_PR_PREVIEW_NUMBER: process.env.GITHUB_PR_PREVIEW_NUMBER,
 		NEXT_PUBLIC_BASE_PATH: basePath,
 	},
+	experimental: {
+		/**
+		 * WARNING: If you are a consumer of the design system reading this you DO NOT need this config in your project.
+		 *
+		 * Disabling `esmExternals` fixes an issue with `@floating-ui/react-dom` which is a dependency of `@ag.ds-next/react`.
+		 * This is due to a combination of how preconstruct and next handle dependencies with ESM modules.
+		 * This issue only occurs for dependencies in the monorepo - so consumers of the design system do not need to worry about this.
+		 * This was done in collaboration with [Emma Hamilton](https://github.com/emmatown)
+		 */
+		esmExternals: false,
+	},
 };
 
 module.exports = withPreconstruct(nextConfig);
