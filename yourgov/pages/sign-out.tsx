d91ca2ae373ef7978aa33c161bdfece40833de28
Jordan Overbye <jordanoverbye@gmail.com>
2023-11-09 13:19:29 +1000

    sign out

--- yourgov/pages/sign-out.tsx
+++ yourgov/pages/sign-out.tsx
@@ -1,36 +1,36 @@
-import { useEffect, useState, Fragment } from 'react';
+import { useEffect, Fragment } from 'react';
 import { H1 } from '@ag.ds-next/react/heading';
 import { Stack } from '@ag.ds-next/react/stack';
 import { PageContent } from '@ag.ds-next/react/content';
-import { TextLink } from '@ag.ds-next/react/text-link';
+import { PageAlert } from '@ag.ds-next/react/page-alert';
+import { Text } from '@ag.ds-next/react/text';
+import { Column, Columns } from '@ag.ds-next/react/columns';
 import { DocumentTitle } from '../components/DocumentTitle';
 import { SiteLayout } from '../components/Layout/SiteLayout';
 import { useAuth } from '../lib/useAuth';
 
 export default function Page() {
-	const [signingOut, setSigningOut] = useState(true);
 	const { signOut } = useAuth();
 
 	useEffect(() => {
-		setTimeout(() => {
-			setSigningOut(false);
-			signOut();
-		}, 2000);
+		signOut();
 	}, [signOut]);
 
 	return (
 		<Fragment>
 			<DocumentTitle title="Sign out" />
-			<SiteLayout focusMode>
+			<SiteLayout>
 				<PageContent>
-					<Stack gap={1.5}>
-						<H1>
-							{signingOut
-								? 'You are being signed out ...'
-								: 'You have been signed out'}
-						</H1>
-						{!signingOut && <TextLink href="/">Back to home</TextLink>}
-					</Stack>
+					<Columns>
+						<Column columnSpan={{ xs: 12, md: 7 }}>
+							<Stack gap={1.5}>
+								<H1>Sign out</H1>
+								<PageAlert tone="success" title="Sign out successful">
+									<Text>You have been successfully signed out.</Text>
+								</PageAlert>
+							</Stack>
+						</Column>
+					</Columns>
 				</PageContent>
 			</SiteLayout>
 		</Fragment>
