3a9b00ee64e0c541446aa25f13293032b0d4222d
jordanoverbye <jordanoverbye@users.noreply.github.com>
2023-11-13 23:51:18 +0000

    Remove preview for PR 1491 🛬

--- pr-preview/pr-1491/storybook/assets/syntaxhighlighter-PONEFOSF-36da37c0.js
+++
@@ -1,2 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-3PUGWF3O-886ec5da.js";import"./iframe-e60485c1.js";import"../sb-preview/runtime.js";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
-//# sourceMappingURL=syntaxhighlighter-PONEFOSF-36da37c0.js.map
