e40baf83ef4484fd7fdc7bfd914e8004ac71b312
danger17 <danger17@users.noreply.github.com>
2023-12-01 06:02:26 +0000

    Remove preview for PR 1513 🛬

--- pr-preview/pr-1513/storybook/assets/syntaxhighlighter-PONEFOSF-782ef623.js
+++
@@ -1,2 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-3PUGWF3O-65d524fa.js";import"./iframe-35015216.js";import"../sb-preview/runtime.js";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
-//# sourceMappingURL=syntaxhighlighter-PONEFOSF-782ef623.js.map
