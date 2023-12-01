e2bbf35bda957038b8e41b560ad0a66e4d3fdcb9
Matt Stow <matt@mattstow.com>
2023-12-01 15:18:04 +1100

    table: Allow focus on TableWrapper to fix a11y for KB users (#1517)

--- packages/react/src/table/TableWrapper.tsx
+++ packages/react/src/table/TableWrapper.tsx
@@ -1,5 +1,10 @@
 import { ReactNode } from 'react';
+import { Box } from '@ag.ds-next/react/box';
 
 export const TableWrapper = ({ children }: { children: ReactNode }) => {
-	return <div css={{ overflowX: 'auto' }}>{children}</div>;
+	return (
+		<Box css={{ overflowX: 'auto' }} focus tabIndex={0}>
+			{children}
+		</Box>
+	);
 };
