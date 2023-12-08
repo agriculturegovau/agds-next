df0ce915ee110bda8f1bb381514bc865cfa6e13d
stowball <stowball@users.noreply.github.com>
2023-12-08 03:59:19 +0000

    Remove preview for PR 1522 🛬

--- pr-preview/pr-1522/storybook/assets/syntaxhighlighter-PONEFOSF-17c60f10.js
+++
@@ -1,2 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-3PUGWF3O-1dca1d80.js";import"./iframe-7738b07e.js";import"../sb-preview/runtime.js";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
-//# sourceMappingURL=syntaxhighlighter-PONEFOSF-17c60f10.js.map
