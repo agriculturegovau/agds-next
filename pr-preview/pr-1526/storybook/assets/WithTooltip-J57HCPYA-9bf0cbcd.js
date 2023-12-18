9bcee4ef31e4331fcd14a5d7c7ae83028168e3e7
stowball <stowball@users.noreply.github.com>
2023-12-18 22:30:54 +0000

    Remove preview for PR 1526 🛬

--- pr-preview/pr-1526/storybook/assets/WithTooltip-J57HCPYA-9bf0cbcd.js
+++
@@ -1,2 +0,0 @@
-import{b as r,b as a,d as T}from"./DocsRenderer-3PUGWF3O-e06d0b8e.js";import"./iframe-e5b00e87.js";import"../sb-preview/runtime.js";export{r as WithToolTipState,a as WithTooltip,T as WithTooltipPure};
-//# sourceMappingURL=WithTooltip-J57HCPYA-9bf0cbcd.js.map
--- pr-preview/pr-1526/storybook/assets/syntaxhighlighter-PONEFOSF-d09f983f.js
+++
@@ -1,2 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-3PUGWF3O-e06d0b8e.js";import"./iframe-e5b00e87.js";import"../sb-preview/runtime.js";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
-//# sourceMappingURL=syntaxhighlighter-PONEFOSF-d09f983f.js.map
