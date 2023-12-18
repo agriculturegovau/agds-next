4ba6cf84f4840ee5e5bec2b005c8445361d4e592
Matt Stow <matt@mattstow.com>
2023-12-18 12:51:51 +1100

    table: Add colSpan & rowSpan support to TableCell & TableHeader (#1527)

---
+++ .changeset/swift-countries-juggle.md
@@ -0,0 +1,7 @@
+---
+'@ag.ds-next/react': minor
+---
+
+table: Add `colSpan` and `rowSpan` support to `TableCell` and `TableHeader`.
+table: Add `as` support to `TableHeader`.
+table: Fixed `scope` on `TableHeader`.
--- packages/react/src/table/TableHeader.tsx
+++ packages/react/src/table/TableHeader.tsx
@@ -3,8 +3,14 @@ import { Box } from '../box';
 import { ResponsiveProp } from '../core';
 
 export type TableHeaderProps = PropsWithChildren<{
+	/** The HTML element to render. */
+	as?: 'td' | 'th';
+	/** Specifies how many columns this cell spans */
+	colSpan?: number;
 	/** Can be used to conditionally hide or show table cells at different breakpoints. */
 	display?: ResponsiveProp<'none' | 'table-cell'>;
+	/** Specifies how many rows this cell spans */
+	rowSpan?: number;
 	/** Defines the cells that the header (defined in the <th>) element relates to. */
 	scope?: 'col' | 'row' | 'colgroup' | 'rowgroup';
 	/** Sets the horizontal alignment of the content. */
@@ -14,20 +20,27 @@ export type TableHeaderProps = PropsWithChildren<{
 }>;
 
 export const TableHeader = ({
+	as = 'th',
 	children,
+	colSpan,
+	rowSpan,
+	scope,
 	textAlign = 'left',
 	width,
 	...props
 }: TableHeaderProps) => {
 	return (
 		<Box
-			as="th"
+			as={as}
 			color="text"
-			padding={0.75}
-			fontWeight="bold"
+			colSpan={colSpan}
 			focus
-			width={width}
+			fontWeight="bold"
+			padding={0.75}
+			rowSpan={rowSpan}
+			scope={scope}
 			textAlign={textAlign}
+			width={width}
 			{...props}
 		>
 			{children}
