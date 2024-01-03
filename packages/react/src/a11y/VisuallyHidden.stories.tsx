8faaebd98a1be9d981aa684ec769b459841c8049
Jordan Overbye <jordanoverbye@gmail.com>
2024-01-03 11:03:07 +1000

    a11y

--- packages/react/src/a11y/VisuallyHidden.stories.tsx
+++ packages/react/src/a11y/VisuallyHidden.stories.tsx
@@ -1,33 +1,49 @@
-import { ComponentMeta } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 import { Stack } from '../stack';
 import { Text } from '../text';
 import { TextLink } from '../text-link';
 import { H1 } from '../heading';
 import { VisuallyHidden, visuallyHiddenStyles } from './VisuallyHidden';
 
-export default {
+const meta: Meta<typeof VisuallyHidden> = {
 	title: 'foundations/A11y/VisuallyHidden',
 	component: VisuallyHidden,
-} as ComponentMeta<typeof VisuallyHidden>;
+};
 
-export const Basic = () => (
-	<Stack gap={1}>
-		<H1>VisuallyHidden</H1>
-		<Text>
-			Interact with the link in this example using Apple VoiceOver or your
-			chosen screen reader.
-		</Text>
+export default meta;
 
-		<TextLink href="#">
-			Read more
-			<VisuallyHidden> about how to visually hide content</VisuallyHidden>
-		</TextLink>
-	</Stack>
-);
+type Story = StoryObj<typeof VisuallyHidden>;
 
-export const Styles = () => (
-	<TextLink href="#">
-		Read more
-		<span css={visuallyHiddenStyles}>about how to visually hide content</span>
-	</TextLink>
-);
+export const Basic: Story = {
+	render: () => (
+		<Stack gap={1}>
+			<H1>VisuallyHidden</H1>
+			<Text>
+				Interact with the link in this example using Apple VoiceOver or your
+				chosen screen reader.
+			</Text>
+			<TextLink href="#">
+				Read more
+				<VisuallyHidden> about how to visually hide content</VisuallyHidden>
+			</TextLink>
+		</Stack>
+	),
+};
+
+export const Styles: Story = {
+	render: () => (
+		<Stack gap={1}>
+			<H1>visuallyHiddenStyles</H1>
+			<Text>
+				Interact with the link in this example using Apple VoiceOver or your
+				chosen screen reader.
+			</Text>
+			<TextLink href="#">
+				Read more
+				<span css={visuallyHiddenStyles}>
+					about how to visually hide content
+				</span>
+			</TextLink>
+		</Stack>
+	),
+};
