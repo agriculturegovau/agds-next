9b8f04b14277463f2f80a1e006b0d3ac49b3cd0b
stowball <stowball@users.noreply.github.com>
2024-01-15 04:43:42 +0000

    Remove preview for PR 1550 🛬

--- pr-preview/pr-1550/storybook/assets/syntaxhighlighter-V7JZZA35-1d09f9bd.js
+++
@@ -1,1 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-NNNQARDV-ca51270b.js";import"./iframe-69aabb89.js";import"../sb-preview/runtime.js";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
