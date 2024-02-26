2764e5832125c43598ae169cb9a6263a404f13a4
stowball <stowball@users.noreply.github.com>
2024-02-26 22:43:05 +0000

    Remove preview for PR 1562 🛬

--- pr-preview/pr-1562/storybook/assets/syntaxhighlighter-V7JZZA35-44299b8f.js
+++
@@ -1,1 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-NNNQARDV-236e0280.js";import"./iframe-304bedd5.js";import"../sb-preview/runtime.js";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
