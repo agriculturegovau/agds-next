962ce5bdd63b0cffec8283bae2751ca5b6e49b80
stowball <stowball@users.noreply.github.com>
2024-02-27 05:09:04 +0000

    Remove preview for PR 1566 🛬

--- pr-preview/pr-1566/storybook/assets/WithTooltip-V3YHNWJZ-49be855f.js
+++
@@ -1,1 +0,0 @@
-import{W as a,W as r,a as W}from"./DocsRenderer-NNNQARDV-4f28b8a0.js";import"./iframe-8365c35b.js";import"../sb-preview/runtime.js";export{a as WithToolTipState,r as WithTooltip,W as WithTooltipPure};
--- pr-preview/pr-1566/storybook/assets/syntaxhighlighter-V7JZZA35-3e39aab1.js
+++
@@ -1,1 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-NNNQARDV-4f28b8a0.js";import"./iframe-8365c35b.js";import"../sb-preview/runtime.js";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
