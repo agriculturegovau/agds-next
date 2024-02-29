c6c8c5d68f8503603eb019072d56f9bc7a2e5dbe
Daniel Tonon <dan503.npm@gmail.com>
2024-02-29 14:42:32 +1100

    isTruthy function is not used

--- docs/components/PageLayout.tsx
+++ docs/components/PageLayout.tsx
@@ -25,21 +25,6 @@ type PageLayoutProps = PropsWithChildren<{
 	};
 }>;
 
-// https://stackoverflow.com/a/58110124/1611058
-/**
- * A strongly typed version of `Boolean()`
- *
- * When using this, TS understands that you are using it to guard against false values.
- *
- * @param value - Any value you want to test the truthiness of
- * @returns Boolean
- */
-export function isTruthy<Test>(value: Test): value is Truthy<Test> {
-	return Boolean(value);
-}
-type Falsy = false | '' | 0 | null | undefined;
-type Truthy<T> = T extends Falsy ? never : T; // from lodash
-
 export function PageLayout({
 	applyMainElement = false,
 	breadcrumbs,
