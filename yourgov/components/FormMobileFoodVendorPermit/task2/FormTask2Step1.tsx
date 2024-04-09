fb288bb5e87e44fecc188ef012044cc8209b302e
Daniel Tonon <dan503.npm@gmail.com>
2024-04-09 21:49:59 +1000

    Added employee list table

--- yourgov/components/FormMobileFoodVendorPermit/task2/FormTask2Step1.tsx
+++ yourgov/components/FormMobileFoodVendorPermit/task2/FormTask2Step1.tsx
@@ -1,16 +1,26 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { ButtonLink } from '@ag.ds-next/react/button';
+import { Button, ButtonLink } from '@ag.ds-next/react/button';
 import { H2, H3 } from '@ag.ds-next/react/heading';
 import { Stack } from '@ag.ds-next/react/stack';
 import { AvatarIcon, PlusIcon } from '@ag.ds-next/react/icon';
 import { Text } from '@ag.ds-next/react/text';
 import { ListItem, UnorderedList } from '@ag.ds-next/react/list';
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+	TableWrapper,
+} from '@ag.ds-next/react/table';
 import { StepActions } from '../StepActions';
 import { useGlobalForm } from '../GlobalFormProvider';
 import { FormTask2Container } from './FormTask2Container';
 import {
 	ArrayOfTask2Step1FormSchema,
+	Task2Step1FormSchema,
 	arrayOfTask2Step1FormSchema,
 } from './FormTask2FormState';
 import { task2FormSteps, useFormTask2Context } from './FormTask2Provider';
@@ -35,6 +45,8 @@ export function FormTask2Step1() {
 		reValidateMode: 'onBlur',
 	});
 
+	const hasEmployees = (formState.task2?.step1?.employeeList?.length || 0) > 0;
+
 	const onSubmit: SubmitHandler<ArrayOfTask2Step1FormSchema> = async (data) => {
 		arrayOfTask2Step1FormSchema.parse(data);
 		await submitStep();
@@ -53,38 +65,86 @@ export function FormTask2Step1() {
 		});
 	};
 
+	function removeEmployee(employee: Partial<Task2Step1FormSchema> | undefined) {
+		setFormState({
+			...formState,
+			task2: {
+				...formState.task2,
+				step1: {
+					...formState.task2?.step1,
+					// ...data,
+					employeeList: formState.task2?.step1?.employeeList?.filter(
+						(emp) => emp?.email !== employee?.email
+					),
+					completed: true,
+				},
+			},
+		});
+	}
+
 	return (
 		<FormTask2Container
 			formTitle={task2FormSteps[0].label}
 			formIntroduction="Add your employee details."
 		>
-			<Stack
-				as="form"
-				gap={3}
-				alignItems="flex-start"
-				width="100%"
-				onSubmit={handleSubmit(onSubmit)}
-			>
-				<Stack gap={1.5} alignItems="flex-start" width="100%">
-					<H2>List of employees</H2>
-					<UnorderedList>
-						{formState.task2?.step1?.employeeList?.map((employee, i) => (
-							<ListItem key={i}>{employee?.email}</ListItem>
-						))}
-					</UnorderedList>
+			<Stack as="form" gap={3} width="100%" onSubmit={handleSubmit(onSubmit)}>
+				<H2>List of employees</H2>
 
-					<AvatarIcon size="xl" />
-					<H3>No employees added</H3>
-					<Text as="p">You must add an employee to proceed.</Text>
-					<ButtonLink
-						href={step1AddEmployeePath}
-						variant="secondary"
-						size="sm"
-						iconBefore={PlusIcon}
-					>
-						Add employee
-					</ButtonLink>
-				</Stack>
+				{hasEmployees && (
+					<TableWrapper>
+						<Stack gap={1.5} alignItems="flex-start" width="100%">
+							<Table>
+								<TableHead>
+									<TableRow>
+										<TableHeader scope="col">Employee name</TableHeader>
+										<TableHeader scope="col">Email address</TableHeader>
+										<TableHeader scope="col">Action</TableHeader>
+									</TableRow>
+								</TableHead>
+								<TableBody>
+									{formState.task2?.step1?.employeeList?.map((employee) => (
+										<TableRow key={employee?.email}>
+											<TableCell>{`${employee?.firstName} ${employee?.lastName}`}</TableCell>
+											<TableCell>{employee?.email}</TableCell>
+											<TableCell>
+												<Button
+													onClick={() => removeEmployee(employee)}
+													variant="text"
+												>
+													Remove
+												</Button>
+											</TableCell>
+										</TableRow>
+									))}
+								</TableBody>
+							</Table>
+							<ButtonLink
+								href={step1AddEmployeePath}
+								variant="secondary"
+								size="sm"
+								iconBefore={PlusIcon}
+							>
+								Add employee
+							</ButtonLink>
+						</Stack>
+					</TableWrapper>
+				)}
+
+				{!hasEmployees && (
+					<Stack gap={1.5} alignItems="flex-start" width="100%">
+						<AvatarIcon size="xl" />
+						<H3>No employees added</H3>
+						<Text as="p">You must add an employee to proceed.</Text>
+						<ButtonLink
+							href={step1AddEmployeePath}
+							variant="secondary"
+							size="sm"
+							iconBefore={PlusIcon}
+						>
+							Add employee
+						</ButtonLink>
+					</Stack>
+				)}
 				<StepActions />
 			</Stack>
 		</FormTask2Container>
