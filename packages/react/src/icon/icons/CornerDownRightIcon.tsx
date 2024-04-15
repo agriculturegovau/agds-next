0ded33d79b409d23ca2504f156c601280c566054
ChrisLaneAU <43688949+ChrisLaneAU@users.noreply.github.com>
2024-04-15 16:55:27 +1000

    Update packages/react/src/icon/icons/CornerDownRightIcon.tsx
    Co-authored-by: Matt Stow <matt@mattstow.com>

--- packages/react/src/icon/icons/CornerDownRightIcon.tsx
+++ packages/react/src/icon/icons/CornerDownRightIcon.tsx
@@ -2,10 +2,6 @@ import { createIcon } from '../Icon';
 
 export const CornerDownRightIcon = createIcon(
 	<path
-		stroke="currentcolor"
-		strokeLinecap="round"
-		strokeLinejoin="round"
-		strokeWidth="2"
 		d="m15 10 5 5m0 0-5 5m5-5H8a4 4 0 0 1-4-4V4"
 	/>,
 	'CornerDownRightIcon'
