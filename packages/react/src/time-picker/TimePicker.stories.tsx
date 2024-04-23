660ff68471a0b0206ecddfdc93a8256c4e08dc69
Matt Stow <matt@mattstow.com>
2024-04-23 15:24:22 +1000

    Storybook

--- packages/react/src/time-picker/TimePicker.stories.tsx
+++ packages/react/src/time-picker/TimePicker.stories.tsx
@@ -1,9 +1,19 @@
-import { StoryObj, Meta } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
+import { type DefaultComboboxOption } from '../combobox';
 import { TimePicker } from './TimePicker';
 
 const meta: Meta<typeof TimePicker> = {
 	title: 'Forms/TimePicker',
 	component: TimePicker,
+	args: {
+		label: 'Select a time',
+		hint: 'Start typing to see results',
+	},
+	render: function TimePickerStory(props) {
+		const [value, onChange] = useState<DefaultComboboxOption | null>(null);
+		return <TimePicker {...props} value={value} onChange={onChange} />;
+	},
 };
 
 export default meta;
@@ -11,3 +21,46 @@ export default meta;
 type Story = StoryObj<typeof TimePicker>;
 
 export const Basic: Story = {};
+
+export const DifferentTimeFormat: Story = {
+	args: {
+		timeFormat: 'HH:mm',
+	},
+};
+
+export const Required: Story = {
+	args: {
+		required: true,
+	},
+};
+
+export const HideOptionalLabel: Story = {
+	args: {
+		hideOptionalLabel: true,
+	},
+};
+
+export const Disabled: Story = {
+	args: {
+		disabled: true,
+	},
+};
+
+export const Invalid: Story = {
+	args: {
+		invalid: true,
+		message: 'Time is required',
+	},
+};
+
+export const Block: Story = {
+	args: {
+		block: true,
+	},
+};
+
+export const Clearable: Story = {
+	args: {
+		clearable: true,
+	},
+};
