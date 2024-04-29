eb14bca9d51705cd7320261be84704165925a7e1
danger17 <danger17@users.noreply.github.com>
2024-04-29 04:53:38 +0000

    Remove preview for PR 1642 🛬

--- pr-preview/pr-1642/storybook/assets/WithTooltip-V3YHNWJZ-3b3fe09f.js
+++
@@ -1,1 +0,0 @@
-import{W as a,W as r,a as W}from"./DocsRenderer-NNNQARDV-661a4c1d.js";import"./iframe-656543c2.js";import"../sb-preview/runtime.js";export{a as WithToolTipState,r as WithTooltip,W as WithTooltipPure};
--- pr-preview/pr-1642/storybook/assets/syntaxhighlighter-V7JZZA35-b0fe2502.js
+++
@@ -1,1 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-NNNQARDV-661a4c1d.js";import"./iframe-656543c2.js";import"../sb-preview/runtime.js";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
