a6663946655c82459dcfaeab9b5606cce1aa33ec
stowball <stowball@users.noreply.github.com>
2024-07-22 03:14:11 +0000

    Remove preview for PR 1716 🛬

--- pr-preview/pr-1716/storybook/assets/WithTooltip-V3YHNWJZ-289cb5eb.js
+++
@@ -1,1 +0,0 @@
-import{W as a,W as r,a as W}from"./DocsRenderer-NNNQARDV-5bc90783.js";import"./iframe-fb374cd7.js";import"../sb-preview/runtime.js";export{a as WithToolTipState,r as WithTooltip,W as WithTooltipPure};
--- pr-preview/pr-1716/storybook/assets/syntaxhighlighter-V7JZZA35-726ab93c.js
+++
@@ -1,1 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-NNNQARDV-5bc90783.js";import"./iframe-fb374cd7.js";import"../sb-preview/runtime.js";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
