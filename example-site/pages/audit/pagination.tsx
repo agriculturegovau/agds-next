706fd0e0c4265d2dc588ae1461688aab569e2145
Matt Stow <matt@mattstow.com>
2024-07-26 15:56:17 +1000

    Audit: Temporary audit redirects (#1722)

---
+++ example-site/pages/audit/pagination.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function AuditPaginationRedirect() {
+	return (
+		<>
+			<Head>
+				<meta
+					http-equiv="refresh"
+					content="1;url=https://design-system.agriculture.gov.au/pr-preview/pr-1726/storybook/iframe.html?args=&id=layout-filtersidebar--full&viewMode=story"
+				/>
+			</Head>
+		</>
+	);
+}
