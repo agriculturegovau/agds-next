282a8f0d5de4e242552c882a3ac6ecee7df11e23
ChrisLaneAU <43688949+ChrisLaneAU@users.noreply.github.com>
2024-07-30 10:41:21 +1000

    Audit: Table settings (#1729)
    * table-settings
    
    * Button panel updates
    
    * Keep business name visible at all times
    
    * Always true
    
    * Close button should not update state
    
    * Handle undefined dates in active filters
    
    * Apply activeColumns prop
    
    * Table settings aria
    
    * Actions as capital
    
    * Design QA
    
    * Fix appliedSettingsCount
    
    * Fix table settings properly
    
    * lint
    
    * checkboxes aren't required
    
    ---------
    
    Co-authored-by: Matt Stow <matt@mattstow.com>

--- .storybook/stories/DataFiltering/components/DashboardPagination.tsx
+++ .storybook/stories/DataFiltering/components/DashboardPagination.tsx
@@ -1,13 +1,64 @@
-import { useEffect, useState } from 'react';
+import {
+	Dispatch,
+	SetStateAction,
+	useCallback,
+	useEffect,
+	useState,
+} from 'react';
+import { PaginationItemsPerPageSelect } from '../../../../packages/react/src/pagination/PaginationItemsPerPageSelect';
+import { useDataContext, useSortAndFilterContext } from '../lib/contexts';
+import { Box } from '../../../../packages/react/src/box';
+import { Button, ButtonGroup } from '../../../../packages/react/src/button';
+import { Checkbox } from '../../../../packages/react/src/checkbox';
+import { ControlGroup } from '../../../../packages/react/src/control-group';
+import { Divider } from '../../../../packages/react/src/divider';
+import { Drawer } from '../../../../packages/react/src/drawer';
+import { FormStack } from '../../../../packages/react/src/form-stack';
+import { tokens, useTernaryState } from '../../../../packages/react/src/core';
 import {
 	PaginationButtons,
 	generatePaginationRangeText,
-} from '@ag.ds-next/react/pagination';
-import { useDataContext, useSortAndFilterContext } from '../lib/contexts';
+} from '../../../../packages/react/src/pagination';
+import { SettingsIcon } from '../../../../packages/react/src/icon';
+import { headers } from './DataTable';
 
-export const DashboardPagination = () => {
+const columnNames = [...headers].slice(1).map((header) => header.label);
+export const defaultActiveColumns = columnNames.reduce(
+	(acc, name) => ({ ...acc, [name]: true }),
+	{ 'Business name': true } as Record<(typeof columnNames)[number], boolean>
+);
+
+type FormState = {
+	activeColumns: typeof defaultActiveColumns;
+	paginationPerPage: number;
+};
+
+export const DashboardPagination = ({
+	activeColumns,
+	setActiveColumns,
+}: {
+	activeColumns?: typeof defaultActiveColumns;
+	setActiveColumns?: Dispatch<SetStateAction<Record<string, boolean>>>;
+}) => {
 	const { pagination, setPagination } = useSortAndFilterContext();
 	const { data, totalItems, totalPages, loading } = useDataContext();
+
+	const [isDrawerOpen, openDrawer, closeDrawer] = useTernaryState(false);
+	const [formState, setFormState] = useState({
+		activeColumns: defaultActiveColumns,
+		paginationPerPage: pagination.perPage,
+	});
+	const handlerForKey = useCallback(
+		(key: keyof typeof defaultActiveColumns) => () =>
+			setFormState((prevFormState: FormState) => ({
+				...prevFormState,
+				activeColumns: {
+					...prevFormState.activeColumns,
+					[key]: !prevFormState.activeColumns[key],
+				},
+			})),
+		[]
+	);
 
 	const itemRangeText = generatePaginationRangeText({
 		totalItems: totalItems,
@@ -36,27 +87,184 @@ export const DashboardPagination = () => {
 		// We only want to update update the display text once the table data has finished loading, all other deps are ignored here
 	}, [loading]); // eslint-disable-line react-hooks/exhaustive-deps
 
+	const [appliedSettingsCount, setAppliedSettingsCount] = useState(0);
+
+	useEffect(() => {
+		const hasChangedColumnSettings = !Object.values(
+			formState.activeColumns
+		).every(Boolean);
+		const hasChangedItemsPerPageSettings = formState.paginationPerPage !== 10;
+
+		setAppliedSettingsCount(
+			Number(hasChangedColumnSettings) + Number(hasChangedItemsPerPageSettings)
+		);
+	}, [formState.activeColumns, formState.paginationPerPage]);
+
 	if (!data.length) return null;
 
 	return (
-		<PaginationButtons
-			currentPage={displayText.page}
-			onChange={(page) => setPagination({ perPage: displayText.perPage, page })}
-			totalPages={totalPages}
-			itemRangeText={displayText.itemRangeText}
-			itemsPerPage={displayText.perPage}
-			onItemsPerPageChange={(perPage) =>
-				setPagination({
-					page: getValidPage({
-						currentPage: pagination.page,
-						totalItems,
-						perPage,
-						prevPerPage: pagination.perPage,
-					}),
-					perPage,
-				})
-			}
-		/>
+		<>
+			<Box
+				display="grid"
+				gap={1}
+				css={{
+					gridTemplateColumns: '1fr',
+					[tokens.mediaQuery.min.lg]: {
+						gridTemplateColumns: '2fr 1fr',
+					},
+				}}
+			>
+				<PaginationButtons
+					currentPage={displayText.page}
+					onChange={(page) =>
+						setPagination({ perPage: displayText.perPage, page })
+					}
+					totalPages={totalPages}
+					itemRangeText={displayText.itemRangeText}
+				/>
+
+				<Button
+					aria-label={
+						appliedSettingsCount
+							? `Table settings. ${appliedSettingsCount} applied ${
+									appliedSettingsCount === 1 ? 'setting' : 'settings'
+							  }`
+							: 'Table settings'
+					}
+					css={{
+						[tokens.mediaQuery.min.lg]: {
+							justifySelf: 'end',
+						},
+					}}
+					iconBefore={SettingsIcon}
+					onClick={openDrawer}
+					variant="tertiary"
+				>
+					Table settings{' '}
+					{Boolean(appliedSettingsCount) && `(${appliedSettingsCount})`}
+				</Button>
+			</Box>
+
+			<Drawer
+				actions={
+					<ButtonGroup>
+						<Button
+							type="submit"
+							form="table-settings-form-id"
+							onClick={(event) => {
+								event.preventDefault();
+
+								setPagination({
+									page: getValidPage({
+										currentPage: pagination.page,
+										totalItems,
+										perPage: formState.paginationPerPage,
+										prevPerPage: pagination.perPage,
+									}),
+									perPage: formState.paginationPerPage,
+								});
+
+								setActiveColumns?.(formState.activeColumns);
+
+								closeDrawer();
+							}}
+						>
+							Save settings
+						</Button>
+
+						<Button
+							type="reset"
+							form="table-settings-form-id"
+							onClick={(event) => {
+								event.preventDefault();
+
+								setFormState({
+									activeColumns: defaultActiveColumns,
+									paginationPerPage: 10,
+								});
+
+								setPagination({
+									page: getValidPage({
+										currentPage: pagination.page,
+										totalItems,
+										perPage: 10,
+										prevPerPage: pagination.perPage,
+									}),
+									perPage: 10,
+								});
+
+								closeDrawer();
+							}}
+							variant="secondary"
+						>
+							Reset settings
+						</Button>
+
+						<Button
+							variant="tertiary"
+							onClick={() => {
+								if (!activeColumns) return;
+
+								setFormState({
+									activeColumns,
+									paginationPerPage: pagination.perPage,
+								});
+
+								closeDrawer();
+							}}
+						>
+							Cancel
+						</Button>
+					</ButtonGroup>
+				}
+				isOpen={isDrawerOpen}
+				onClose={() => {
+					if (!activeColumns) return;
+
+					setFormState({
+						activeColumns,
+						paginationPerPage: pagination.perPage,
+					});
+
+					closeDrawer();
+				}}
+				title="Table settings"
+			>
+				<form id="table-settings-form-id">
+					<FormStack>
+						<PaginationItemsPerPageSelect
+							value={formState.paginationPerPage}
+							onChange={(perPage) =>
+								setFormState((prevFormState) => ({
+									...prevFormState,
+									paginationPerPage: perPage,
+								}))
+							}
+						/>
+
+						<Divider />
+
+						<ControlGroup
+							label="Visible columns"
+							hint="Select, Business name and Actions will always be visible."
+							block
+							hideOptionalLabel
+						>
+							{columnNames.map((columnName) => (
+								<Checkbox
+									key={columnName}
+									name="columnName"
+									checked={formState.activeColumns[columnName]}
+									onChange={handlerForKey(columnName)}
+								>
+									{columnName}
+								</Checkbox>
+							))}
+						</ControlGroup>
+					</FormStack>
+				</form>
+			</Drawer>
+		</>
 	);
 };
 
