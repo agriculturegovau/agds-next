4200301d59ee35f9086bd51e428c06b1652dc405
Matt Stow <matt@mattstow.com>
2024-08-02 08:58:10 +1000

    prevent overscroll bounce

--- packages/react/src/table/TableScroller.tsx
+++ packages/react/src/table/TableScroller.tsx
@@ -217,6 +217,7 @@ export function TableScroller({ children }: TableScrollerProps) {
 				css={{
 					msOverflowStyle: 'none',
 					overflowX: 'auto',
+					overscrollBehaviorX: 'none',
 					scrollbarWidth: 'none',
 					WebkitOverflowScrolling: 'touch',
 					width: '100%',
