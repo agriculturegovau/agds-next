cd19e9bae955908bd2bc1c1d82289a90e43a1f4e
Matt Stow <matt@mattstow.com>
2024-08-27 17:37:56 +1000

    combobox: Announce loading and error states to screen readers

--- packages/react/src/combobox/ComboboxBase/ComboboxListLoading.tsx
+++ packages/react/src/combobox/ComboboxBase/ComboboxListLoading.tsx
@@ -7,7 +7,7 @@ export function ComboboxListLoading() {
 	return (
 		<ComboboxListItem isActiveItem={false} isInteractive={false}>
 			<Flex gap={1} alignItems="center">
-				<LoadingDots size="sm" aria-label="Loading" />
+				<LoadingDots role="status" size="sm" aria-label="Loading" />
 				<Text>Loading</Text>
 			</Flex>
 		</ComboboxListItem>
