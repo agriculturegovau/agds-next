02ba3e2b96b6d874d79a9209d8e23af1a7b7eeef
Chris Lane <chris.lane@thinkmill.com.au>
2024-09-12 09:46:18 +1000

    fit-content and display block

--- packages/react/src/details/Details.tsx
+++ packages/react/src/details/Details.tsx
@@ -32,7 +32,7 @@ export const Details = forwardRef<HTMLDetailsElement, DetailsProps>(
 			>
 				<Flex
 					as="summary"
-					css={{ width: 'min-content' }}
+					css={{ display: 'block', width: 'fit-content' }}
 					link
 					focusRingFor="keyboard"
 					alignItems="center"
