38ada321cad5ef31fbd4919b293b520e03af5b85
Matt Stow <matt@mattstow.com>
2024-09-17 09:50:50 +1000

    Thanks Sonar

--- jest.setup.js
+++ jest.setup.js
@@ -1,10 +1,7 @@
 Object.defineProperty(window, 'CSS', {
 	value: {
 		supports: jest.fn().mockImplementation((property, value) => {
-			if (property === '-webkit-tap-highlight-color' && value === 'black') {
-				return true;
-			}
-			return false; // Fallback for unsupported properties
+			return property === '-webkit-tap-highlight-color' && value === 'black';
 		}),
 	},
 });
