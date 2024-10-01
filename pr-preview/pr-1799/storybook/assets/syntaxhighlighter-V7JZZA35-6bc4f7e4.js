531f4eed13fc645748905f7d1dea20e88b89df17
stowball <stowball@users.noreply.github.com>
2024-10-01 04:18:11 +0000

    Remove preview for PR 1799 🛬

--- pr-preview/pr-1799/storybook/assets/syntaxhighlighter-V7JZZA35-6bc4f7e4.js
+++
@@ -1,1 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-NNNQARDV-f16c104a.js";import"./iframe-2d2f990f.js";import"../sb-preview/runtime.js";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
