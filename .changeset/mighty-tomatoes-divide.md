712262ee367e21bf833c3dff4b4d33c12fb02d06
Chris Lane <chris.lane@thinkmill.com.au>
2024-10-03 11:04:17 +1000

    docs: Set edit-this-page link to dynamically select a branch

---
+++ .changeset/mighty-tomatoes-divide.md
@@ -0,0 +1,5 @@
+---
+'@ag.ds-next/docs': minor
+---
+
+docs: Set dynamic branch for edit-this-page link so that editors can primarily be sent to develop.
--- docs/components/EditPage.tsx
+++ docs/components/EditPage.tsx
@@ -1,12 +1,23 @@
 import { TextLink } from '@ag.ds-next/react/text-link';
 
+/**
+ * We only want editors to be sent to the main branch on GitHub when they are:
+ * - Not on /develop
+ * - Not on /pr-preview/...
+ * - Not in development
+ *
+ * See .github/workflows/deploy-develop-docs.yml
+ */
+const isDevelop =
+	!!process.env.BASE_PATH || process.env.NODE_ENV !== 'production';
+const branch = isDevelop ? 'develop' : 'main';
+
 const ORG = 'agriculturegovau';
 const REPO = 'agds-next';
-const BRANCH = 'main';
 
 export function EditPage({ path = '' }) {
 	return (
-		<TextLink href={`https://github.com/${ORG}/${REPO}/edit/${BRANCH}${path}`}>
+		<TextLink href={`https://github.com/${ORG}/${REPO}/edit/${branch}${path}`}>
 			Edit this page
 		</TextLink>
 	);
