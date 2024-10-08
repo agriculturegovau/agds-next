2e144d0048b53e06cfdb1610f9427df3ee30bc62
Chris Lane <chris.lane@thinkmill.com.au>
2024-10-08 12:33:47 +1100

    file-upload: Announce descriptions of file size and accepted files

--- packages/react/src/file-upload/FileUpload.tsx
+++ packages/react/src/file-upload/FileUpload.tsx
@@ -8,7 +8,7 @@ import {
 import { DropzoneOptions, FileRejection, useDropzone } from 'react-dropzone';
 import { visuallyHiddenStyles } from '../a11y';
 import { Button } from '../button';
-import { boxPalette, mergeRefs, tokens } from '../core';
+import { boxPalette, mergeRefs, tokens, useId } from '../core';
 import { Field } from '../field';
 import { UploadIcon } from '../icon';
 import { ListItem, UnorderedList } from '../list';
@@ -248,6 +248,10 @@ export const FileUpload = forwardRef<HTMLInputElement, FileUploadProps>(
 			(invalidRejections && invalidRejections?.length > 1) ||
 			(tooManyFilesRejections && tooManyFilesRejections?.length > 1);
 
+		const fallbackId = useId(id);
+		const fileSizeDescriptionId = `${fallbackId}-file-size-desc`;
+		const acceptedFilesDescriptionId = `${fallbackId}-accepted-files-desc`;
+
 		return (
 			<Field
 				label={label}
@@ -313,18 +317,19 @@ export const FileUpload = forwardRef<HTMLInputElement, FileUploadProps>(
 											</Text>
 										</span>
 										{maxSize ? (
-											<Text color="muted">
+											<Text color="muted" id={fileSizeDescriptionId}>
 												{multiple ? 'Each file' : 'File'} cannot exceed{' '}
 												{formattedMaxFileSize}.
 											</Text>
 										) : null}
 										{accept ? (
-											<Text color="muted">
+											<Text color="muted" id={acceptedFilesDescriptionId}>
 												Files accepted: {acceptedFilesSummary}.
 											</Text>
 										) : null}
 									</Stack>
 									<Button
+										aria-describedby={`${fileSizeDescriptionId} ${acceptedFilesDescriptionId}`}
 										disabled={disabled}
 										onClick={open}
 										type="button"
