9d32031d9cbead81ac1b754839ad1f12a1a23a5c
stowball <stowball@users.noreply.github.com>
2024-10-14 03:34:09 +0000

    Remove preview for PR 1834 🛬

--- pr-preview/pr-1834/storybook/assets/WithTooltip-V3YHNWJZ-ff4d3436.js
+++
@@ -1,1 +0,0 @@
-import{W as a,W as r,a as W}from"./DocsRenderer-NNNQARDV-333e9ac4.js";import"./iframe-db6c87ac.js";import"../sb-preview/runtime.js";export{a as WithToolTipState,r as WithTooltip,W as WithTooltipPure};
--- pr-preview/pr-1834/storybook/assets/syntaxhighlighter-V7JZZA35-b2625361.js
+++
@@ -1,1 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-NNNQARDV-333e9ac4.js";import"./iframe-db6c87ac.js";import"../sb-preview/runtime.js";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
