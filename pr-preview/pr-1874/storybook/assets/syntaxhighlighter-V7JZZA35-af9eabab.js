fce5b6c706df8098dd8f8da137572d5b4fe78826
stowball <stowball@users.noreply.github.com>
2024-11-11 04:36:32 +0000

    Remove preview for PR 1874 🛬

--- pr-preview/pr-1874/storybook/assets/syntaxhighlighter-V7JZZA35-af9eabab.js
+++
@@ -1,1 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-NNNQARDV-3a6f4706.js";import"./iframe-0946a550.js";import"../sb-preview/runtime.js";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
