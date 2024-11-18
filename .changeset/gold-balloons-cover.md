d6ee73c26705acbe5ba7d50ba4230381d115d570
Chris Lane <chris.lane@thinkmill.com.au>
2024-11-18 12:03:39 +1100

    file-input: Use design system button

---
+++ .changeset/gold-balloons-cover.md
@@ -0,0 +1,5 @@
+---
+'@ag.ds-next/react': minor
+---
+
+file-input: Use design system secondary button for consistent visual and screen-reader announcement.
--- packages/react/src/file-input/FileInput.tsx
+++ packages/react/src/file-input/FileInput.tsx
@@ -4,13 +4,16 @@ import {
 	InputHTMLAttributes,
 	useCallback,
 	useMemo,
+	useRef,
 	useState,
 } from 'react';
 import { Field } from '../field';
-import { packs, boxPalette, fontGrid, mapSpacing, tokens } from '../core';
-import { buttonStyles } from '../button';
+import { Button } from '../button';
 import { AcceptedFileMimeTypes } from '../file-upload';
 import { fileTypeMapping } from '../file-upload/utils';
+import { Flex } from '../flex';
+import { VisuallyHidden } from '../a11y';
+import { Text } from '../text';
 
 type NativeInputProps = InputHTMLAttributes<HTMLInputElement>;
 
@@ -60,16 +63,25 @@ export const FileInput = forwardRef<HTMLInputElement, FileInputProps>(
 		},
 		ref
 	) {
-		const [hasFile, setHasFile] = useState(false);
-		const styles = useFileInputStyles({ hasFile });
+		const fallbackRef = useRef(null);
+		const hiddenInputRef = ref || fallbackRef;
 
+		const [fileNames, setFileNames] = useState<string[]>([]);
+
+		// Called on the hidden input
 		const onChange = useCallback(
 			(event: ChangeEvent<HTMLInputElement>) => {
 				onChangeProp?.(event);
-				setHasFile(Boolean(event.target.files?.length));
+				if (!event.target.files) return;
+				setFileNames(Array.from(event.target.files).map((file) => file.name));
 			},
 			[onChangeProp]
 		);
+
+		// Used as a visual front to provide a consistent screen reader announcement across different browsers (e.g. Firefox and Chrome have "Browse..." and "Choose file" respectively)
+		const onVisualButtonClick = () => {
+			if ('current' in hiddenInputRef) hiddenInputRef.current?.click();
+		};
 
 		// Converts an array of mime types, e.g. `image/jpeg`, `application/pdf` into valid file extensions
 		const acceptedFileExtensions = useMemo(() => {
@@ -104,56 +116,55 @@ export const FileInput = forwardRef<HTMLInputElement, FileInputProps>(
 				id={id}
 			>
 				{(a11yProps) => (
-					<input
-						accept={accept?.toString()}
-						ref={ref}
-						css={styles}
-						{...a11yProps}
-						type="file"
-						disabled={disabled}
-						onChange={onChange}
-						{...props}
-					/>
+					<>
+						<Flex gap={1} alignItems="center">
+							<Button
+								{...a11yProps}
+								aria-invalid={undefined} // Buttons not announced as invalid when `aria-invalid`, add announcement to label instead
+								aria-label={`${label}. ${
+									a11yProps['aria-invalid'] ? 'Invalid. ' : ''
+								}${a11yProps['aria-required'] ? 'Required. ' : ''}Select file`}
+								aria-required={undefined} // Buttons can't be `aria-required`, add announcement to label instead
+								css={{ minWidth: 'max-content' }}
+								disabled={disabled}
+								onClick={onVisualButtonClick}
+								variant="secondary"
+							>
+								Select file
+							</Button>
+
+							<div role="status">
+								{!!fileNames.length && (
+									<Text breakWords>
+										{fileNames.join(', ')}
+										<VisuallyHidden>, selected.</VisuallyHidden>
+									</Text>
+								)}
+							</div>
+						</Flex>
+
+						<input
+							accept={accept?.toString()}
+							type="file"
+							disabled={disabled}
+							onChange={onChange}
+							{...props}
+							aria-hidden={true}
+							css={{
+								position: 'absolute',
+								height: 1,
+								width: 1,
+								pointerEvents: 'none',
+							}}
+							ref={hiddenInputRef}
+							tabIndex={-1}
+						/>
+					</>
 				)}
 			</Field>
 		);
 	}
 );
-
-function useFileInputStyles({ hasFile }: { hasFile: boolean }) {
-	// Use `buttonStyles` to generate the styles for a standard secondary button
-	const defaultButtonStyles = buttonStyles({
-		size: 'sm',
-		variant: 'secondary',
-		block: false,
-	});
-
-	return {
-		...fontGrid('sm', 'default'),
-		fontFamily: tokens.font.body,
-		color: boxPalette.foregroundText,
-		fontWeight: hasFile ? tokens.fontWeight.bold : tokens.fontWeight.normal,
-
-		'&::file-selector-button': {
-			...defaultButtonStyles,
-			margin: `0 ${mapSpacing(1)} 0 0`,
-		},
-
-		'&:hover': {
-			cursor: 'pointer',
-			'&:not(:disabled)::file-selector-button':
-				defaultButtonStyles['&:not(:disabled):hover'],
-		},
-
-		'&:disabled': {
-			cursor: 'not-allowed',
-			opacity: 0.3,
-			'&::file-selector-button': { cursor: 'not-allowed' },
-		},
-
-		'&:focus': packs.outline,
-	};
-}
 
 const hasLabel = (
 	mimeTypeData: (typeof fileTypeMapping)[keyof typeof fileTypeMapping]
