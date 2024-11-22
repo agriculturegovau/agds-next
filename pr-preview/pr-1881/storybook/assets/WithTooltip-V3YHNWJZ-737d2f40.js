415c49634f64c201276f06fca948423afd9c8534
stowball <stowball@users.noreply.github.com>
2024-11-22 05:30:05 +0000

    Remove preview for PR 1881 🛬

--- pr-preview/pr-1881/storybook/assets/WithTooltip-V3YHNWJZ-737d2f40.js
+++
@@ -1,1 +0,0 @@
-import{W as a,W as r,a as W}from"./DocsRenderer-NNNQARDV-f677d737.js";import"./iframe-1f5af2d3.js";import"../sb-preview/runtime.js";export{a as WithToolTipState,r as WithTooltip,W as WithTooltipPure};
--- pr-preview/pr-1881/storybook/assets/syntaxhighlighter-V7JZZA35-e33ea60e.js
+++
@@ -1,1 +0,0 @@
-import{S as i,c as e,s as p}from"./DocsRenderer-NNNQARDV-f677d737.js";import"./iframe-1f5af2d3.js";import"../sb-preview/runtime.js";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
