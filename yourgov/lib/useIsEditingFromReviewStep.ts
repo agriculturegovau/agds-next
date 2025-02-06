41e58d99e36cb10a7999f671c85c06ddc8a0246c
Matt Stow <matt@mattstow.com>
2025-02-06 16:51:53 +1100

    Fix matching edit links when other query params exist

--- yourgov/lib/useIsEditingFromReviewStep.ts
+++ yourgov/lib/useIsEditingFromReviewStep.ts
@@ -3,21 +3,23 @@ import { stepsData } from '../components/FormMobileFoodVendorPermit';
 import { type FormStep } from '../components/FormMobileFoodVendorPermit/steps/stepsData';
 
 export const useIsEditingFromReviewStep = () => {
-	const { asPath } = useRouter();
+	const { query, route } = useRouter();
+	const path = `${route.replace('/[...slug]', '')}${
+		Array.isArray(query?.slug) ? `/${query.slug.join('/')}` : ''
+	}`;
 
 	const isChangeHrefMatch = (
 		steps: FormStep[],
-		asPath: string,
 		// depth currently is not used because of the way sub steps are structured
 		depth = 0
 	): { match: FormStep; depth: number } | undefined => {
 		for (const step of steps) {
-			if (step.changeHref === asPath) {
+			if (step.changeHref === path) {
 				return { depth, match: step };
 			}
 
 			if (step.items) {
-				const result = isChangeHrefMatch(step.items, asPath, depth + 1);
+				const result = isChangeHrefMatch(step.items, depth + 1);
 				if (result) {
 					return result;
 				}
@@ -27,5 +29,5 @@ export const useIsEditingFromReviewStep = () => {
 		return undefined;
 	};
 
-	return isChangeHrefMatch(stepsData, asPath);
+	return isChangeHrefMatch(stepsData);
 };
