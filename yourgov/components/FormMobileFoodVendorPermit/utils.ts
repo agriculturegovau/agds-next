41a233d0be75530a8e2cb07117a9ce04f0ac88c0
Matt Stow <matt@mattstow.com>
2025-02-11 09:19:20 +1100

    yourgov: Fix final redirect to success page

--- yourgov/components/FormMobileFoodVendorPermit/utils.ts
+++ yourgov/components/FormMobileFoodVendorPermit/utils.ts
@@ -33,14 +33,13 @@ export function getStepCompletionUrl({
 	editingStep,
 	steps,
 }: GetStepNavigationUrlParams) {
-	const nextStepUrl = `${(editingStep?.match
-		? steps.at(-1)
-		: steps[currentStepIndex + 1]
-	)?.href}${
-		editingStep?.match ? `?success=${editingStep.match.formStateKey}` : ''
-	}`;
-	return (
-		nextStepUrl ||
-		`/app/permits/apply-for-new-permit/mobile-food-vendor-permit/success?id=${id}`
-	);
+	const nextStepUrl = (
+		editingStep?.match ? steps.at(-1) : steps[currentStepIndex + 1]
+	)?.href;
+
+	return nextStepUrl
+		? `${nextStepUrl}${
+				editingStep?.match ? `?success=${editingStep.match.formStateKey}` : ''
+		  }`
+		: `/app/permits/apply-for-new-permit/mobile-food-vendor-permit/success?id=${id}`;
 }
