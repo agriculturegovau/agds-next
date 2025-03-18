d895c373e1fc2f749a509573556ebb16d9bbad41
Matt Stow <matt@mattstow.com>
2025-03-18 14:03:48 +1100

    icon: Add 13 new icons (#1980)

---
+++ packages/react/src/icon/icons/EmailOpenIcon.tsx
@@ -0,0 +1,6 @@
+import { createIcon } from '../Icon';
+
+export const EmailOpenIcon = createIcon(
+	<path d="M22 10c0-.63-.3-1.22-.8-1.6l-8-6a2 2 0 0 0-2.4 0l-8 6A2 2 0 0 0 2 10m20 0v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V10m20 0-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 10" />,
+	'EmailOpenIcon'
+);
---
+++ packages/react/src/icon/icons/FlagFilledIcon.tsx
@@ -0,0 +1,9 @@
+import { createIcon } from '../Icon';
+
+export const FlagFilledIcon = createIcon(
+	<path
+		d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1v12Zm0 0v7"
+		fill="currentColor"
+	/>,
+	'FlagFilledIcon'
+);
