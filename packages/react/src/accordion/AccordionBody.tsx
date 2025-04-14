75682e65a416008131fb094610c64519ac14f4d7
Matt Stow <matt@mattstow.com>
2025-04-14 10:04:00 +1000

    accordion, app-layout, collapsing-sidebar, drawer: Remove react spring (#1998)

--- packages/react/src/accordion/AccordionBody.tsx
+++ packages/react/src/accordion/AccordionBody.tsx
@@ -1,6 +1,5 @@
-import { PropsWithChildren, useRef } from 'react';
-import { useSpring, animated } from '@react-spring/web';
-import { packs, usePrefersReducedMotion } from '../core';
+import { type PropsWithChildren, useRef } from 'react';
+import { packs, useTransitionHeight } from '../core';
 
 export type AccordionBodyProps = PropsWithChildren<{
 	ariaLabelledBy: string;
@@ -15,32 +14,18 @@ export const AccordionBody = ({
 	isOpen,
 }: AccordionBodyProps) => {
 	const ref = useRef<HTMLDivElement>(null);
-
-	const prefersReducedMotion = usePrefersReducedMotion();
-	const animatedHeight = useSpring({
-		from: { display: 'none', height: 0 },
-		to: async (next) => {
-			if (isOpen) await next({ display: 'block' });
-			await next({
-				height: isOpen ? ref.current?.offsetHeight : 0,
-				immediate: prefersReducedMotion,
-			});
-			await next(isOpen ? { height: 'auto' } : { display: 'none' });
-		},
-	});
+	const [transitionHeightProp, transitionHeightStyles] =
+		useTransitionHeight(isOpen);
 
 	return (
-		<animated.div
+		<div
 			aria-labelledby={ariaLabelledBy}
-			css={{
-				overflow: 'hidden',
-				...packs.print.visible,
-			}}
+			css={[transitionHeightStyles, packs.print.visible]}
 			id={id}
 			role="region"
-			style={animatedHeight}
+			{...transitionHeightProp}
 		>
 			<div ref={ref}>{children}</div>
-		</animated.div>
+		</div>
 	);
 };
