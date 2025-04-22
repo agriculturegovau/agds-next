5c8b1c654cfd7a3564233b4c9c7f92d9c8e8dfb6
Alex Lee <201531458+atlee025@users.noreply.github.com>
2025-04-22 13:20:03 +1000

    table: add onClick support to TableRow (#1994)
    Adds a hover style and updates selected style.
    
    Co-authored-by: Matt Stow <matt@mattstow.com>

--- packages/react/src/table/TableRow.tsx
+++ packages/react/src/table/TableRow.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from 'react';
+import { theme } from '../ag-branding';
 import { backgroundColorMap, type BackgroundColor } from '../box';
 import { boxPalette, tokens } from '../core';
-import { theme } from '../ag-branding';
 import { useTableContext } from './TableContext';
 
 export type TableRowProps = PropsWithChildren<{
@@ -11,6 +11,8 @@ export type TableRowProps = PropsWithChildren<{
 	background?: BackgroundColor;
 	/** Style the row when a cell contains an error. */
 	invalid?: boolean;
+	/** Callback function to execute when the row is clicked. Callback is not run when the clicked element is not a td, th or tr. */
+	onClick?: (event: React.MouseEvent<HTMLTableRowElement>) => unknown;
 	/** Indicates the current selected state of the table row. */
 	selected?: boolean;
 }>;
@@ -20,9 +22,28 @@ export function TableRow({
 	background,
 	children,
 	invalid,
+	onClick,
 	selected,
 }: TableRowProps) {
 	const { tableLayout } = useTableContext();
+
+	function handleOnClick(event: React.MouseEvent<HTMLTableRowElement>) {
+		// Check if the the target has an interactive parent before running row onClick
+		const targetElement = event.target;
+		if (
+			(targetElement as HTMLElement).closest(
+				'a, button, input, select, textarea'
+			)
+		) {
+			return;
+		}
+
+		if ((targetElement as HTMLElement).closest('tr')) {
+			event.stopPropagation();
+			onClick?.(event);
+		}
+	}
+
 	return (
 		<tr
 			aria-rowindex={ariaRowindex}
@@ -35,18 +56,18 @@ export function TableRow({
 					: undefined,
 				...(selected && {
 					...(tableLayout === 'auto' && {
-						position: 'relative',
 						backgroundColor: boxPalette.selectedMuted,
+						position: 'relative',
 
 						// Add outline
 						'&::after': {
-							content: '""',
-							pointerEvents: 'none',
-							position: 'absolute',
-							inset: 0,
-							borderWidth: tokens.borderWidth.md,
 							borderColor: boxPalette.selected,
 							borderStyle: 'solid',
+							borderWidth: tokens.borderWidth.sm,
+							content: '""',
+							inset: 0,
+							pointerEvents: 'none',
+							position: 'absolute',
 						},
 
 						// Remove the border top (if next table row is selected)
@@ -59,19 +80,56 @@ export function TableRow({
 							borderTopWidth: 0,
 						},
 					}),
+				}),
+				...(onClick && {
+					...(tableLayout === 'auto' && {
+						position: 'relative',
 
+						'&:hover': {
+							'& > td, & > th': {
+								cursor: 'pointer',
+							},
+
+							// Add outline
+							'&::after': {
+								borderColor: boxPalette.selected,
+								borderStyle: 'solid',
+								borderWidth: tokens.borderWidth.md,
+								content: '""',
+								inset: 0,
+								pointerEvents: 'none',
+								position: 'absolute',
+							},
+
+							// Set border bottom for select & hover
+							":has(+ tr[aria-selected='true'])::after": {
+								borderBottomWidth: tokens.borderWidth.md,
+							},
+						},
+					}),
+				}),
+				...(invalid && {
+					background: theme.lightSystemErrorMuted,
+				}),
+				...((selected || onClick) && {
 					// Chrome and Firefox doesn't support ::after elements in fixed table layouts
 					// FIXME Once Chrome Firefox fixes this issue, these alternative styles should be removed
-					...(tableLayout === 'fixed' && alternativeSelectedStyles),
+					...(tableLayout === 'fixed' && {
+						...(selected && alternativeSelectedStyles),
+						...(onClick && alternativeHoverStyles),
+					}),
 
 					// Safari does not support relative positioning on `tr` elements
 					// FIXME Once safari fixes this issue, these alternative styles should be removed
 					// More info https://www.reddit.com/r/css/comments/s195xg/safari_alternative_to_positionrelative_on_tr/?rdt=41288
-					'@supports (-webkit-appearance: -apple-pay-button)':
-						alternativeSelectedStyles,
+					'@supports (-webkit-appearance: -apple-pay-button)': {
+						...(selected && alternativeSelectedStyles),
+						...(onClick && alternativeHoverStyles),
+					},
 				}),
 			}}
 			data-has-background={Boolean(background || invalid) || undefined}
+			onClick={onClick ? handleOnClick : undefined}
 		>
 			{children}
 		</tr>
@@ -81,9 +139,19 @@ export function TableRow({
 // Use an outline instead of an ::after element
 const alternativeSelectedStyles = {
 	backgroundColor: boxPalette.selectedMuted,
-	outlineWidth: '2px',
-	outlineStyle: 'solid',
 	outlineColor: boxPalette.selected,
-	outlineOffset: '-3px',
+	outlineOffset: -tokens.borderWidth.md,
+	outlineStyle: 'solid',
+	outlineWidth: tokens.borderWidth.sm,
+	'&::after': { display: 'none' },
+};
+
+const alternativeHoverStyles = {
+	'&:hover': {
+		outlineColor: boxPalette.selected,
+		outlineOffset: -tokens.borderWidth.lg,
+		outlineStyle: 'solid',
+		outlineWidth: tokens.borderWidth.md,
+	},
 	'&::after': { display: 'none' },
 };
